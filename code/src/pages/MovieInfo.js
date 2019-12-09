196618f0dfe88cf65ecfc5a2791fd284bc03429e
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-09 14:53:43 +0100

    Console log of second page

--- code/src/pages/MovieInfo.js
+++ code/src/pages/MovieInfo.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const MovieInfo = () => {
+    const { movieId } = useParams()
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=3812b9925d12c2723ac148f3607b8bb5&language=en-US`)
+        .then((res) => res.json())
+        .then((json) => {
+            console.log(json)
+        })
+    }, [movieId])
+
     return(
         <div>Movie Info</div>
     )
