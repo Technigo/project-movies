38cd35d9ac93422b15f3a37bca8702c3e338b834
nyblad <nyblad@hotmail.com>
2019-12-09 21:29:03 +0100

    Started creating list of movies  and styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { PopularList } from 'pages/PopularList'
+import { Detail } from 'pages/Detail'
+
+//Page/Component: Popular list that maps all the popular movies from API
+//Page/Component: Detail that renders details about the movie (poster, info, voting)
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <BrowserRouter>
+        <Switch>
+
+          <Route path="/" exact>
+            <PopularList />
+          </Route>
+
+          <Route path="/movies/:id">
+            <Detail />
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
