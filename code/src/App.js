7e21339b318336884b964cf0685252eb7842ab3f
Petra B <petrabrinck@icloud.com>
2019-12-09 11:40:34 +0100

    first commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { MovieList } from "pages/MovieList"
+
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+
+    // 0acc5941943be92854a9966e91961a97 //
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MovieList />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    // <div>
+    //   Find me in src/app.js!
+    // </div>
   )
 }
---
+++ code/src/pages/MovieList.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+export const MovieList = () => {
+    const [movies, setMovies] = useState([])
+
+    // useEffect(() => {
+    //     fetch()
+    //         .then((res) => res.json())
+    //         .then((json) => {
+    //         })
+    // }, [])
+
+    return (
+        <div>
+            Movie List
+        </div>
+    )
+}
+
