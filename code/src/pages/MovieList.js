ce97dea54940f07da97e4aedd88d828742c2658a
Petra B <petrabrinck@icloud.com>
2019-12-09 12:30:12 +0100

    added poster path, release dates and fetch works andwe now have poster path date and orig title

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -3,16 +3,23 @@ import React, { useEffect, useState } from 'react'
 export const MovieList = () => {
     const [movies, setMovies] = useState([])
 
-    // useEffect(() => {
-    //     fetch()
-    //         .then((res) => res.json())
-    //         .then((json) => {
-    //         })
-    // }, [])
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=0acc5941943be92854a9966e91961a97&language=en-US&page=1')
+            .then((res) => res.json())
+            .then((json) => {
+                setMovies(json.results)
+            })
+    }, [])
 
     return (
         <div>
-            Movie List
+            {movies.map((movie) => (
+                <div>
+                    <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title} />
+                    <h2>{movie.original_title}</h2>
+                    <h3>{movie.release_date}</h3>
+                </div>
+            ))}
         </div>
     )
 }
