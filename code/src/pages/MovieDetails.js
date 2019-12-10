9ab18b027135dd0d328b366002c014aa86fae171
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-10 10:53:24 +0100

    Update movie details component

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -6,6 +6,7 @@ import styles from './MovieDetails.module.scss';
 export const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState({});
+  let history = useHistory();
 
   useEffect(() => {
     API.getMovie(movieId)
@@ -13,18 +14,20 @@ export const MovieDetails = () => {
         setMovie(data);
       })
       .catch(err => {
-        console.log(err);
+        console.log(err.status);
+        // setMovie({});
       });
   }, [movieId]);
 
-  if (!movie) {
-    return <div>Not found!</div>;
+  if (movie.status_code) {
+    history.push('/');
+    return null;
+  } else {
+    return (
+      <div>
+        Movie title <b>{movie.title}</b>!<br />
+        <Link to="/">Back to the list</Link>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      Movie title <b>{movie.title}</b>!<br />
-      <Link to="/">Back to the list</Link>
-    </div>
-  );
 };
