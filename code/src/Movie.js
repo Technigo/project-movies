ea99bdeaa904800d74e2107b9b68922b9767e9b2
Jenfi <jenniferfick@LTWHYRED39.Whyred.local>
2019-12-10 12:51:56 +0100

    all data for MovieList ready

--- code/src/Movie.js
+++ code/src/Movie.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 // import burgers from 'data/burgers.json'
 
 
 export const Movie = () => {
+  const { movieId } = useParams()
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=2a89f9965a48e33d809fbd966cc6a018&language=en-US`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        // setMovies(json)
+      })
+  }, [movieId])
+
+
   // const params = useParams() "This is a hook to get a value and do what we want with it"... What does that mean?
   // const burgerMatch = burgers.find((burgers) => burger.slug === params.slug)
   // console.log(burgerMatch)
 
   return (
-
     <main>
-      <span>
+      <div>
         <Link to="/">Go back</Link>
-      </span>
-      Här ska det vara en stor background och en mindre bild.
-      <div className="img-container">
-        {/* <Burger name={burgerMatch.name} image={burgerMatch.img} /> */}
+      </div>
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <h3>{movie.overview}</h3>
+          </div>
+        ))
+        }
       </div>
     </main>
   )
 }
+
+
+  //    < div >
+  //    // 
+  //    // </div >
+  //    Här ska det vara en stor background och en mindre bild.
+  //  </div >
+  //  // < div className = "img-container" >
+  //  {/* <Burger name={burgerMatch.name} image={burgerMatch.img} /> */ }
--- code/src/MovieList.js
+++ code/src/MovieList.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-// import burgers from 'data/burgers.json'
 // NavLinlk är användbart ist för Link om jag vill stylea länken för sidan jag är på/visa vilken sida jag är på. Det skapar en class "active" so mjag kan stylea i css -> a.active{}
 
-// för all data: https://api.themoviedb.org/3/movie/popular?api_key=2a89f9965a48e33d809fbd966cc6a018&language=en-US&page=1
+import 'MovieList.css'
+// för all data: 
 
 export const MovieList = () => {
   const api_key = ("2a89f9965a48e33d809fbd966cc6a018")
+  const [movies, setMovies] = useState([])
 
-  fetch(`https://api.themoviedb.org/3/configuration?api_key=${api_key}`)
-    .then((response) => {
-      return response.json()
-    })
-    .then((json) => {
-      console.log(json)
-    }
-    )
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=2a89f9965a48e33d809fbd966cc6a018&language=en-US&page=1`)
+      .then((res) => res.json())
+      .then((json) => {
+        setMovies(json.results)
+      })
+
+  }, [])
+
 
   return (
+    <article>
+      {movies.map((movie) => (
+        <div className="movie-list" key={movie.id}>
+          <Link to={`/movie/${movie.id}`} className="movie-link">
+            {/*  */}
+            <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} />
+            <div className="hover-text">
+              <h1>{movie.original_title}</h1>
+              <h2>{movie.release_date}</h2>
+            </div>
+          </ Link>
+
+        </div>
+      ))}
+    </article>
+
+
+
     // <section className="movies">
     //   {images.map((props) =>(
     //     <Link key={props.results.id} to={`/about/${props.id}`}>
@@ -28,13 +48,13 @@ export const MovieList = () => {
     // }
 
     // </section >
-    <main>
-      Hej hej här är massa bilder fångade genom api och fetch. Men också useState och useEffect?
-    <ul>
-        <li>
-          <Link to="/movie/:movieId">Film 1, länken måste kopplas till API, så att det blir rätt länk</Link>
-        </li>
-      </ul>
-    </main >
+    // <main>
+    //   Hej hej här är massa bilder fångade genom api och fetch. Men också useState och useEffect?
+    // <ul>
+    //     <li>
+    //       <Link to="/movie/:movieId">Film 1, länken måste kopplas till API, så att det blir rätt länk</Link>
+    //     </li>
+    //   </ul>
+    // </main >
   )
 }
