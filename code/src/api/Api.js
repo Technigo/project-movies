f7999b3d8ae02d3028b2aff6376bcae84419ae89
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-11 11:58:54 +0100

    Update error handling

--- code/src/api/Api.js
+++ code/src/api/Api.js
@@ -5,25 +5,29 @@ export const getMovies = async () => {
     `https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1`
   );
 
-  if (response.status !== 200) {
-    throw Error(`Request rejected with status ${response.status}`);
-  } else {
+  if (response.status === 200) {
     const data = await response.json();
-
     return data;
+  } else {
+    throw Error(`Request rejected with status ${response.status}`);
   }
 };
 
 export const getMovie = async movieId => {
   console.log(movieId);
+
+  if (isNaN(movieId)) {
+    throw Error(`Request ID rejected because it's not a number`);
+  }
+
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIKEY}&language=en-US`
   );
 
-  if (response.status !== 200) {
-    throw Error(`Request rejected with status ${response.status}`);
-  } else {
+  if (response.status === 200) {
     const data = await response.json();
     return data;
+  } else {
+    throw Error(`Request rejected with status ${response.status}`);
   }
 };
