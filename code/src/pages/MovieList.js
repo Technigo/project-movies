3f05f09c4a0aad46850c11d4bcd11974f5a4b455
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-11 10:48:25 +0100

    Implement progress bar

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import TopBarProgress from 'react-topbar-progress-indicator';
 import * as API from '../api/Api';
 import styles from './MovieList.module.scss';
-// import TopBarProgress from 'react-topbar-progress-indicator';
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -20,41 +20,50 @@ export const MovieList = () => {
       });
   }, []);
 
+  TopBarProgress.config({
+    barColors: {
+      '0': '#ff0000',
+      '1.0': '#ff0000'
+    },
+    shadowBlur: 5,
+    barThickness: 2
+  });
+
   return (
-    <div className={styles.movieList}>
-      {movies
-        .filter(movie => {
-          return (
-            movie.id !== 540247 &&
-            movie.id !== 449924 &&
-            movie.id !== 474350 &&
-            movie.id !== 492188
-          );
-        })
-        .map(movie => (
-          <Link key={movie.id} to={`/movie/${movie.id}`}>
-            <div className={styles.movie}>
-              <img
-                src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
-                alt="poster"
-              />
-              <div className={styles.overlay}>
-                <div className={styles.movieDetails}>
-                  <h1>{movie.title}</h1>
-                  <p>Released: {movie.release_date}</p>
+    <div>
+      {isLoading ? (
+        <div>
+          <TopBarProgress />
+        </div>
+      ) : (
+        <div className={styles.movieList}>
+          {movies
+            .filter(movie => {
+              return (
+                movie.id !== 540247 &&
+                movie.id !== 449924 &&
+                movie.id !== 474350 &&
+                movie.id !== 492188
+              );
+            })
+            .map(movie => (
+              <Link key={movie.id} to={`/movie/${movie.id}`}>
+                <div className={styles.movie}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+                    alt="poster"
+                  />
+                  <div className={styles.overlay}>
+                    <div className={styles.movieDetails}>
+                      <h1>{movie.title}</h1>
+                      <p>Released: {movie.release_date}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </Link>
-        ))}
+              </Link>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
-
-// style={{
-//   backgroundImage: `url(https://image.tmdb.org/t/p/w780${movie.poster_path})`
-// }}
-
-{
-  /* <img src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt="backdrop image" /> */
-}
