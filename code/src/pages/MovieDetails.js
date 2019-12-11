ab5e8c33093121e20670b175187633300eeac8f9
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-11 10:40:14 +0100

    Implement progress bar

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useHistory, Link } from 'react-router-dom';
+import TopBarProgress from 'react-topbar-progress-indicator';
 import * as API from '../api/Api';
 import styles from './MovieDetails.module.scss';
 
@@ -7,13 +8,16 @@ export const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState({});
   const [errorLoading, SetErrorLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
     console.log(movieId);
+    setIsLoading(true);
     API.getMovie(movieId)
       .then(data => {
         setMovie(data);
+        setIsLoading(false);
         SetErrorLoading(false);
       })
       .catch(err => {
@@ -22,23 +26,40 @@ export const MovieDetails = () => {
       });
   }, [movieId]);
 
+  TopBarProgress.config({
+    barColors: {
+      '0': '#ff0000',
+      '1.0': '#ff0000'
+    },
+    shadowBlur: 5,
+    barThickness: 2
+  });
+
   if (errorLoading) {
     history.push('/');
   }
 
   return (
-    <div>
-      {errorLoading === false && (
-        <div
-          className={styles.movieDetails}
-          style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`
-          }}
-        >
-          <div className={styles.overlay}>
-            <p>Movie title: {movie.title}</p>
-            <Link to="/">Back</Link>
-          </div>
+    <div className="app-container">
+      {isLoading ? (
+        <div>
+          <TopBarProgress />
+        </div>
+      ) : (
+        <div>
+          {errorLoading === false && (
+            <div
+              className={styles.movieDetails}
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`
+              }}
+            >
+              <div className={styles.overlay}>
+                <p>Movie title: {movie.title}</p>
+                <Link to="/">Back</Link>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
