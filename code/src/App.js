1442e98813fe2794093d5f4cf17e13beb15f6965
Emmi Nilsson <emmie.nilsson@gmail.com>
2019-12-11 14:18:12 +0100

    Changed name of movie list component and structure for it, moved components into new folder.

--- code/src/App.js
+++ code/src/App.js
@@ -1,42 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
-import { MovieCard } from './list/MovieCard'
-import { MovieDetails } from './detail/MovieDetails'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { MovieList } from './components/MovieList'
+import { MovieDetails } from './components/MovieDetails'
 
 export const App = () => {
-  const [movies, setMovies] = useState([])
-  // Fetch data from movie database
-  useEffect(() => {
-    fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=1a381f2cdba45c6f12c9451edcfa9c50&language=en-US&page=1")
-      .then(response => response.json())
-      .then(json => setMovies(json.results))
-  }, [])
 
   return (
     <main>
-      <BrowserRouter>
+      <Router>
         <Switch>
           <Route path="/" exact>
-            <section className="movie-list">
-              {movies.map((movie) => {
-                return (
-                  <Link to={`/movies/${movie.id}`} key={movie.id}>
-                    <MovieCard
-                      title={movie.title}
-                      date={movie.release_date}
-                      poster={movie.poster_path} />
-                  </Link>
-                )
-              })}
-            </section>
+            <MovieList />
           </Route>
           <Route path="/movies/:movieId">
-            <section className="movie-overview">
-              <MovieDetails />
-            </section>
+            <MovieDetails />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </main>
   )
 }
---
+++ code/src/components/MovieList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './movieList.css'
+
+export const MovieList = (props) => {
+  const [movies, setMovies] = useState([])
+  // Fetch data from movie database
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=1a381f2cdba45c6f12c9451edcfa9c50&language=en-US&page=1")
+      .then(response => response.json())
+      .then(json => setMovies(json.results))
+  }, [])
+
+  return (
+    <section className="movie-list">
+      {movies.map((movie) => {
+        return (
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <article className="movie" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w342${movie.poster_path})` }} >
+              <h2 className="movie-title">{movie.title}</h2>
+              <p className="movie-release-date">Release date: {movie.release_date}</p>
+            </article>
+          </Link>
+        )
+      })}
+    </section>
+  )
+}
