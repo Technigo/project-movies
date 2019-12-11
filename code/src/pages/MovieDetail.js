75f2b9c956732393ccf978dc87653ded27d5af23
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2019-12-11 12:48:18 +0100

    Added styling to details-page

--- code/src/pages/MovieDetail.js
+++ code/src/pages/MovieDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
+import { ReactComponent as Arrow } from "./icons/arrow.svg"
 import "./css/MovieDetail.css"
 
 const apiKey = "0a7bdc5f7b44e6a5230c95a3dbb9bbbc"
@@ -19,15 +20,25 @@ export const MovieDetail = () => {
 
 
   return (
-    <div className="details" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
-      <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title}></img>
 
-      <h1>{movie.original_title}</h1>
-      <br></br>
-      <h2>{movie.vote_average}/10</h2>
-      <br></br>
-      <p>{movie.overview}</p>
+    <div className="background" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
 
+      <div className="back-button">
+        <Link to={"/"}>
+          <Arrow />
+          <p>Movies</p>
+        </Link>
+      </div>
+      <div className="details">
+        <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title}></img>
+        <div className="text-details">
+          <h2>{movie.original_title}</h2>
+          <h3>{movie.vote_average}/10</h3>
+          <br></br>
+          <p>{movie.overview}</p>
+        </div>
+
+      </div>
     </div>
   )
 
