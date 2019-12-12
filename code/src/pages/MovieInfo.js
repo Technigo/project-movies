b4a52450e279224358ab5df453477c00bbad62a6
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-12 12:49:48 +0100

    movie info in place - makes me happy

--- code/src/pages/MovieInfo.js
+++ code/src/pages/MovieInfo.js
@@ -17,19 +17,19 @@ export const MovieInfo = () => {
 
     return(
         <section className="infoContainer">
-            {/*background picture for the site*/}
-            <img className="backDrop" src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={movie.original_title} />
-             {/*contains poster, text about movie, title and rating*/}
-            <div className="aboutContainer">
-                <div className="infoPoster">             
-                    <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title} />
-                </div> 
+            {/*backDrop background picture with CSS style in react for the site*/}
+            < div className="backDrop" style={{backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`}} alt={movie.original_title} />
+             {/*infoBox contains poster, text about movie, title and rating*/}
+            <div className="infoBox">    
+                <img className="infoPoster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title} /> 
+                {/*titleInfo contains title and rating to make them flex in css*/} 
                 <div className="titleInfo">   
-                         {/*contains title and rating*/} 
-                        <h1 className="movieTitle">{movie.original_title}
-                            <p className="movieVotes">{movie.vote_average}/10</p>
-                        </h1>
-                        <p>{movie.overview}</p>
+                    <h1 className="movieTitle">{movie.original_title}
+                        <p className="movieVotes">{movie.vote_average}/10</p>
+                    </h1>
+                    <p>{movie.overview}</p>
+                 
+                
                 </div>  
             </div>
         </section> 
