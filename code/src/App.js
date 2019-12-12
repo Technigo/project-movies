77e2534d709bee8e5842a32046cf59d5c5e1f4ad
Jens_Nilsen <jensnilsen89@gmail.com>
2019-12-12 23:02:16 +0100

    project set up with npm and router

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+  return <div>API key: c616ef82836e90a0073b3a17b8bde4banpm</div>;
+};
