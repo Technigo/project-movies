18f225d12c2d6cf3060beb9d6b5a3822fb5e9e17
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-12 14:23:32 +0100

    Moved link on MovieList.js to surround all info

--- code/src/App.js
+++ code/src/App.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MovieList } from "pages/MovieList"
 import { MovieInfo } from "pages/MovieInfo"
- 
-
-//const apiKey 3812b9925d12c2723ac148f3607b8bb5
 
 export const App = () => {
   return (
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -18,11 +18,12 @@ useEffect(() => {
         <div className="movieContainer">
             {movies.map((movie) => (
                 <div className="moviePoster" key={movie.id}>
+                    <Link to={`/movies/${movie.id}`}>
                     <div className="titleRelease">
                         <h1>{movie.original_title}</h1>
                         <p>Released {movie.release_date}</p>
                     </div>
-                    <Link to={`/movies/${movie.id}`}>
+                    
                         <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title} />  
                     </Link>
                 </div>
