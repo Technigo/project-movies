c60b71c9301606b95acd669d0abc59f52d4d11bf
SanSjo <susanna.sjoberg@gmail.com>
2019-12-12 11:40:16 +0100

    add poster image and summary text on detailpage.js and add home button with arrow icon and the styling for homebutton

--- code/src/pages/DetailPage.js
+++ code/src/pages/DetailPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import './DetailPage.css'
 
 export const DetailPage = () => {
   const api_key = 'dcb0caab506cac37c3f7dc479ca8aee2'
@@ -15,9 +16,28 @@ export const DetailPage = () => {
       })
   }, [movieId])
   return (
-    <div>
-      <img src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`} alt={details.backdrop_path} />
+    <article className="detail-page-container">
+      <Link className="home-button" to="/">
+        <i class="fas fa-chevron-circle-left"></i> <p className="home">Movies</p>
+      </Link>
+      <div className="backdrop-poster" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${details.backdrop_path})` }}>
+        <div className="movie-overview">
+          <img className="poster" src={`https://image.tmdb.org/t/p/w342${details.poster_path}`} alt={details.poster_path} />
 
-    </div>
+          <div className="movie-summary">
+            <h2>{details.title} <span className="vote">{details.vote_average}/10</span></h2>
+            <p>{details.overview}</p>
+          </div>
+        </div>
+
+      </div>
+    </article >
+
+
+
   )
 }
+
+/*
+
+*/
