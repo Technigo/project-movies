89aed298aace516904f3194b393e6bae2d4e6842
ELIN OLSSON <elinolsson@ELINs-MacBook-Air.local>
2019-12-13 15:47:31 +0100

    new funtions

--- code/src/components/Movies.js
+++ code/src/components/Movies.js
@@ -17,18 +17,16 @@ export const Movies = () => {
     return (
         <section className="movie-section">
             {movies.map((movie) => (
-                <div key={movie.id}>
-                    <div className="movie-info">
+
+             <Link key={movie.id} to={`/movies/${movie.id}`}>        
+                <div className="movie-info">
                      <h1>{movie.title}</h1>
                      <h2>Released {movie.release_date}</h2>
                      </div>
                      <div className="movie-poster">
-                         <Link to={`/movies/${movie.id}`}>
                     <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title} />
-                    </Link>
                     </div>
-                </div>
-                
+                    </Link>
             ))}
         </section>
     )
