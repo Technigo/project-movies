68c255b62b462a7b3d60ed3ada39528ef5919b68
Petra B <petrabrinck@icloud.com>
2019-12-13 15:45:57 +0100

    btn on second page is working with animation. I will do some more css

--- code/src/pages/MovieInfo.js
+++ code/src/pages/MovieInfo.js
@@ -1,50 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-// import { backIcon } from 'icons/Back'
+import { Link } from 'react-router-dom'
 import "./movieInfo.css"
-
-
 
 export const MovieInfo = () => {
     const { movieId } = useParams()
     const [movie, setMovie] = useState([])
-    // const { BackIcon } = () =>
-
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=0acc5941943be92854a9966e91961a97&language=en-US`)
             .then((res) => res.json())
             .then((json) => {
                 setMovie(json)
-                console.log(json)
+                // console.log(json)
             })
     }, [movieId])
 
+    if (!movie) {
+        return <div>Ups, need a moment...</div>
+    }
+
     return (
-
         <section className="infoContainer">
+            <Link className="backLink" to={'/'}>
+                <svg className="backLinkImg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                    <path d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z" fill="#fff" fillRule="evenodd">
+                    </path></svg>
+                <p>Movies</p>
+            </Link>
 
-
-            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-        <path d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z" fill="#fff" fillRule="evenodd" />
-        </svg> */}
-
-
-
-            {/* poster, text about the movie, title and ratings */}
-
-            {/* <section className="detailsContainer"> */}
-            <img className="backdrop" src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt="movie.backdrop_path" />
+            <div className="backdrop" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }} alt="movie.original_title" />
             {/* background image */}
-            <div className="aboutContainer">
-                <div className="infoPoster">
-                    <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie.poster_path" />
-                </div>
+            <div className="infoBox">
+                <img className="infoPoster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie.original_title" />
+
                 <div className="titleInfo">
-                    {/* title and ratings */}
                     <h1 className="movieTitle">{movie.original_title}</h1>
                     <p className="movieVotes">{movie.vote_average}/10</p>
-                    <p className="movieInfo">{movie.overview}</p>
+
+                    <p className="movie.overview">{movie.overview}</p>
                 </div>
             </div>
         </section>
