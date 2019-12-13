8e2b02bdf2220046d3f3e51e5bc65583b85f714e
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2019-12-13 09:47:46 +0100

    set up done with API, npm ist.

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+/* https://api.themoviedb.org/3/movie/550?api_key=0774e20c1c8bfc2f79af713d52e9832d
+
+https://api.themoviedb.org/3/movie/popular?api_key=0774e20c1c8bfc2f79af713d52e9832d&language=en-US&page=1
+
+https://api.themoviedb.org/3/movie/{movie_id}?api_key=0774e20c1c8bfc2f79af713d52e9832d&language=en-US
+
+"backdrop_path": "/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg",
+ex : `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
+https://api.themoviedb.org/3/configuration?api_key=0774e20c1c8bfc2f79af713d52e9832d
+ */
+
+
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  return ( <
+    div >
+    Find me in src / app.js!
+    <
+    /div>
   )
 }
