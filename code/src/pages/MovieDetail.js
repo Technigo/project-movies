4c8ab41450e560c50b2ea09cd697980ce30910bc
Johanna Luks Wærn <johanna@misswaern.se>
2019-12-14 13:07:50 +0100

    updates

--- code/src/pages/MovieDetail.js
+++ code/src/pages/MovieDetail.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
+import "./movieDetail.css"
+import { BackArrow } from "../icons/BackArrow.js"
 
 export const MovieDetail = () => {
   const [movie, setMovie] = useState()
-  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState(true)
   const { id } = useParams()
 
+  const api_key = "9d205e0f5fff6f5614be3be92179fc10"
+
   useEffect(() => {
-    setLoading(true)
     fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=9d205e0f5fff6f5614be3be92179fc10&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US&page=1`
     )
       .then(res => res.json())
       .then(json => {
-        setMovie(json)
+        if (json.status_code === 34) {
+          setError("movie not found")
+        } else {
+          setMovie(json)
+        }
         setLoading(false)
       })
   }, [id])
@@ -22,9 +30,41 @@ export const MovieDetail = () => {
     return <h1>LOADING</h1>
   }
 
-  if (!movie) {
-    return <h1>no movie</h1>
+  if (error) {
+    return <h1>{error}</h1>
   }
 
-  return <div>{movie && <div>{movie.title}</div>}</div>
+  return (
+    <div
+      className='hero'
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`
+      }}
+    >
+      <div className='movieInfo'>
+        <img
+          src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+          style={{ border: "solid white 3px" }}
+        />
+        <div className='movieDetail'>
+          <div className='heading'>
+            <h1>{movie.title}</h1>
+            <h2>Rating {movie.vote_average}/10</h2>
+          </div>
+          <p>{movie.overview}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+//copy the svg link and paste
+//make a folder called icon
+//a js-file "backArrow" - paste all code from svg here
+//import
+//remember that - is not allowed in jsx, so go through the svg text
+
+/* <Link to='/' className='backLink'>
+{BackArrow}
+</Link>
+<h1>{movie.title}</h1> */
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -5,9 +5,11 @@ import "./movieList.css"
 export const MovieList = () => {
   const [movies, setMovies] = useState([])
 
+  const api_key = "9d205e0f5fff6f5614be3be92179fc10"
+
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=9d205e0f5fff6f5614be3be92179fc10&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
     )
       .then(res => res.json())
       .then(json => setMovies(json.results))
@@ -26,4 +28,11 @@ export const MovieList = () => {
       ))}
     </div>
   )
+}
+
+{
+  /* <select value={category} onChange={(e) => setCategory(e.target.value)}> 
+        <option value='popular'>Popular</option>
+        <option value='top_rated'>Top Rated</option>
+      </select> */
 }
