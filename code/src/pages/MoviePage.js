68a028cfa490714f2d65d563d6ee338f330848f9
Disa de Beau <Disan@Disas-MacBook-Air.local>
2019-12-15 22:37:43 +0100

    added stretch goal

--- code/src/pages/MoviePage.js
+++ code/src/pages/MoviePage.js
@@ -4,32 +4,43 @@ import "./MoviePage.css"
 
 export const MoviePage = () => {
   const [movies, setMovies] = useState([])
+  const [category, setCategory] = useState("popular")
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=2afda563012307ae2aead8ab351056e0&language=en-US&page=1`)
+    fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=2afda563012307ae2aead8ab351056e0&language=en-US&page=1`)
       .then((res) => res.json())
       .then((json) => {
         setMovies(json.results)
       })
-  }, [])
+  }, [category])
 
   console.log(movies)
 
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <Link key={movie.id} to={`/movies/${movie.id}`}>
-          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie" />
 
-          <article className="title">
-            <h2>{movie.title}</h2>
-            <h3>released {movie.release_date}</h3>
-          </article>
+    <div>
+      <h4>Select Category:</h4>
+      <select className="drop-down" value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="now_playing">Now playing</option>
+        <option value="top_rated">Top Rated</option>
+        <option value="upcoming">Upcoming</option>
+      </select>
 
-        </Link>
-      ))
-      }
-    </div >
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie" />
+
+            <article className="title">
+              <h2>{movie.title}</h2>
+              <h3>released {movie.release_date}</h3>
+            </article>
+
+          </Link>
+        ))
+        }
+      </div >
+    </div>
   )
 
 
