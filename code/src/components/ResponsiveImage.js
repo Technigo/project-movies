b47db679cf487eaa280f8ddf9691f6c099c183f3
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-15 13:42:35 +0100

    Update structure and logic

---
+++ code/src/components/ResponsiveImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const ResponsiveImage = ({picture_path}) => {
+  const w92;
+  const w154;
+  const w185;
+  const w342;
+  const w500;
+  const w2000;
+
+
+  return (
+    <img src=""
+  )
+}
