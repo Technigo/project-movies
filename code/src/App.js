06b92567a35178177df13d46168293a8ffd8e3ec
Tiago Ferreira <tiagoferreira@tuta.io>
2019-12-15 16:33:04 +0100

    Changing 404 netlify

--- code/src/App.js
+++ code/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { MovieList } from './pages/MovieList'
 import { MovieDetails } from './components/MovieDetails'
 import { ProdCompanies } from './pages/ProdCompanies'
+import { NotFound } from 'pages/NotFound'
 
 export const App = () => {
   return (
@@ -16,6 +17,9 @@ export const App = () => {
         </Route>
         <Route path='/companies/:id'>
           <ProdCompanies />
+        </Route>
+        <Route path='/404'>
+          <NotFound />
         </Route>
       </Switch>
     </Router>
