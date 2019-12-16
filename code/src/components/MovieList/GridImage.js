53ddf2d7a405b0c118af72b124b1e304a148e3e9
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-16 16:18:11 +0100

    Update logic

--- code/src/components/MovieList/GridImage.js
+++ code/src/components/MovieList/GridImage.js
@@ -19,13 +19,13 @@ export const GridImage = ({ imagePath, gridCol }) => {
 
   if (gridItemWidth < 2000 && gridItemWidth > 780) {
     url = `https://image.tmdb.org/t/p/original${imagePath}`;
-    // console.log('Image: 780px');
+    // console.log('Image: 2000px');
   } else if (gridItemWidth <= 780 && gridItemWidth > 500) {
     url = `https://image.tmdb.org/t/p/w780${imagePath}`;
-    // console.log('Image: 500px');
+    // console.log('Image: 768px');
   } else if (gridItemWidth <= 500 && gridItemWidth > 342) {
     url = `https://image.tmdb.org/t/p/w500${imagePath}`;
-    // console.log('Image: 342px');
+    // console.log('Image: 500x');
   } else if (gridItemWidth <= 342 && gridItemWidth > 185) {
     url = `https://image.tmdb.org/t/p/w342${imagePath}`;
     // console.log('Image: 342px');
@@ -33,20 +33,6 @@ export const GridImage = ({ imagePath, gridCol }) => {
     url = `https://image.tmdb.org/t/p/w185${imagePath}`;
     // console.log('Image: 185px');
   }
-
-  // if (width / columns > 2000) {
-  //   url = `https://image.tmdb.org/t/p/w780${imagePath}`;
-  //   // console.log('Image: 780px');
-  // } else if (width / columns < 2000 && width > 342) {
-  //   url = `https://image.tmdb.org/t/p/w500${imagePath}`;
-  //   // console.log('Image: 500px');
-  // } else if (width / columns < 342 && width > 185) {
-  //   url = `https://image.tmdb.org/t/p/w342${imagePath}`;
-  //   // console.log('Image: 342px');
-  // } else {
-  //   url = `https://image.tmdb.org/t/p/w185${imagePath}`;
-  //   // console.log('Image: 185px');
-  // }
 
   return (
     <div>
