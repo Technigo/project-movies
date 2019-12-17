69a9b4503caa98537f3d08260474a721f3acb491
Jens_Nilsen <jensnilsen89@gmail.com>
2019-12-17 21:05:59 +0100

    project movie night is done. made a couple of stretch goals, also added back buttons on error and if stuck on loading. overall happy.

--- code/src/Pages/MovieDetails.js
+++ code/src/Pages/MovieDetails.js
@@ -1,45 +1,79 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import "Css/movieDetails.css";
+import { BackButton } from "Icons/BackButton";
 
 const apiKey = "c616ef82836e90a0073b3a17b8bde4ba";
 
 export const MovieDetails = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    setLoading(true);
     fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
     )
       .then(res => res.json())
       .then(json => {
-        setMovie(json);
+        if (json.status_code === 34) {
+          setError("movie not found");
+        } else {
+          setMovie(json);
+        }
+
         setLoading(false);
         console.log(json);
       });
-  }, []);
+  }, [id]);
 
-  if (loading) return <h1>Loading...</h1>;
-  if (!movie) return <h1>Cant Find Your Movie</h1>;
+  if (loading)
+    return (
+      <div>
+        <Link className="backButton" to="/">
+          <BackButton /> <p>Back</p>
+        </Link>
+        <h1 className="loading">Loading...</h1>
+      </div>
+    );
+  if (error)
+    return (
+      <div>
+        <Link className="backButton" to="/">
+          <BackButton /> <p>Back</p>
+        </Link>
+        <h1 className="sorry">Sorry! Cant Find Your Movie</h1>
+      </div>
+    );
 
   return (
-    <article>
+    <section>
+      <Link className="backButton" to="/">
+        <BackButton /> <p>Back</p>
+      </Link>
       {movie && (
-        <div
+        <article
           className="background"
           style={{
             backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`
           }}
         >
-          <div>
-            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} />
+          <div className="summary">
+            <img
+              alt="poster"
+              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+            />
+            <div className="details">
+              <h1>
+                {movie.title}
+                <span className="rating">{movie.vote_average}/10</span>
+              </h1>
+              <p>{movie.overview}</p>
+            </div>
           </div>
-          <h1>{movie.title}</h1>
-        </div>
+        </article>
       )}
-    </article>
+    </section>
   );
 };
--- code/src/Pages/MovieList.js
+++ code/src/Pages/MovieList.js
@@ -6,29 +6,45 @@ const apiKey = "c616ef82836e90a0073b3a17b8bde4ba";
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [category, setCategory] = useState("popular");
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&language=en-US&page=1`
     )
       .then(res => res.json())
       .then(json => {
         console.log(json);
         setMovies(json.results);
       });
-  }, []);
+  }, [category]);
 
   return (
-    <section className="movieList">
-      {movies.map(movie => (
-        <Link key={movie.id} to={`/movies/${movie.id}`}>
-          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} />
-          <div className="listDetails">
-            <h1>{movie.title}</h1>
-            <p>Released {movie.release_date}</p>
-          </div>
-        </Link>
-      ))}
+    <section className="listPage">
+      <header>
+        <h1>Movie Night</h1>
+        <select value={category} onChange={e => setCategory(e.target.value)}>
+          <option value="popular">Popular</option>
+          <option value="top_rated">Top Rated</option>
+          <option value="upcoming">Upcoming</option>
+          <option value="now_playing">Now on Cinema</option>
+        </select>
+      </header>
+      <section className="movieList">
+        {movies.map(movie => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <img
+              alt="movieCard"
+              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+            />
+            <div className="listDetails">
+              <h1>{movie.title}</h1>
+              <span className="rating">{movie.vote_average}/10</span>
+              <p>Released {movie.release_date}</p>
+            </div>
+          </Link>
+        ))}
+      </section>
     </section>
   );
 };
