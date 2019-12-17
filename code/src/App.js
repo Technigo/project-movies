9c07e5de73fcc071ec012e7c2ca84f0ceb72b1d1
Jens_Nilsen <jensnilsen89@gmail.com>
2019-12-17 16:34:01 +0100

    Styiling added for the homepage

--- code/src/App.js
+++ code/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import { MovieList } from "./Pages/MovieList"
-import { MovieDetails } from "./Pages/MovieDetails"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MovieList } from "./Pages/MovieList";
+import { MovieDetails } from "./Pages/MovieDetails";
 
 export const App = () => {
   return (
     <Router>
-      <Route path='/' exact>
+      <Route path="/" exact>
         <MovieList />
       </Route>
-      <Route path='/movies/:id' exact>
+      <Route path="/movies/:id" exact>
         <MovieDetails />
       </Route>
     </Router>
-  )
+  );
 };
--- code/src/Pages/MovieList.js
+++ code/src/Pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "Css/movieList.css";
 
 const apiKey = "c616ef82836e90a0073b3a17b8bde4ba";
 
@@ -18,13 +19,16 @@ export const MovieList = () => {
   }, []);
 
   return (
-    <div>
+    <section className="movieList">
       {movies.map(movie => (
         <Link key={movie.id} to={`/movies/${movie.id}`}>
           <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} />
-          <h1>{movie.title}</h1>
+          <div className="listDetails">
+            <h1>{movie.title}</h1>
+            <p>Released {movie.release_date}</p>
+          </div>
         </Link>
       ))}
-    </div>
+    </section>
   );
 };
