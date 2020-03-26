f5a424e664f9934712f27368494f6684a6252cd3
annesophiegendron <annesophie.gendron@gmail.com>
2020-03-26 16:05:54 +0100

    loading function added

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Replace this readme with your own information about your project.
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
-## The problem
+## Learnings
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+- How to use React router to create multi-page applications
+- How to use placeholders in urls to pick dynamic parts from urls
+- Practice using APIs in React - combining `useState` with `useEffect`
 
 ## View it live
 
--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 export const MovieDetails = () => {
   const api_key = "67a5c607e91b1a60e64b4e131b88956b";
   const [movieDetails, setMovieDetails] = useState([]);
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const useHistory = useHistory();
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US`)
@@ -13,8 +15,19 @@ export const MovieDetails = () => {
       .then((json) => {
         console.log(json)
         setMovieDetails(json)
+        setLoading(false)
       })
   }, [id])
+
+  if (loading) {
+    return (
+      <h3>Loading...</h3>
+    )
+  }
+
+  if (!moviesDetails) {
+    history.push('/')
+  }
 
   return (
     <section>
