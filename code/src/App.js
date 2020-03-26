bcf5aa81c520becf731ac1f8968b5f6f2de85e92
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-03-26 15:37:29 +0100

    img titel shows up on the first page, and the step to page 2 works

--- code/src/App.js
+++ code/src/App.js
@@ -3,11 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MovieList } from "components/MovieList"
 import { MovieB } from "components/MovieB"
 
-//const api_key = 55cd0e0e418083e581aec6766c6a1677
-
-
-//https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page=1
-
 export const App = () => {
   return (
     <BrowserRouter>
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -3,16 +3,40 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=55cd0e0e418083e581aec6766c6a1677&language=en-US&page=1')
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json.results)
+            setMovies(json.results)
+        })
+}, [])
 
   return (
 
-    <section className="movieContainer">
+    <section 
+      
+      className="movieContainer">
+            {movies.map((movie) => (
 
-      <Link to={'/Order Now'}>
-      <p> Hejhej</p>
-      </Link>
+          <div 
+            
+          className="movieImg" key={movie.id}>
+
+          <img src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} /*alt={movie.original_title}*/ />
+          <h1>{movie.original_title}</h1>
+          
+
+          <Link to={'/Order Now'}>
+            <h1>{movie.original_language}</h1>
+          </Link>
 
 
-    </section>
+          </div>
+                
+            ))}
+        </section>
   )
 }
