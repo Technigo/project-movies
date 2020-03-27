b21137b463be07266e12efa4f2349c18763f5475
bealun <53275597+bealun@users.noreply.github.com>
2020-03-27 21:16:48 +0100

    styled back button

--- code/src/components/Info.js
+++ code/src/components/Info.js
@@ -22,15 +22,18 @@ export const DetailedInfo = () => {
     backgroundRepeat: "no-repeat"
   };
 
+{/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm-1-17v4h8v2h-8v4l-6-5 6-5z"/></svg> */}
+
   return (
     <section className="eachMovie" style={detailStyle}>
       <div className="darkenBackground">
         <div className="goBack">
           <Link to="/">
-            <span role="img" aria-label="Back">
+            {/* <span role="img" aria-label="Back">
             {" ⬅️ "}
-            </span>
-            Go Back
+            </span> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#fff"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm-1-17v4h8v2h-8v4l-6-5 6-5z"/></svg>
+            <span>Back</span>
         </Link>
         </div>
 
