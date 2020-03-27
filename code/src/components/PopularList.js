d051684bd91ebc89b8ecef8fcd67140b24126fac
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-03-27 16:47:30 +0100

    added img x2 content on secondpart

--- code/src/components/PopularList.js
+++ code/src/components/PopularList.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 export const PopularList = () => {
   const { movieId } = useParams()
-  //const [movie, setMovie] = useState([])
+  const [movie, setMovie] = useState([])
 
 
   useEffect(() => {
@@ -13,7 +13,7 @@ export const PopularList = () => {
         .then((res) => res.json())
         .then((json) => {
           console.log(json)
-            //setMovie(json)
+            setMovie(json)
         })
 }, [movieId])
 
@@ -25,6 +25,28 @@ export const PopularList = () => {
       <Link 
         
         className="backLink" to={'/'}></Link>
+
+        <div 
+          
+          className="background" ></div>
+
+        <div 
+          
+          className="summary"style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }} alt="movie.original_title" >
+        
+        
+          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie.original_title" />
+
+          {movie.original_title}
+          {movie.vote_average}
+          {movie.overview}
+
+          </div>
+
+        
+
+
+      
 
 
 
