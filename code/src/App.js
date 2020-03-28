3b604b2077494120da90018aefafa98173328ff1
Josephine <josephine@Josephines-Air.lan>
2020-03-28 10:10:38 +0100

    prints out poster images of all movies

--- code/src/App.js
+++ code/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react'
-// import { BrowserRouter, Switch, Router } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MovieList } from 'pages/MovieList'
 
-export const App = (props) => {
-  const { popularMovies, setPopularMovies } = props
+export const App = () => {
   return (
-    // <BrowserRouter>
-    //   <main>
-    //     <Switch>
-    //       <Router exact path="/">
-    <MovieList popularMovies={popularMovies} setPopularMovies={setPopularMovies} />
-    //       </Router>
-    //       Find me in src/app.js!
-    //     </Switch>
-    //   </main>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -3,22 +3,26 @@ import React, { useState, useEffect } from 'react'
 const API = 'https://api.themoviedb.org/3/movie/popular?api_key=83328e75221cc6b9f332da9a39a408f8&language=en-US&page=1'
 
 export const MovieList = () => {
-  // const [popularMovies, setPopularMovies] = useState('')
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     fetch(API)
       .then((res) => res.json())
-      .then((json) => console.log(json.results[0].title))
+      .then((json) => {
+        setMovies(json.results)
+      })
   }, [])
 
   return (
-    <section>
-      {/* {popularMovies.map((movie) => (
-         <div className="movie-cards">
-           key={movie.id}
-             popularMovies={popularMovies}
-
-        </div>))} */}
-    </section>
+    <div>
+      <div className="movielist">
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie poster" />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
+
