6d9a3247f852de42c6395969c8cb2dfbfd2a3ad3
Frida Eriksson <Frida.Eriksson@silf.se>
2020-03-28 22:49:01 +0100

    updated content and styling of About page

--- code/src/pages/About.js
+++ code/src/pages/About.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './about.css'
+import { Nav } from 'components/Nav'
 
 export const About = () => {
   return (
-    <div>Blabla</div>
+    <>
+      <Nav />
+      <div className="about-wrapper">
+        <section className="about-container">
+          <h2 className="about-title">Project Movies</h2>
+          <p className="about-text">This site is a multi-page React application made during the Technigo frontend boot camp. The application is based on the Movie Database API and two lists made by me for this project.</p>
+        </section>
+      </div>
+    </>
   )
 }
