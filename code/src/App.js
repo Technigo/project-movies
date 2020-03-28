6ba6ce176bd541436edc6df3141c352c5787a466
ebbabw <ebbabergholtz@gmail.com>
2020-03-28 21:41:40 +0100

    first draft

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { MoviesList } from './Pages/MoviesList'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+    
+    <BrowserRouter>
+
+    <main>
+
+      <Switch>
+      <Route path="/" exact>
+      <MoviesList />
+      </Route>
+      </Switch>
+
+    </main>
+
+    </BrowserRouter>
+
     </div>
   )
 }
