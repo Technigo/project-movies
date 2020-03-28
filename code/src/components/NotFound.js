139259e7315814c6460b2a581af1edd0e7d7e191
Frida Eriksson <Frida.Eriksson@silf.se>
2020-03-28 20:24:04 +0100

    styled NotFound component

--- code/src/components/NotFound.js
+++ code/src/components/NotFound.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { LeftArrow } from './LeftArrow'
+import './notfound.css'
 
 export const NotFound = () => {
   return (
-    <div className="not-found">
-      <h2>Is it too late now to say sorry?</h2>
-      <p>Look's like we're missing more than just our body.</p>
-      <Link to="/">
-        <button type="button" className="homepage-button"><LeftArrow />Back to homepage</button>
-      </Link>
+    <div className="not-found-wrapper">
+      <div className="not-found-container">
+        <h2 className="not-found-title">Is it too late now to say sorry?</h2>
+        <p className="not-found-text">Look's like we're missing more than just our body.</p>
+        <Link to="/">
+          <button type="button" className="homepage-button">
+            <LeftArrow />
+            <span className="homepage-button-text">Back to homepage</span>
+          </button>
+        </Link>
+      </div>
     </div>
   )
 
