df3bca7c21e6675437d8f9bec74fb6b3209f8066
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-03-29 19:41:45 +0200

    added pseudo elements for movielisting

--- code/src/components/Movies.js
+++ code/src/components/Movies.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Movies = (props) => {
   const { poster_path, id, title, vote_average } = props
@@ -8,8 +7,10 @@ export const Movies = (props) => {
   return (
     <>
       <NavLink to={`/movies/${id}`}><img src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt="" /></NavLink>
-      <NavLink to={`/movies/${id}`}><h2>{title}</h2></NavLink>
-      <h3>{vote_average}/10 <FontAwesomeIcon icon={['far', 'star']} /></h3>
+      <div className="movie-title-container">
+        <h2>{title}</h2>
+        <h3>{vote_average}/10 </h3>
+      </div>
     </>
   )
 }
