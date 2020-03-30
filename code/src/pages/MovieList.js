a90ea5cc132a2fd8a1bbcd9bc35055ad0b7ebbd2
Christina Persson <christina@MacBook-Pro.local>
2020-03-30 15:14:48 +0200

    Pushing before creating new branch

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 export const MovieList = () => {
 	const [ movies, setMovies ] = useState([]);
 
+	const filteredMovies = movies.filter((movie) => movie.poster_path !== null);
+
 	useEffect(() => {
 		fetch(
 			'https://api.themoviedb.org/3/movie/popular?api_key=3506f645d143411491b3a1c8d00f1217&language=en-US&page=1'
@@ -16,9 +18,9 @@ export const MovieList = () => {
 
 	return (
 		<div className="movies-container">
-			{movies.map((movie) => (
-				<Link to={`/movies/${movie.id}`}>
-					<div key={movie.id}>
+			{filteredMovies.map((movie) => (
+				<Link key={movie.id} to={`/movies/${movie.id}`}>
+					<div>
 						<img
 							className="img-poster"
 							src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
