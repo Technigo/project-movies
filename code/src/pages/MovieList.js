3ff3eed697277c6790cb6681003b935ac8d6980b
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-03-31 10:05:40 +0200

    Managed to print title and poster on MovieList

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -8,13 +8,19 @@ export const MovieList = () => {
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=b2d2d0205b4ac1ee806c10f3c7abcb87&language=en-US&page=1')
       .then((res) => res.json())
       .then((json) => {
-        setMovies(json)
+        setMovies(json.results)
       })
   }, [])
 
   return (
     <div>
-      Movie list
+      {movies.map((movie) => (
+        <div>
+          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title} />
+          <h2>{movie.title}</h2>
+
+        </div>
+      ))}
     </div>
   )
 }
