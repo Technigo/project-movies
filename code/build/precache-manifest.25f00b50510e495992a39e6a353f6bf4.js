844107fb781e7927e21c753fa9da1054fa697312
Ruterryter <hanna.ruter@gmail.com>
2020-03-31 20:09:02 +0200

    changed file structure

---
+++ code/build/precache-manifest.25f00b50510e495992a39e6a353f6bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8802fd4522b120af92800f5230bc7828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "057a043b8645e9b2abe7",
+    "url": "/static/css/main.43cc898c.chunk.css"
+  },
+  {
+    "revision": "ac413d39b568dc74b667",
+    "url": "/static/js/2.d0514c30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0514c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057a043b8645e9b2abe7",
+    "url": "/static/js/main.7e61d4ef.chunk.js"
+  },
+  {
+    "revision": "6364cdec4743245b3e80",
+    "url": "/static/js/runtime-main.b3f7ecfb.js"
+  }
+]);
