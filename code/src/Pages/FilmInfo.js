4c2178eb74eaddaff299bcd05b2653c156b10211
Tuliany <tuliany@mail.com>
2020-03-31 15:59:36 +0200

    filmInfo almost done

--- code/src/Pages/FilmInfo.js
+++ code/src/Pages/FilmInfo.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import "./FilmInfo.css"
 
 export const FilmInfo = () => {
   const {filmId} = useParams()
   const [movies, setMovies] =useState([])
-  let backgroundUrl = `http://image.tmdb.org/t/p/w1280"${movies.backdrop_path}`
+  
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${filmId}?api_key=095b0eff24360a92b50f50bfecf730fb`)
@@ -17,15 +18,14 @@ export const FilmInfo = () => {
   if (!movies) {
     return <></>
   }
+  
 
   return (
-    <div>
-    
+    <div class>
       <div key={movies.id}>
-        <div className="dataDiv" style={{backgroundImage: `url(${backgroundUrl})`}}></div>
-        {/* <img src={`http://image.tmdb.org/t/p/w300${movies.backdrop_path}`} alt={movies.original_title} /> */}
-        <h2>{movies.original_title}</h2>
-        <p>{movies.overview}</p>
+        <img class="poster" src={`http://image.tmdb.org/t/p/w300${movies.poster_path}`} alt={movies.original_title} />
+        <p><h2>{movies.original_title}</h2>
+        {movies.overview}</p>
 
         </div>
    
