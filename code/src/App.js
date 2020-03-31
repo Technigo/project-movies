583cfd1df2dc3639f057f451b183bf84cc7c7b8a
palhamel <pal.hamel@gmail.com>
2020-03-31 10:38:32 +0200

    Initial setup and list movies from API

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import { MovieList } from './pages/MovieList'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      {/* Hey You - Find me in src/app.js! */}
     </div>
   )
 }
---
+++ code/src/pages/MovieList.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState} from 'react'
+
+import './movielist_style.css'
+
+export const MovieList = () => {
+  
+  const api_key = "4e2114a81de8aa1d1db942cbcc0d3021"
+  const API_LINK_LIST = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
+  // const API_LINK_LIST = "https://api.themoviedb.org/3/movie/popular?api_key=4e2114a81de8aa1d1db942cbcc0d3021&language=en-US&page=1"
+  // Save a list of movies:
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(API_LINK_LIST)
+      .then((res) => res.json())
+      .then((json) => {
+        setMovies(json.results)
+      })
+  }, [])
+
+
+  return (
+    <main>
+
+      <div className="movies-container">
+        {movies.map((movie) => (
+
+          <article className="movie-card" key={movie.id}>
+            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+            <div className="movie-card-info">
+              <h2>{movie.title}</h2>
+              <h2>{movie.release_date}</h2>
+            </div>
+          </article>
+
+        ))}
+      </div>
+
+
+
+
+    </main>
+
+  )
+
+}
