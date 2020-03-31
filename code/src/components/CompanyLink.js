a2d671044f5a208e5790360d054e1484dc59a0d2
Frida Eriksson <Frida.Eriksson@silf.se>
2020-03-31 11:58:55 +0200

    updated ternary operators of Company Link, minor styling updates

--- code/src/components/CompanyLink.js
+++ code/src/components/CompanyLink.js
@@ -4,14 +4,18 @@ import './companylink.css'
 export const CompanyLink = ({ homepage, logo_path, imgSrc, name }) => {
   return (
     <div className="company-link">
-      {homepage !== '' ?
+      {homepage ?
+
         <a href={homepage}>
-          {logo_path !== null ? <img className="company-image" src={imgSrc} alt={name} /> : <h2 className="company-name-nav">{name}</h2>}
+          {logo_path ? <img className="company-image" src={imgSrc} alt={name} /> : <h2 className="company-name-nav">{name}</h2>}
         </a>
+
         :
+
         <>
-          {logo_path !== null ? <img className="company-image" src={imgSrc} alt={name} /> : <h2 className="company-name-nav">{name}</h2>}
+          {logo_path ? <img className="company-image" src={imgSrc} alt={name} /> : <h2 className="company-name-nav">{name}</h2>}
         </>
+
       }
     </div>
   )
