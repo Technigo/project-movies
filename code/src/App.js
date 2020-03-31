2291340e1c7e49facd1229b42f84c3a119f0f6ee
Lisas Sturegard <lisasturegard@hotmail.com>
2020-03-31 13:26:17 +0200

    Fetched movie poster, original title and release date for MovieList Page

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MoviesList } from './pages/MoviesList'
 // API key = ef0fb1aeba7a73c8909c5037c23fc608
 
 export const App = () => {
   return (
-    <div>
-      <MoviesList />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact >
+          <MoviesList />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
--- code/src/pages/MoviesList.js
+++ code/src/pages/MoviesList.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const MoviesList = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=ef0fb1aeba7a73c8909c5037c23fc608&language=en-US&page=1')
+    .then((res) => res.json())
+    .then((json) => {
+      setMovies(json.results)
+    })
+
+  }, [])
 
   return (
     <div>
-      Movies List
+      {movies.map((movie) =>(
+        <div>
+          <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.original_title}/> 
+          <h2>{movie.original_title}</h2>
+          <p>Released {movie.release_date}</p>
+        </div>
+      ))}
     </div>
   )
 }
