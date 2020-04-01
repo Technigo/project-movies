97ff2c87162feef6b32c77928747f77aabeb3be8
annatakman <annatakman@gmail.com>
2020-04-01 13:04:26 +0200

    final before pull request

--- code/src/pages/SimilarMovies.js
+++ code/src/pages/SimilarMovies.js
@@ -15,33 +15,31 @@ export const SimilarMovies = () => {
       })
   }, [movieId])
 
-  if (!movies.length) {
+  if (movies.length) {
     return (
-      <section className="not-found-message">
-        <p>No similar movies found.</p>
-        <Link className="backlink" to="/">
-          <span role="img" aria-label="back-to-movies">◀️ </span>
-          <span className="back-to-movies">Back to movies</span>
-        </Link>
+      <section className="movies-container">
+        {movies.map(movie => (
+          <article className="movie-card" key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <img className="poster" src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt={movie.title} />
+              <div className="text-container">
+                <h1>{movie.title}</h1>
+                <p>Released: {movie.release_date}</p>
+              </div>
+            </Link>
+          </article>
+        ))}
       </section>
     )
   }
 
   return (
-
-    <section className="movies-container">
-
-      {movies.map(movie => (
-        <article className="movie-card" key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>
-            <img className="poster" src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt={movie.title} />
-            <div className="text-container">
-              <h1>{movie.title}</h1>
-              <p>Released: {movie.release_date}</p>
-            </div>
-          </Link>
-        </article>
-      ))}
+    <section className="not-found-message">
+      <p>No similar movies found.</p>
+      <Link className="backlink" to="/">
+        <span role="img" aria-label="back-to-movies">◀️ </span>
+        <span className="back-to-movies">Back to movies</span>
+      </Link>
     </section>
   )
 }
