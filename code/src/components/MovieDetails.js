84bf8c0673ca42f2a8e2d4c4a727206d254cb0ae
VickBe <32632292+VickBe@users.noreply.github.com>
2020-04-01 15:05:42 +0200

    Added api bg-image to the detail component. Put the movie-list in a grid and added hover effects to each thumbnail

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import './movie.css'
+import './moviedetails.css'
 
-export const MovieDetails = () => {
+
+export const MovieDetails = ({style}) => {
   const { movieId } = useParams()
   const [movie, setMovie] = useState([])
-  
+  const backgroundImage = `http://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
+   
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=ce155d55b3ce81f17dbaf865fc989515&language=en-US`)
       .then((res) => res.json())
@@ -16,9 +18,14 @@ export const MovieDetails = () => {
   }, [movieId])
 
   return (
-
+   
       <div>
+        <div className="details-container">
+        <img src={backgroundImage}/>
         <h1 className='title'>{movie.title}</h1>
       </div>
+      </div>
+    
   )
+  
 }
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 // import { MovieThumb } from './MovieThumb'
 import { Link } from 'react-router-dom'
-
+import './movielist.css'
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([])
@@ -16,23 +16,26 @@ export const MovieList = () => {
   }, [])
 
   return (
-    <div>
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>
-            <section className="movie-thumb">
-              <p>{movie.title}</p>
-              <div className="movie-thumb-image-wrapper">
-                <img className="movie-thumb-image"
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title} />
-                <p className="movie-thumbnail-image-description">{movie.release_date}</p>
+    <div className="movie-list-container">
 
+      {movies.map((movie) => (
+        <article className="movie-thumb" key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>
+            <div className="overlay">
+              <div className="movie-thumb-text">
+              <h1>{movie.title}</h1>
+              <p className="movie-thumbnail-image-releasedate">{movie.release_date}</p>
               </div>
-            </section>
+            </div>
+            <img className="movie-thumb-image"
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title} />
+
           </Link>
-        </div>
+
+        </article>
       ))}
+
     </div>
   )
 }
