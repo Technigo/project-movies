a9cace565672e7ae64a977e8e9e421819fef9818
Lisas Sturegard <lisasturegard@hotmail.com>
2020-04-01 09:26:00 +0200

    Styling added for the Movie Info page

--- code/src/pages/MovieInfo.js
+++ code/src/pages/MovieInfo.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+
+import '../style/movieInfo.css'
 
 export const MovieInfo = () => {
   const { movieInfo } = useParams();
@@ -16,12 +18,19 @@ export const MovieInfo = () => {
   }
 
   return (
-    <div>
-      <img src={`https://image.tmdb.org/t/p/w300${info.poster_path}`} />
-      <img src={`https://image.tmdb.org/t/p/w300${info.backdrop_path}`} />
-      <h3>{info.original_title}</h3>
-      <h4>{info.vote_average}</h4>
+    <article className="info-container">
+      <div className="poster-container"><Link className="back" to="/"><i className="arrow-right"></i>Movies</Link>
+        <img className="poster" src={`https://image.tmdb.org/t/p/w300${info.poster_path}`} />
+      </div>
+      <div className="info-text">
+        <div className="title-and-rating">
+          <h3>{info.original_title}</h3>
+          <h4>{info.vote_average}/10</h4>
+        </div>
       <p>{info.overview}</p>
-    </div>
+      </div>
+    </article>
   )
 }
+/*
+<img className="bakground-image" src={`https://image.tmdb.org/t/p/w1280${info.backdrop_path}`} />*/
