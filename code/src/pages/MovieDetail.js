6d49a705aa27cc1c3b68697f6881c37c5c9bf982
Christina Persson <christina@MacBook-Pro.local>
2020-04-01 15:46:08 +0200

    silly git

---
+++ code/src/pages/MovieDetail.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { BackButton } from 'components/BackButton';
+import './moviedetail.css';
+
+export const MovieDetail = ({ director, directorName }) => {
+	const { movieId } = useParams();
+	const [ movieInfo, setMovieInfo ] = useState([]);
+
+	useEffect(
+		() => {
+			fetch(
+				`https://api.themoviedb.org/3/movie/${movieId}?api_key=3506f645d143411491b3a1c8d00f1217&language=en-US`
+			)
+				.then((res) => res.json())
+				.then((json) => {
+					setMovieInfo(json);
+				});
+		},
+		[ movieId ]
+	);
+
+	return (
+		<section className="wrapper-for-details">
+			<BackButton path={`/${director}/`} text={`Back to ${directorName}'s movies`} />
+			<div
+				className="movie-detail-container"
+				style={{
+					backgroundImage  : `linear-gradient(rgba(0, 0, 0, 0) -10%, rgb(0, 0, 0) 50%), url(https://image.tmdb.org/t/p/w1280${movieInfo.backdrop_path})`,
+					backgroundRepeat : 'no-repeat',
+					backgroundSize   : 'cover'
+				}}
+				key={movieInfo.id}
+			>
+				<div className="movie-detail-all-info">
+					<img
+						className="img-poster-detail"
+						src={`https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`}
+						alt={`${movieId.original_title} poster`}
+					/>
+					<div className="movie-detail-text">
+						<h1>
+							{movieInfo.original_title} <span className="movie-rating">{movieInfo.vote_average}/10</span>
+						</h1>
+						<p>{movieInfo.overview}</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
---
+++ code/src/pages/MovieList.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './movielist.css';
+import './director.css';
+import { BackButton } from 'components/BackButton';
+
+export const MovieList = ({ setDirector, director, directorName }) => {
+	const [ movies, setMovies ] = useState([]);
+	let filteredMovies = [];
+	setDirector(localStorage.getItem('director'));
+	console.log(director);
+	const url = `https://api.themoviedb.org/3/person/${director}/movie_credits?api_key=3506f645d143411491b3a1c8d00f1217`;
+	useEffect(
+		() => {
+			fetch(url).then((res) => res.json()).then((json) => {
+				setMovies(json.crew);
+			});
+		},
+		[ url, director ]
+	);
+
+	//So many if's but I'm not sure how to write this otherwise so I'll keep it like this for now
+	if (director !== undefined) {
+		const filteredMoviesIds = [];
+
+		movies.forEach((movie) => {
+			if (!filteredMoviesIds.includes(movie.id) && !filteredMoviesIds.includes(movie.original_title)) {
+				if (
+					movie.department.toLowerCase().includes('directing') ||
+					movie.department.toLowerCase().includes('writing')
+				) {
+					if (movie.poster_path !== null && movie.release_date !== '') {
+						filteredMoviesIds.push(movie.id);
+						filteredMoviesIds.push(movie.original_title);
+						filteredMovies.push(movie);
+					}
+				}
+			}
+		});
+	}
+
+	return (
+		<div className="site-container">
+			<BackButton path={'/'} text={'Back to Movies'} />
+			<header>
+				<h1>Written or directed by {directorName}</h1>
+			</header>
+			<div className="movies-container">
+				{filteredMovies.map((movie) => (
+					<Link key={movie.id} to={`/${director}/${movie.id}`}>
+						<div className="movie-card">
+							<img
+								className="img-poster"
+								src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+								alt={movie.original_title}
+							/>
+
+							<div className="movie-card-layer" />
+							<div className="text-container">
+								<h2>{movie.original_title}</h2>
+								<p>{movie.release_date}</p>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
+		</div>
+	);
+};
