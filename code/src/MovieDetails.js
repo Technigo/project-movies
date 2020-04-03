9a4220548f1fab803f8ef2bb0ec687a69c86a141
Louise Eriksson <louise@Louises-MacBook-Air.local>
2020-04-03 13:22:03 +0200

    added a map() for every genre. cleaned up the code/css

--- code/src/MovieDetails.js
+++ code/src/MovieDetails.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const MovieDetails = () => {
   const { movieId } = useParams()
-  const [movie, setMovie] = useState({})
+  const [movie, setMovie] = useState(undefined)
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=8470fd181cec36bb64fcc0377f8eb255&language=en-US`)
@@ -13,28 +14,50 @@ export const MovieDetails = () => {
       })
   }, [movieId])
 
+  if (movie === undefined) {
+    return (
+      <div className="loader">
+      </div>
+    )
+  }
 
   return (
     <div
       className="movie-details-wrapper"
       style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
+
+      <Link
+        to="/">
+        <h2>
+          ⬅︎ MOVIES
+        </h2>
+      </Link>
+
       <h1>
         {movie.original_title}
       </h1>
+
       <div className="poster-text-wrapper">
         <img src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt="movieposter" />
         <div className="overview-rating-wrapper">
           <p>
             {movie.overview}
           </p>
-          <h2>
-
-            {/* add genre instead of budget! */}
-
-            Genre: {movie.budget} | Rating: {movie.vote_average}/10 | Released: {movie.release_date}
-          </h2>
+          <span>
+            <h2>
+              Genre:
+            </h2>
+            {movie.genres.map((genres) => (
+              <h2>
+                {genres.name}
+              </h2>
+            ))}
+            <h2>
+              | Rating: {movie.vote_average}/10 | Released: {movie.release_date}
+            </h2>
+          </span>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
--- code/src/MovieList.js
+++ code/src/MovieList.js
@@ -1,19 +1,8 @@
-//api-key: 8470fd181cec36bb64fcc0377f8eb255
-//popular movies (replace api-key): https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page=1
-//moviedetails (replace key and id): https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US
-//different sized img: https://api.themoviedb.org/3/configuration?api_key={api_key}
-//example: https://image.tmdb.org/t/p/w1280${movie.backdrop_path}
-
-// ## Requirements 🧪
-// - Your app should have at least two pages - one showing a list of movies and one showing details
-// - You should follow the design from our example (but it's ok to change things - just try to make it look nice)
-// https://technigo-movies-project.netlify.com/
-
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const MovieList = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState(undefined)
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=8470fd181cec36bb64fcc0377f8eb255&language=en-US&page=1')
@@ -26,6 +15,13 @@ export const MovieList = () => {
         console.log(error)
       })
   }, [])
+
+  if (movies === undefined) {
+    return (
+      <div className="loader">
+      </div>
+    )
+  }
 
   return (
     <div className="movie-list">
