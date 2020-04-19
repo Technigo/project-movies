9543f90450e70b85e0764c24800c1c37a00f6b4a
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-04-19 20:34:20 +0200

    initiated styling, added functionality - links between pages, states in both movies page and details page

---
+++ code/src/Details.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom'
+import './details.css'
+
+
+
+export const Details = () => {
+
+  const [details, setDetails] = useState()
+  const { id } = useParams()
+
+  useEffect(() => {
+      fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=8fe2cb625158d587f8b0296620d5d249&language=en-US`)
+        .then(res => res.json())
+        .then(json => {
+          setDetails(json)
+        })          
+    }, [id])
+
+    return (
+      <section className="detail-page">
+            {console.log(details)}
+              <Link to={`/`} className="go-back"> Go back </Link> 
+                {details && (
+                  <div className="background" style={{ backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${details.backdrop_path})` }}>
+                    <div className="summary">
+                      <img className="summary-img" alt={details.title} src={`https://image.tmdb.org/t/p/w342${details.poster_path}`} />
+                      <div className="details">
+                        <h1>{details.title} <span className="rating">{details.vote_average}/10</span></h1>
+                        <p>{details.overview}</p>
+                      </div>
+                    </div>
+                  </div>
+                )}          
+      </section>
+    )
+  }
---
+++ code/src/Movies.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import 'movies.css'
+
+
+export const Movies = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=8fe2cb625158d587f8b0296620d5d249&language=en-US&page=1')
+          .then(res => res.json())
+          .then(json => setMovies(json.results))          
+      }, [])
+    
+    return (
+        
+      <main className="movie-container">
+          {movies.map((movie) => (
+              <Link key={movie.id} to={`/movies/${movie.id}`}>
+                <div className="movie-div"> 
+                    <div className="movie-details">
+                        <h1>{movie.title}</h1>
+                        <p>{movie.release_date}</p>
+                        <p>{movie.id}</p>
+                        <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+                    </div>
+                </div>
+              </Link> 
+            ))} 
+      </main>
+    )
+  }
