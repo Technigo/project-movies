bd31bf02d620fe985b839852fd3104acbf83b670
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-10-31 14:29:40 +0100

    Browserrouter, switch, route added in app.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,20 @@
-import MoviesList from 'Components/MoviesList'
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import MoviesList from 'Components/MoviesList';
+import MovieDetails from 'Components/MovieDetails';
 
 export const App = () => {
   return (
-    <div>
-      <MoviesList />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact> 
+          <MoviesList />
+        </Route>
+        <Route>
+          <MovieDetails path="/movies:id" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
--- code/src/Components/MoviesList.js
+++ code/src/Components/MoviesList.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import { API_URL } from '../urls';
 
@@ -16,28 +17,23 @@ const MoviesList = () => {
     }, []);
 
     return (
-        <main className="movies-container">
-            {moviesList.map((movie) => {
-                return (
-                    <article className="movie-wrapper" key={movie.id}>
-                        
-                        <div className="movie-image-overlay">
-                            
-                        </div>
-                        <div className="movie-start-info">
-                            <h1>{movie.original_title}</h1>
-                            <p>Released {movie.release_date}</p>
-                        </div>
-                        <img className="movie-image" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title}></img>
-                        
-                        
-                        
-                    </article>
-
-                )
-            })}
-            
-        </main>
+        <Link to="/movies:id">
+            <main className="movies-container">
+                {moviesList.map((movie) => {
+                    return (
+                        <article className="movie-wrapper" key={movie.id}>
+                            <div className="movie-image-overlay"> 
+                            </div>
+                            <div className="movie-start-info">
+                                <h1>{movie.original_title}</h1>
+                                <p>Released {movie.release_date}</p>
+                            </div>
+                            <img className="movie-image" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title}></img>
+                        </article>
+                    )
+                })}
+            </main>
+        </Link> 
     ) 
 }
 
