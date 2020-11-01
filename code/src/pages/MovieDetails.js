9ae93353a5e82b24c33cf2659c05d24fc6a513fc
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-01 09:05:50 +0100

    Fixed MoviesList page: showing main data

---
+++ code/src/pages/MovieDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const MovieDetails = () => {
+  return (
+    <h1>Movie Details Page</h1>
+  )
+};
--- code/src/pages/MoviesList.js
+++ code/src/pages/MoviesList.js
@@ -1,1 +1,28 @@
-//Api 2ff070f81f8c9206d9426765e063f416
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const API_KEY = '2ff070f81f8c9206d9426765e063f416';
+
+export const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.results)
+      })
+  }, []);
+
+  return (
+    <div className="movie-card">
+      {movies.map((movie) => (
+        <div className="movie-card" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title} />
+          <h2>{movie.original_title}</h2>
+          <p>{movie.release_date}</p>
+        </div>
+      ))}
+    </div>
+  )
+};
