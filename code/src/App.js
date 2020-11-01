9954095d58277fbfcdaa26f7f5e981e0b9be8cfe
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-01 08:28:48 +0100

    Initial setup: started App.js, created Pages files and installed Router

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { MoviesList } from 'pages/MoviesList';
+import { MovieDetails } from 'pages/MovieDetails';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <Switch>
+
+        <Route path="/" exact>
+          <MoviesList />
+        </Route>
+
+        <Route path="/movies/:movieID">
+          <MovieDetails />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
-}
+};
