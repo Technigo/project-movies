2fa72a21283c257c9d45401fb4ed2fce2e1ffe12
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-01 17:05:20 +0100

    Added rel attribute to solve deploying issue

--- code/src/Nav.js
+++ code/src/Nav.js
@@ -12,7 +12,7 @@ export const Nav = () => {
         <Link to="/top-movies">See also: Our Top Rated Movies of All Time!</Link>
       </p>
 
-      <a href="https://www.themoviedb.org/" target="_blank">Visit The Movie Database HomePage</a>
+      <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">Visit The Movie Database HomePage</a>
     </nav>
   )
 };
