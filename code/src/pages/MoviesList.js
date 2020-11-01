382e8cacbf8001e9db79e3d9d6df302bfefc9f1b
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-01 09:10:49 +0100

    Movie cards are now linking to movie details component, yay!

--- code/src/pages/MoviesList.js
+++ code/src/pages/MoviesList.js
@@ -18,9 +18,11 @@ export const MoviesList = () => {
     <div className="movie-card">
       {movies.map((movie) => (
         <div className="movie-card" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title} />
-          <h2>{movie.original_title}</h2>
-          <p>{movie.release_date}</p>
+          <Link to={`/movies/${movie.id}`}>
+            <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title} />
+            <h2>{movie.original_title}</h2>
+            <p>{movie.release_date}</p>
+          </Link>
         </div>
       ))}
     </div>
