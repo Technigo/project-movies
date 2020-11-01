2c9d5ccfe3a15591eaf6fa523ee54cbffbbb0ca6
Corneliaryas <hello@corneliaryas.com>
2020-11-01 15:33:52 +0100

    React Router, Components set up, Fetch of Movie list, basic styling of movie list

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import {MovieList} from './components/MovieList'
+import {MovieDetails} from './components/MovieDetails'
+
 
 export const App = () => {
+  
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+          <Route path ="/movies/:movieID" exact>
+            <MovieDetails />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   )
 }
---
+++ code/src/components/MovieList.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
+
+export const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+    // const movieKey = "868bb0ec2938c0c650aeb654eb40061d";
+
+    useEffect(() => {
+        fetch ("https://api.themoviedb.org/3/movie/popular?api_key=868bb0ec2938c0c650aeb654eb40061d&language=en-US&page=1")
+        .then ((res) => res.json())
+        .then ((json) => {
+            setMovies(json.results)
+        })
+    }, [])
+
+    
+
+    return (
+        <section className="movie-list">
+         {movies.map((movie) => {      
+            console.log(movie)
+         return ( 
+            <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-in-list">
+                <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} className="movie-poster"/>
+                <div className="list-details">
+                  <h2>{movie.title}</h2>
+                  <p>{movie.release_date}</p>
+                </div>
+            </Link>
+            )
+        })
+        } 
+        </section>
+    )
+}
