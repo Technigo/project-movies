9d27b4980c96940e686270fadfe0805e593518d2
Andrea Osmar <andrea88@live.se>
2020-11-02 16:23:48 +0100

    moved some divs around

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -15,18 +15,16 @@ export const MovieList = () => {
   })
 
   return (
-    <div className="list-wrapper">
+    <div className="list-wrapper"> 
       {movies.map((movie) => (
-        <div key={movie.id} className="lists">
+        <Link className="lists" key={movie.id} to={`/movies/${movie.id}`}>
           <img className="posters" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} />
-          <h2>
-            <Link to={`/movies/${movie.id}`}>
-            {movie.original_title}
-            </Link>
-            </h2>
-          <h3>{movie.release_date}</h3>
-        </div>
+            <div className="list-details">
+              <h2>{movie.original_title}</h2>
+              <h3>Released{movie.release_date}</h3>
+          </div>
+        </Link>
       )
-  )} </div>
+  )}</div>
 )
 }
