c9a812f2f53c9f7f0fd664ddc976dd1e2838c8bb
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2020-11-02 18:57:14 +0100

    set up casic structure for project

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { MovieList } from 'pages/MovieList'
 
 export const App = () => {
+  const [configuration, setConfiguration] = useState([])
+  const API_KEY = 'e09a63c0dcf4e1007de642df9ee29784'
+  const CONFIG_URL = `https://api.themoviedb.org/3/configuration?api_key=${API_KEY}`
+
+  useEffect(() => {
+    fetch(CONFIG_URL)
+    .then(res => res.json())
+    .then(json => (
+      setConfiguration(json.images)
+    ))
+  }, [])
+
+  console.log(configuration)
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route exact path='/'>
+            <MovieList apiKey={API_KEY} configuration={configuration}/>
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   )
 }
---
+++ code/src/pages/MovieList.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+import { Header } from 'components/Header'
+import { Footer } from 'components/Footer'
+
+export const MovieList = ({apiKey, configuration}) => {
+  const [movies, setMovies] = useState([])
+  const MOVIES_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=de-DE&sort_by=popularity.desc&with_original_language=de`
+
+  useEffect(() => {
+    fetch(MOVIES_URL)
+    .then(res => res.json())
+    .then(json => (
+      setMovies(json.results)
+    ))
+  }, [])
+
+  console.log(movies)
+  console.log(configuration.poster_sizes)
+
+  let imageSource = ''
+  imageSource = `${configuration.base_url}${configuration.poster_sizes[5]}`
+
+  return (
+    <>
+      <Header />
+      <section>
+        {movies.map((movie) => (
+          <img src={`imageSource${movie.poster_path}`}/>
+        ))}
+      </section>
+    </>
+  )
+}
