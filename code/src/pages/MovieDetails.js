5e3f8dbc5d41fba07bce71e9c3e12bcc7a78a2f1
AxelHammarback <axel.hammarback@gmail.com>
2020-11-02 23:11:32 +0100

    Whoa, progress was made! Managed to fetch info about each individual movie. Reeeally happy at this state.

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useParams } from 'react-router-dom'
 
-
 export const MovieDetails = () => {
-  const {id} = useParams()
   // Why does id need to be in curlies?
-  const params = useParams()
-  console.log(id)
-  console.log(params)
+  const { id } = useParams()
+  const MOVIE_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=0db2a5ac0c510ce6221cadf605f4d3e6&language=en-US`
+  const [movie, setMovie] = useState({})
 
-  return <p>This is the movie details for movie with id {id}</p>
+  useEffect(() => {
+    fetch(MOVIE_URL)
+      .then(response => response.json())
+      // .then(json => console.log(typeof(json)))
+      .then(json => setMovie(json))
+  }, [])
+
+  const prodCompanies = movie.production_companies
+  const backgroundSize = "w1280"
+  const posterSize = "w500"
+  const backdropURL = `https://image.tmdb.org/t/p/${backgroundSize}${movie.backdrop_path}`
+  const posterURL = `https://image.tmdb.org/t/p/${posterSize}${movie.poster_path}`
+
+  return (
+    <main
+      className="movie-details"
+      style={{
+        backgroundImage: `url('${backdropURL}')`
+      }}>
+        
+      {console.log(prodCompanies)}
+      <p>Title: {movie.title}</p>
+      <p>Tagline: {movie.tagline}</p>
+      <p>Production companies: </p>
+      <img src={posterURL} alt={`Still image from ${movie.title}`}></img>
+    </main>
+  )
 }
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from 'react'
 import { MovieCard } from '../components/MovieCard'
 
 export const MovieList = () => {
-  const MOVIE_URL = "https://api.themoviedb.org/3/movie/popular?api_key=0db2a5ac0c510ce6221cadf605f4d3e6&language=en-US&page=1";
+  const MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=0db2a5ac0c510ce6221cadf605f4d3e6&language=en-US&page=1";
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
-    fetch(MOVIE_URL)
+    fetch(MOVIES_URL)
       .then(response => response.json())
       .then(json => setMovies(json.results))
   }, [])
