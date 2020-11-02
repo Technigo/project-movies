85ad6b3e4cc57a3f2806c88e6e5b3fcbec268638
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2020-11-02 19:53:58 +0100

    added imageSource state

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -5,21 +5,20 @@ import { Footer } from 'components/Footer'
 
 export const MovieList = ({apiKey, configuration}) => {
   const [movies, setMovies] = useState([])
+  const [imageSource, setImageSource] = useState('')
   const MOVIES_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=de-DE&sort_by=popularity.desc&with_original_language=de`
 
   useEffect(() => {
     fetch(MOVIES_URL)
     .then(res => res.json())
     .then(json => (
-      setMovies(json.results)
+      setMovies(json.results),
+      setImageSource(`${configuration.base_url}${configuration.poster_sizes[5]}`)
     ))
   }, [])
 
   console.log(movies)
   console.log(configuration.poster_sizes)
-
-  let imageSource = ''
-  imageSource = `${configuration.base_url}${configuration.poster_sizes[5]}`
 
   return (
     <>
