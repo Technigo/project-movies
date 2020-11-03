aefc4b82875aa9dc682aac47072f99043c97fc30
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-03 14:58:46 +0100

    Started styling: NavBar in mobile done

--- code/src/Nav.js
+++ code/src/Nav.js
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom';
 export const Nav = () => {
   return (
     <nav>
-      <p>
-        <Link to="/">PROJECT MOVIES - Popular Movies Right Now!</Link>
+      <p className="movies-nav-text-mobile">
+        <Link to="/"><span role="img" aria-label="popcorn emoji">&#127871;</span>Popular Movies</Link>
       </p>
 
-      <p>
+      <p className="movies-nav-text">
+        <Link to="/">Popular Movies Right Now!</Link>
+      </p>
+
+      <p className="top-movies-nav-text">
         <Link to="/top-movies">See also: Our Top Rated Movies of All Time!</Link>
       </p>
 
-      <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">Visit The Movie Database HomePage</a>
+      <a className="tmdb-nav-text" href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">Visit The Movie Database HomePage</a>
     </nav>
   )
 };
