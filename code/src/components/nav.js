534a4cdb3811d83eadb8adbb8c7de8ef4387205a
Andrea Osmar <johndoe@email.com>
2020-11-03 20:31:52 +0100

    updated styling on moviedetails

--- code/src/components/nav.js
+++ code/src/components/nav.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
 
 export const Nav = () => (
   <header>
     <h1> POPULAR MOVIES</h1>
       <Route path="/movies">
         <Link to="/">
-        <button type="button">Home</button>
+        <button type="button"><span>⬅</span> Back</button>
         </Link>
       </Route>
   </header>
--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -4,36 +4,36 @@ import { useParams } from 'react-router-dom'
 export const MovieDetails = () => {
   const { id } = useParams()
   const [details, setDetails] = useState([])
-  const ApiKey = '40be626d591abb41158713818687432b'
-  const detailListURL = `https://api.themoviedb.org/3/movie/${id}?api_key=${ApiKey}&language=en-US`
 
   useEffect(() => {
-    fetch(detailListURL)
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=40be626d591abb41158713818687432b&language=en-US`)
     .then((res) => res.json())
     .then((json) => {
       console.log(json)
       setDetails(json)
     })
+  
   },[])
 
-  const movieDetailStyle = {
-    backgroundImage: `url("http://image.tmdb.org/t/p/w1280${details.backdrop_path}")`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-  }
+
+const movieDetailStyle = {
+  backgroundImage: `url("http://image.tmdb.org/t/p/w1280${details.backdrop_path}")`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+}
 
   return (
-    <section className="movie-detail-container" style={movieDetailStyle} >
-      <img className="poster" src={`https://image.tmdb.org/t/p/w500${details.poster_path}`} alt={details.original_title} />
-        <div key={details.id}>
-          <section className="details">
+    <article className="movie-detail" style={movieDetailStyle} >
+      
+        <div key={details.id} className="details">
+          <img className="poster" src={`https://image.tmdb.org/t/p/w500${details.poster_path}`} alt={details.original_title} />
+          <div className="summary">
             <h2>{details.original_title}</h2>
             <h3>{details.vote_average}/10</h3>
-            {details.overview}
-          </section>
+            <p className="overview">{details.overview}</p>
+          </div>  
         </div>
-    </section> 
+    </article> 
     
   )
 }
-
