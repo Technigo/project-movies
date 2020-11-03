533ecefde84ede46680b3e92c508c3b832e03750
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-03 12:51:24 +0100

    Updated FilmDetail.js: 1. Created a new variable called fetchFilmId where I'm defining the api key, api url for the film id and the fetch. 2. Then I'm calling this variable in the useEffect. 3. Also added the FilmId which is the useParams name in the square brackets for the useEffect dependancy. 4. Updated my jsx elements with the new FilmDetails state name.

--- code/src/pages/FilmDetail.js
+++ code/src/pages/FilmDetail.js
@@ -2,26 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const FilmDetail = () => {
-    const { id } = useParams();
-    const api_key = "cf10fb16d5d29cc1bc7eb10f47f96663"
-    const RG_API_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US`;
+    const { FilmId } = useParams();
 
-    const [MovieDetails, setMovieDetails] = useState(""); 
+    const [FilmDetails, setFilmDetails] = useState(""); 
 
-    useEffect(() => {
-        fetch(RG_API_MOVIE_DETAILS)
+    const fetchFilmId = () => {
+        const api_key = "cf10fb16d5d29cc1bc7eb10f47f96663";
+        const RG_API_FILM_DETAILS = `https://api.themoviedb.org/3/movie/${FilmId}?api_key=${api_key}&language=en-US`;
+
+        fetch(RG_API_FILM_DETAILS)
             .then((data) => {
                 return data.json()
             })
                 .then((data) => {
-                    setMovieDetails(data);
+                    setFilmDetails(data);
                 })
                 .catch((error) => {
                     console.error(error);
                     })
-    }, []);
+    };
 
-    console.log(MovieDetails);
+    useEffect(() => {
+        fetchFilmId();        
+    }, [FilmId]);
 
     return (
         <article>
@@ -29,13 +32,13 @@ export const FilmDetail = () => {
                 Movie list
             </a>
             <div>
-                <img src={`https://image.tmdb.org/t/p/w342${MovieDetails.poster_path}`} />
+                <img src={`https://image.tmdb.org/t/p/w342${FilmDetails.poster_path}`} alt="{title} film poster" />
                 <div>
-                    <img src={`https://image.tmdb.org/t/p/w342${MovieDetails.backdrop_path}`} />
+                    <img src={`https://image.tmdb.org/t/p/w342${FilmDetails.backdrop_path}`} alt="{title} film poster"/>
                     <div>
-                        <h3>{MovieDetails.title}</h3>
-                        <p>{MovieDetails.vote_average} / 10</p>
-                        <p>{MovieDetails.overview}</p>
+                        <h3>{FilmDetails.title}</h3>
+                        <p>{FilmDetails.vote_average} / 10</p>
+                        <p>{FilmDetails.overview}</p>
                     </div>
                 </div>
             </div>            
