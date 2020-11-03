684c934cb6069aafe6907049ba3aee2048f9d7d5
Rebkova <rebkova@gmail.com>
2020-11-03 11:28:18 +0100

    displyed top rated movies, title, rating, release date, no styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
+import MovieList from "./components/MovieList";
+
+const API_key = "5ddb547497cb885965bcba7335e4be9c";
+const API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_key}&language=en-US&page=1`;
+
 export const App = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchTopRatedMovies = () => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setMovies(data.results));
+  };
+
+  fetchTopRatedMovies();
+
   return (
     <BrowserRouter>
-      <div>
-        Hello, Rebeka. Best of luck with this week's project! ^.^
-      </div>
+      {/* <h1>Hello, Rebeka. Best of luck with this week's project! ^.^</h1> */}
+      <section className="app-container">
+        {movies.map(movie => (
+          <MovieList
+            key={movie.id}
+            movieTitle={movie.title}
+            averageVote={movie.vote_average}
+            releaseDate={movie.release_date}
+          />
+        ))}
+
+      </section>
     </BrowserRouter>
   )
 }
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,3 +1,24 @@
-import React from "react"
+import React from "react";
 
-API_key = "5ddb547497cb885965bcba7335e4be9c";
+import "./movie-list.css";
+
+
+const MovieList = ({ movieTitle, averageVote, releaseDate }) => {
+  // const [movies, setMovies] = useState();
+
+  // fetch(API_URL)
+  //   .then(response => response.json())
+  //   .then(data => setMovies(data.results));
+  // .then(data => setMovies(data));
+
+  return (
+    <div className="movie-card">
+      <h1>{movieTitle}</h1>
+      <p>Rating: {averageVote}/10</p>
+      <p>Release date: {releaseDate}</p>
+    </div>
+  );
+
+};
+
+export default MovieList;
