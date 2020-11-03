0606045a6e5a9ffb2c264c4ecf6baf7e70ffd2da
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-03 09:36:52 +0100

    Removed import to fix deployment issue

--- code/src/pages/TopMovieDetails.js
+++ code/src/pages/TopMovieDetails.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 import { TopMovie } from 'components/TopMovie';
 import { Loading } from 'components/Loading';
