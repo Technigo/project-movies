23ba08e12223186d4bb881eea2fe5e27c716a37f
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-03 12:45:28 +0100

    Updated FilmList.js: 1. Put api key, api url and fetch into a function which I then call in the useEffect. This will help with scope when fetching from the api so no elements are defined outside of the scope as I was doing the fetch inside the useEffect directly before.

--- code/src/components/FilmList.js
+++ code/src/components/FilmList.js
@@ -4,28 +4,32 @@ import { FilmCard } from 'components/FilmCard';
 
 export const FilmList = () => {
 
-    const api_key = "cf10fb16d5d29cc1bc7eb10f47f96663"
-    const RG_API = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_cast=30614`
+    const [FilmList, setFilmList] = useState([]); 
 
-    const [MovieList, setMovieList] = useState([]); 
-  
-    useEffect(() => {
+    const fetchFilms = () => {
+        const api_key = "cf10fb16d5d29cc1bc7eb10f47f96663";
+        const RG_API = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_cast=30614`;
+
         fetch(RG_API)
         .then((data) => {
             return data.json()
         })
             .then((data) => {
-            setMovieList(data.results);
+            setFilmList(data.results);
             })
                 .catch((error) => {
                 console.error(error);
                 })
+    };
+  
+    useEffect(() => {
+        fetchFilms();        
     }, []);
 
     return (
         <section>
-            {MovieList.map((movies) => (
-            <FilmCard key={movies.id} {...movies} />
+            {FilmList.map((films) => (
+            <FilmCard key={films.id} {...films} />
             ))}
         </section>
     );
