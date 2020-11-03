0aa4233f021c547c7cbf8ce775a8227e6acc6441
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-03 17:00:06 +0100

    Styling for mobile done

--- code/src/components/Movie.js
+++ code/src/components/Movie.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Arrow from '../components/back-arrow.png';
+
 export const Movie = ({poster_path, original_title, vote_average, overview, backdrop_path}) => {
   return (
-    <>
+    <section className="movie-details-page" style={{backgroundImage:`url('https://image.tmdb.org/t/p/w1280/${backdrop_path}')`}}>
       <Link to="/" exact="true">
-        <p>Back to Movies List</p>
+        <div className="back-arrow-text">
+          <img className="arrow" src={Arrow} alt="back-arrow"/>
+          <p className="back-movies-text">Movies</p>
+        </div>
       </Link>
-      <div className="movie-details-page">
+      <div className="movie-details-container">
         <img src={`https://image.tmdb.org/t/p/w780/${poster_path}`} alt={original_title} />
         <h2>{original_title}</h2>
-        <p>{`${vote_average} / 10`}</p>
-        <p>{overview}</p>
-        <img width="300px" src={`https://image.tmdb.org/t/p/w1280/${backdrop_path}`} alt={original_title} />
+        <p className="movie-rating-text">{`${vote_average} / 10`}</p>
+        <p className="movie-overview-text">{overview}</p>
       </div>
-    </>
+    </section>
   )
 };
-
-//backdrop as background, access backdrop: src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
