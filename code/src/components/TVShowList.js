74a55e7903b8e34f879b669f13204ec0a2ec325b
Evelina Berg <evelina.berg@outlook.com>
2020-11-03 23:16:06 +0100

    useEffect in place without error

--- code/src/components/TVShowList.js
+++ code/src/components/TVShowList.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { API_URL } from '../urls.js';
 import { API_KEY } from '../API_KEY.js';
 
 export const TVShowList = () => {
-    const TV_URL = `${API_URL}popular?&api_key=${API_KEY}`;
     // eslint-disable-next-line
     const [TVShows, setTVShows] = useState([]);
 
     console.log('Render');
 
-    fetch(TV_URL)
-        .then(respons => respons.json())
-        .then(json => console.log(json.results));
+    const fetchTVShows = () => {
+      const TV_URL = `${API_URL}popular?&api_key=${API_KEY}`;
 
+      fetch(TV_URL)
+        .then(respons => respons.json())
+        .then(json => console.log(json.results))
+        .catch(error => console.error(error));
+    };
+
+    useEffect(() => {
+        fetchTVShows()
+    }, []);
+    
     return <section> TV-shows go here </section>
 }
