58dfebdbe32853cceb59e7ee4061376ca6d82396
Bäm Stugendal <Bam@Bams-iMac.local>
2020-11-03 23:13:21 +0100

    Styled front page

--- code/src/componet/MovieContainer.js
+++ code/src/componet/MovieContainer.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 export const MovieContainer = ({ title, release, poster, id}) => {
     return (
         <Link to={`/movies/${id}`}>
-            <div>
-                <h2>{title}</h2>
-                <p>{release}</p>
+            <div className="movie-container">
                 <img src={`https://image.tmdb.org/t/p/w500${poster}`} alt={title} />
+                <div className="movie-info-container">
+                    <h2>{title}</h2>
+                    <p>Released {release}</p>
+                </div>
             </div>
         </Link>
     )
