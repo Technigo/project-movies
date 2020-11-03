c2d45254461877dd74932c091129505e02b642f0
Jamie Cook <jamie.p.cook@outlook.com>
2020-11-03 21:11:48 +0100

    movie list created and API info displaying

---
+++ code/src/components/MovieList.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+import { MovieCard } from "components/MovieCard";
+
+export const MovieList = () => {
+  const MOVIES_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=5e0af1d18e77dbd12a3e994aa1316cbf&language=en-US&page=1";
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(MOVIES_URL)
+      .then(response => response.json())
+      .then(json => setMovies(json.results));
+  }, []);
+
+  return (
+    <section className="movie-list">
+      {movies.map(movie => (
+        <MovieCard key={movie.id} {...movie} />
+      ))}
+    </section>
+  );
+};
