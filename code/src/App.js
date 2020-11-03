61f4129b29ce3d4e210ce2ae821d1c23403fa2ee
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2020-11-03 15:10:25 +0100

    fixed the configuration fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { MovieList } from 'pages/MovieList'
+import { MovieList } from 'pages/MovieList';
 
 export const App = () => {
-  const [configuration, setConfiguration] = useState([])
-  const API_KEY = 'e09a63c0dcf4e1007de642df9ee29784'
-  const CONFIG_URL = `https://api.themoviedb.org/3/configuration?api_key=${API_KEY}`
+  //state variables for parts of the image file paths
+  const [baseUrl, setBaseUrl] = useState('');
+  const [posterSize, setPosterSize] = useState();
+  const API_KEY = 'e09a63c0dcf4e1007de642df9ee29784';
+  const CONFIG_URL = `https://api.themoviedb.org/3/configuration?api_key=${API_KEY}`;
 
+  //Fetch to get the base URL and poster size for the image file paths
   useEffect(() => {
     fetch(CONFIG_URL)
     .then(res => res.json())
     .then(json => (
-      setConfiguration(json.images)
+      setBaseUrl(json.images.base_url),
+      setPosterSize(json.images.poster_sizes[3])
     ))
-  }, [])
-
-  console.log(configuration)
+  }, []);
 
   return (
     <BrowserRouter>
       <main>
         <Switch>
           <Route exact path='/'>
-            <MovieList apiKey={API_KEY} configuration={configuration}/>
+            <MovieList 
+              apiKey={API_KEY} 
+              baseUrl={baseUrl} 
+              posterSize={posterSize}
+            />
           </Route>
         </Switch>
       </main>
     </BrowserRouter>
   )
-}
+};
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,33 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-import { Header } from 'components/Header'
-import { Footer } from 'components/Footer'
+import { Header } from 'components/Header';
+import { Footer } from 'components/Footer';
 
-export const MovieList = ({apiKey, configuration}) => {
-  const [movies, setMovies] = useState([])
-  const [imageSource, setImageSource] = useState('')
-  const MOVIES_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=de-DE&sort_by=popularity.desc&with_original_language=de`
+export const MovieList = ({apiKey, baseUrl, posterSize}) => {
+  const [movies, setMovies] = useState([]);
+  const MOVIES_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=de-DE&sort_by=popularity.desc&with_original_language=de`;
 
   useEffect(() => {
     fetch(MOVIES_URL)
     .then(res => res.json())
     .then(json => (
-      setMovies(json.results),
-      setImageSource(`${configuration.base_url}${configuration.poster_sizes[5]}`)
+      setMovies(json.results)
     ))
-  }, [])
-
-  console.log(movies)
-  console.log(configuration.poster_sizes)
+  }, []);
 
   return (
     <>
       <Header />
       <section>
         {movies.map((movie) => (
-          <img src={`imageSource${movie.poster_path}`}/>
+          <img src={baseUrl + posterSize + `${movie.poster_path}`}/>
         ))}
       </section>
     </>
   )
-}
+};
