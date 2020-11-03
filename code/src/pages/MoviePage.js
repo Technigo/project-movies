9c7aa7c6987d951684d3fd627af9a3ed2489feb3
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-11-03 19:53:21 +0100

    Added conditional to avoid error

--- code/src/pages/MoviePage.js
+++ code/src/pages/MoviePage.js
@@ -7,16 +7,11 @@ import Movie from '../components/Movie';
 const MoviePage = ({ movies }) => {
   const params = useParams();
   const [movieDetails, setMovieDetails] = useState('');
-  const [coverImagePath, setCoverImagePath] = useState('');
 
   useEffect(() => {
-    // const movieMatch = movies.find(
-    //   (movie) => JSON.stringify(movie.id) === params.id
-    // );
-
-    // if (!movieMatch) {
-    //   return <div>Not found!</div>;
-    // }
+    const movieMatch = movies.find(
+      (movie) => JSON.stringify(movie.id) === params.id
+    );
 
     fetch(
       `https://api.themoviedb.org/3/movie/${params.id}?api_key=${ApiKey}&language=en-US`
@@ -24,32 +19,33 @@ const MoviePage = ({ movies }) => {
       .then((results) => results.json())
       .then((json) => {
         setMovieDetails(json);
-        setCoverImagePath(json.backdrop_path);
       });
   }, [params.id, movies]);
 
   return (
-    <section
-      className="movie--wrapper"
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${BackdropImgUrl}${coverImagePath})`,
-      }}
-    >
-      <Link className="movies--back-button" key={movies.id} to="/">
-        <img
-          alt="back-button"
-          className="movies--back-button--img"
-          src="../images/noun_back arrow_2223783.svg"
+    movieDetails && (
+      <section
+        className="movie--wrapper"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${BackdropImgUrl}${movieDetails.backdrop_path})`,
+        }}
+      >
+        <Link className="movies--back-button" key={movies.id} to="/">
+          <img
+            alt="back-button"
+            className="movies--back-button--img"
+            src="../images/noun_back arrow_2223783.svg"
+          />
+          <span className="movies--back-button--text">Movies</span>
+        </Link>
+        <Movie
+          posterImg={`${PosterImgUrl}${movieDetails.poster_path}`}
+          title={movieDetails.title}
+          rating={movieDetails.vote_average}
+          description={movieDetails.overview}
         />
-        <span className="movies--back-button--text">Movies</span>
-      </Link>
-      <Movie
-        posterImg={`${PosterImgUrl}${movieDetails.poster_path}`}
-        title={movieDetails.title}
-        rating={movieDetails.vote_average}
-        description={movieDetails.overview}
-      />
-    </section>
+      </section>
+    )
   );
 };
 
