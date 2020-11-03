ceb2585d9c95c793e0f4cb667b34e60abdfd068a
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-03 18:48:05 +0100

    Styling done for NavBar and MoviesList Page on Desktop

--- code/src/Nav.js
+++ code/src/Nav.js
@@ -16,7 +16,7 @@ export const Nav = () => {
         <p className="top-movies-nav-text"><span className="nav-emoji-tablet" role="img" aria-label="movies emoji">&#127916;</span>See also: Our Top Rated Movies of All Time!</p>
       </Link>
 
-      <a className="tmdb-nav-text" href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">Visit The Movie Database HomePage</a>
+      <a className="tmdb-nav-text" href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer"><span className="nav-emoji-tablet" role="img" aria-label="movies emoji">&#127910;</span>Visit The Movie Database!</a>
     </nav>
   )
 };
