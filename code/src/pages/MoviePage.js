b80a995809a547d43e1d1ea1f9c99a3b74e45177
Sofia Sousa <sofiavazsousa@gmail.com>
2020-11-04 18:34:59 +0100

    Add back button + main styling completed

--- code/src/pages/MoviePage.js
+++ code/src/pages/MoviePage.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Link, useParams } from 'react-router-dom'; 
-import { apiKey } from '../key';
+import { Link, useParams } from "react-router-dom"; 
+import { apiKey } from "../key";
+import { BackArrow } from "../Icon/BackArrow";
 
 export const MoviePage = () => { 
   const { movieId } = useParams (); 
@@ -19,19 +20,18 @@ export const MoviePage = () => {
 
 return (
   <>
-    <Link to ="/" exact='true'>
-'      <p>Movies List</p>
-'    </Link>
-          <section className="movie-page" 
-            style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 10%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
-              <div className="movie-page-details">
-                <img className="movie-poster" src= {`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title}/>
-                <div class="details-container">
-                  <h2>{movie.original_title}</h2>
-                  <p className="movie-rating">{`${movie.vote_average} /10`}</p>
-                  <p className="movie-overview">{movie.overview}</p>
-                </div>
+          <section className="movie-page" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 10%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`}}>
+            <Link className="back-link" to ="/" exact='true'>
+              <BackArrow/>Movies List
+            </Link>
+            <div className="movie-page-details">
+              <img className="movie-poster" src= {`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title}/>
+              <div class="details-container">
+                <h2>{movie.original_title}</h2>
+                <p className="movie-rating">{`${movie.vote_average} /10`}</p>
+                <p className="movie-overview">{movie.overview}</p>
               </div>
+            </div>
           </section> 
   </>
   );
