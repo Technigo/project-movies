96507a044b1eef4445ed576955e9d1de53f1ec59
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-04 10:00:57 +0100

    Styling for NotFound Page done

--- code/src/components/NotFound.js
+++ code/src/components/NotFound.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import 'components/notfound.css';
+
 export const NotFound = () => {
   return (
-    <>
-      <h1>Movie not Found</h1>
+    <section className="not-found">
+      <h1>Oops! Bummer, We couldn't find that movie... <span role="img" aria-label="shy emoji">&#128517;</span></h1>
+      <img src="https://thumbs.gfycat.com/UnpleasantFancyFairyfly-size_restricted.gif" />
+      <h2>Check the most popular movies and grab that pop corn!</h2>
       <Link to="/" exact="true">
-        <p>Back to Movies List</p>
+        <p><span role="img" aria-label="movies emoji">&#127871;</span>Back to Popular Movies<span role="img" aria-label="movies emoji">&#127871;</span></p>
       </Link>
-    </>
+    </section>
   )
 };
