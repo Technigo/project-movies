2567469cc022b77a855673e2e41619b4b9548ac7
Therese <64402455+Thessan@users.noreply.github.com>
2020-11-04 15:19:31 +0100

    first commit: fetching the API, displaying each movie title & release date

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { MovieList } from './MovieList'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MovieList />
+        </Route>
+
+        
+
+      </Switch>
+    
+    
+    </BrowserRouter>
   )
 }
---
+++ code/src/MovieList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+export const MovieList = () => {
+    const [movies, setMovies] = useState([])
+
+    // fetch to call the API
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=b230af0ba5dd0959429a398c958e6e4e&language=en-US&page=1')
+            .then((response) => response.json())
+            .then((json) => {
+                setMovies(json.results);
+            })
+    }, []);
+
+    return (
+        <div className="allMovies">
+            {movies.map((movies) => (  // map over all movies and for each one display: img, title & release date 
+                <div className="eachMovie">
+                    <h1 className="movieTitle1"> {movies.title} </h1>
+                        <p className="releaseDate"> {movies.release_date} </p>
+                        
+                </div>
+            ))} 
+            
+        </div> 
+    ) 
+
+}
+
+
