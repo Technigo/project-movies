551bdc7a55ccebe34de5a70b1625999f740df4d0
Emelie Svensson <emeliesv@live.se>
2020-11-04 09:54:46 +0100

    added overlay to images

--- code/src/components/Movie.js
+++ code/src/components/Movie.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { IMAGE_URL } from './URLS';
+import { IMAGE_URL } from "./URLS";
 
-import '../styles/movie.css';
+import "../styles/movie.css";
 
-/* The idea is to use props to display 
-movie-titles and images from different endpoints
-of the API */
-export const Movie = ({title, poster_path}) => {
-    return ( 
-        <section className='movie__card'>
-            <h2>{title}</h2>
-            <img src={`${IMAGE_URL}${poster_path}`} alt={title}/>
-        </section>
-    );
+/* This component should be reusable no matter what typ of movie you'd like to display
+Should this component be the link? */
+export const Movie = ({ title, poster_path, release }) => {
+  return (
+    <section className="movie__card">
+      <div className="movie__overlay">
+        <div className='movie__info-text'>
+        <h2 className="movie__heading">{title}</h2>
+        <p className="movie__released">Released {release}</p>
+        </div>
+      </div>
+      <img src={`${IMAGE_URL}${poster_path}`} alt={title} />
+    </section>
+  );
 };
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -4,6 +4,9 @@ import { Movie } from './Movie';
 
 import '../styles/movielist.css';
 
+/* This component should show a list of movies
+Each movie should be a link to a more detailed page about that specific movie */
+
 export const MovieList = ({movies}) => {
 
     return (
