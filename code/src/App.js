67ac183c827665388e5edb8301333cbe12b06850
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2020-11-04 22:21:17 +0100

    Basic structure done

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { MovieList } from "components/MovieList";
+import { MovieDetails } from "components/MovieDetails";
+
+// api-key 995425e2e6cd0fcf599ff795098e1e8b
+
+// import { MovieList } from "./MovieList";
+
+// import { MovieDetails } from "./MovieDetails";
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MovieList />
+        </Route>
+        <Route path="/movies/:movieId" exact>
+          <MovieDetails />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+// return (
+//   <BrowserRouter>
+//     <Switch>
+//       <Route path="/" exact>
+//         <MovieList />
+//       </Route>
+//       <Route path="/movies/:slug" exact>
+//         <MovieDetails />
+//       </Route>
+//     </Switch>
+//   </BrowserRouter>
+// );
---
+++ code/src/components/MovieThumb.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// //deconstructure the props with {name} and call the parameter//
+
+// export const MovieThumb = ({ title, poster_path, rating, slug }) => {
+//   return (
+//     // <Link to={`/movies/${slug}`}>
+//     <section className="movie-thumb">
+//       {title}
+//       <div className="movie-thumb-image-wrapper">
+//         <img className="movie-thumb-image" srs={poster_path} alt={title} />
+//         <p className="movie-thumb-image-description">{rating}</p>
+//       </div>
+//     </section>
+//   );
+// };
