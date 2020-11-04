ecd7314568533b3116e58b8e131323a2f4788ffb
Moa Blomkvist <moa@birgerochblom.se>
2020-11-04 16:03:25 +0100

    path

---
+++ code/src/pages/MovieDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MovieDetails = () => {
+  return (
+    <div>
+      <h2>Hej filmälskare</h2>
+    </div>
+  )
+
+}
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
-export const MovieList = () => {
-  const [movies, setMovies] = useState ([])
-
+import { Link } from 'react-router-dom'
 
 const API_KEY = "d18f9e3ce26c7875c97d06084254693a";
 const URL_POPULAR_MOVIES = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+export const MovieList = () => {
+  const [movies, setMovies] = useState ([])
 
   useEffect(() => {
     fetch(URL_POPULAR_MOVIES)
@@ -14,12 +15,19 @@ const URL_POPULAR_MOVIES = `https://api.themoviedb.org/3/movie/popular?api_key=$
         setMovies(json.results)
 
       })
-
   }, [])
 
   return (
     <div>
-      Movie List
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title}/>
+            <Link to={`/movies/${movie.id}`}>
+              <h2>{movie.original_title}</h2>
+              <h3>{movie.release_date}</h3>
+            </Link>
+        </div>
+      ))}
     </div>
   )
 }
