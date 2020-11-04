db1a26debf9cad3d88356e7a3f0c707181fcfbd6
Moa <63230455+mhammarback@users.noreply.github.com>
2020-11-04 17:17:19 +0100

    added liniar gradient in background on movie details

--- code/src/components/MovieDetail.js
+++ code/src/components/MovieDetail.js
@@ -22,37 +22,28 @@ export const MovieDetail = () => {
 
 
     return(
-     <>
+    <>
 
-     <section className="detail-backdrop" style={{  backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movies.backdrop_path})` }}>
+    <section className="detail-backdrop" style={{  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%),  url(https://image.tmdb.org/t/p/w1280${movies.backdrop_path})` }}>
         <div className="movie-detail-content">  
-        <div className="movie-detail-back">
+            <div className="movie-detail-back">
 
-        <Link to ="/" className="link" exact='true'>
-        <img className="back-icon" src={Icon} style={ {width:"40px"} }  alt="icon" /> 
-            <div className="back-text">Movies</div>
-        </Link>
-        <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`} alt={movies.title} /> 
-        </div>      
+                <Link to ="/" className="link" exact='true'>
+                    <img className="back-icon" src={Icon} style={ {width:"40px"} }  alt="icon" /> 
+                    <div className="back-text">Movies</div>
+                </Link>
+                    <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`} alt={movies.title} /> 
+            </div>      
 
-        <div className="movie-detail-text"> 
-        <h1 className="movie-detail-title">{movies.title}
-        <span className="rating"><p> {movies.vote_average}/10</p></span>
-        </h1> 
-        <p className="overview">{movies.overview}</p>     
+            <div className="movie-detail-text"> 
+                <h1 className="movie-detail-title">{movies.title}
+                <span className="rating"><p> {movies.vote_average}/10</p></span>
+                </h1> 
+                <p className="overview">{movies.overview}</p>     
+            </div>
+
         </div>
-
-    </div>
-</section>
+    </section>
     </>
     )
 }
-
-/*
-
-
-
-<img className="movie-detail-background" 
-src={`https://image.tmdb.org/t/p/w1280${movies.backdrop_path} `} alt={`${`original_title`}poster`}  /> 
-
-*/
