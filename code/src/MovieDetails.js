45fd72b376b722c7b8ea8db373e61756756c1c08
Sara Sundblom <sundblomsara@gmail.com>
2020-11-04 18:16:46 +0100

    started to style the individual movie sites

--- code/src/MovieDetails.js
+++ code/src/MovieDetails.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const MovieDetails = ({ title, overview, vote_average }) => {
+import "./css/MovieDetails.css"
+
+export const MovieDetails = ({ title, overview, vote_average, backdrop_path, poster_path }) => {
+    const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p'
+    const BACKDROP_SIZE = '/w1280'
+    const POSTER_SIZE = '/w300'
+
+
 
     return (
         <section className='movie-details'>
+            <img className="background" src={`${IMAGE_BASE_URL + BACKDROP_SIZE + backdrop_path}`} alt='backdrop'></img>
+
             <Link to='/'>
-                <p>Back</p>
+                <p className="back-button">Movies</p>
             </Link>
-            <img alt='backdrop'></img>
-            <img alt='poster'></img>
-            <h3>{title}</h3>
-            <p>{vote_average} / 10</p>
-            <p>{overview}</p>
+
+            <div className="movie-info">
+                <img className="poster" src={`${IMAGE_BASE_URL + POSTER_SIZE + poster_path}`} alt='poster'></img>
+                
+                <div className="details">
+                    <h3>{title} <span>{vote_average} / 10</span></h3>                   
+                    <p>{overview}</p>
+                </div>
+            </div>
+
         </section>
     )
 }
