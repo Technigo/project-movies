f3cc2a28b663e9508e57326859366bfb25725b03
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-04 10:03:23 +0100

    Added alt text to fix deployment issue

--- code/src/components/NotFound.js
+++ code/src/components/NotFound.js
@@ -7,7 +7,7 @@ export const NotFound = () => {
   return (
     <section className="not-found">
       <h1>Oops! Bummer, We couldn't find that movie... <span role="img" aria-label="shy emoji">&#128517;</span></h1>
-      <img src="https://thumbs.gfycat.com/UnpleasantFancyFairyfly-size_restricted.gif" />
+      <img src="https://thumbs.gfycat.com/UnpleasantFancyFairyfly-size_restricted.gif" alt="funny popcorn gif"/>
       <h2>Check the most popular movies and grab that pop corn!</h2>
       <Link to="/" exact="true">
         <p><span role="img" aria-label="movies emoji">&#127871;</span>Back to Popular Movies<span role="img" aria-label="movies emoji">&#127871;</span></p>
