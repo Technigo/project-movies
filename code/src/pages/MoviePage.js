c523aedccbef7ea12dbea867369d33fc56f9969f
Sofia Sousa <sofiavazsousa@gmail.com>
2020-11-04 15:30:16 +0100

    Add main styling movie page

--- code/src/pages/MoviePage.js
+++ code/src/pages/MoviePage.js
@@ -22,11 +22,19 @@ return (
     <Link to ="/" exact='true'>
 '      <p>Movies List</p>
 '    </Link>
-          <section className="movie-page" style={{backgroundImage:`url('https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}')`}}>
-            <img src= {`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title}/>
-            <h2>{movie.original_title}</h2>
-            <p className="movie-rating">{`${movie.vote_average} /10`}</p>
-            <p className="movie-overview">{movie.overview}</p>
+          <section className="movie-page">
+            <div 
+              className="movie-page-background" 
+              style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 10%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
+              <div className="movie-page-details">
+                <img className="movie-poster" src= {`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.original_title}/>
+                <div class="details-container">
+                  <h2>{movie.original_title}</h2>
+                  <p className="movie-rating">{`${movie.vote_average} /10`}</p>
+                  <p className="movie-overview">{movie.overview}</p>
+                </div>
+              </div>
+            </div>
           </section> 
   </>
   );
