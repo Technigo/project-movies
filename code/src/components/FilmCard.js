46ffee1d0347233aea198fa87d69f93e768c1823
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-04 22:56:31 +0100

    Updated FilmCard.js: 1. Added a div for the h1 and p tags. 2. And added a div for the p tag.

--- code/src/components/FilmCard.js
+++ code/src/components/FilmCard.js
@@ -13,8 +13,13 @@ export const FilmCard = ({
         <Link to={`/films/${id}`}>
             <article className="film-card">
                 <div className="film-card-overlay">
-                    <h1>{title}</h1>
-                    <p>Released {release_date}</p>
+                    <div className="title-button">
+                        <h1 className="title">{title}</h1>
+                        <svg className="arrow" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                    </div>
+                    <div>
+                        <p className="released">Released {release_date}</p>
+                    </div>
                 </div>
                 <img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt="{title} film poster" />
             </article>
