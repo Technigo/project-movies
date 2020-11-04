0f8cda03c4bb159b052d78256bb82629511320be
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-04 11:12:44 +0100

    Styling for loading page done

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import 'components/loading.css';
+
 export const Loading = () => {
   return (
-    <h1>Loading... ...</h1>
+    <section className="loading">
+      <img src="https://cdn.dribbble.com/users/848623/screenshots/3796170/corn1.gif" alt="popcorn animation" />
+      <p>Loading...</p>
+      <p>Get that corn popping!</p>
+    </section>
   )
 };
