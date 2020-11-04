c184899ed6cde7bb24c761fd083c5a66de7a7b8f
Max Forssén <max.forssen@mentimeter.com>
2020-11-04 17:46:57 +0100

    added back button and position, fixed position on movie details to bottom

--- code/src/ShowMovie.js
+++ code/src/ShowMovie.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-
+import { Route, Link} from 'react-router-dom'
 
 
 
@@ -22,14 +22,26 @@ export const ShowMovie = () => {
         console.error('Request failed', error)
     })
 
-    }, [])
+    }, [id])
 
     return  (
-        <div style={{ backgroundImage: `URL(https://image.tmdb.org/t/p/w1280/${details.backdrop_path})` }} className="individual-movie-container">
-            <p>{details.original_title}</p>
-            <p>Rating: {details.vote_average} / 10</p>
-            <p>{details.tagline}</p>
-            <p>{details.overview}</p>
+        <div style={{ backgroundImage: `URL(https://image.tmdb.org/t/p/w1280/${details.backdrop_path})` }} className="backdrop-container">
+            
+                <div className="button-container">
+                    <Route path="/:id">
+                        <Link to="/">
+                            <img src="https://img.pngio.com/back-button-free-arrows-icons-back-button-png-512_512.png" alt="back button" className="back-button" />
+                        </Link>
+                    </Route>
+                </div>
+                <div className="movie-details">
+                    <div>
+                        <p>{details.original_title}</p>
+                        <p>Rating: {details.vote_average} / 10</p>
+                        <p>{details.tagline}</p>
+                        <p>{details.overview}</p>
+                    </div>
+                </div>
         </div>
 
     )
