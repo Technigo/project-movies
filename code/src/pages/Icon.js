93d82487789f19dce51445839862e1f82be552bf
LINDA HINTZE <lindahintze@hotmail.com>
2020-11-04 16:09:44 +0100

    styled details page and added link to homepage

---
+++ code/src/pages/Icon.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Icon = () => {
+  return(
+    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+      <g>
+        <title>background</title>
+        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+      </g>
+      <g>
+        <title>Layer 1</title>
+        <path fill="#ffffff" id="svg_1" d="m8,0c-4.418,0 -8,3.582 -8,8s3.582,8 8,8s8,-3.582 8,-8s-3.582,-8 -8,-8zm2.354,12.646l-0.707,0.707l-5.354,-5.353l5.354,-5.354l0.707,0.707l-4.647,4.647l4.647,4.646z"/>
+      </g>
+    </svg>
+  )
+}
--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './MovieDetails.css'
 import { useParams } from 'react-router-dom'
+import { Icon } from './Icon.js'
+import { Link } from 'react-router-dom'
 
 export const MovieDetails = () => {
 
   const { id } = useParams()
 
   const DETAILS_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=e2eb26a39cdd68b3570a5c1b62c9c638&language=en-US`
+ 
   const [details, setDetails] = useState([])
 
   useEffect (() => {
@@ -18,12 +21,18 @@ export const MovieDetails = () => {
   console.log(details) // - don't forget to remove
 
   return (
-    <section className='movie-details'>
-      
-      //<img src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`} alt={details.title}/>
-      //<h2>{details.title}</h2>
-      //<p>{details.vote_average}</p>
-      //<p>{details.overview}</p>
+    <section className='movie-details'style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${details.backdrop_path})`}}>
+      <Link className='homepage-link' to='/'>
+        <Icon/>
+        <p className='link-title'>Movies</p>
+      </Link>
+      <div className='details-summary'>
+        <img className="details-image" src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`} alt={details.title}/>
+        <div className ="details-text">
+          <h2>{details.title}<span>{details.vote_average}/10</span></h2>
+          <p>{details.overview}</p>
+        </div>
+      </div>
     </section>
   )
 }
