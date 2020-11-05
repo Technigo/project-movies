9ee756b9884151ff1ff4141b654fde9728a3df77
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-11-05 14:33:38 +0100

    made movie thumbnail comp, imported in to Movielist, working

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from "react";
+import { MovieThumb } from "components/MovieThumb";
 
 export const MovieList = () => {
-    
-   const MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=9dbb13757543047a90651a50d540380e&language=en-US&page=1";
-   const[movies, setMovies] = useState([]);
+  const MOVIES_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=9dbb13757543047a90651a50d540380e&language=en-US&page=1";
+  const [movies, setMovies] = useState([]);
 
+  console.log('RENDER');
+
+  useEffect(() => {
     fetch(MOVIES_URL)
-    .then(response => response.json())
-    .then((json) => setMovies(json.results));
+      .then((response) => response.json())
+      .then((json) => setMovies(json.results));
+  }, []);
+  return (
+    <section className="movie-list">
+      {movies.map((movie) => (
+       <MovieThumb  />
+      ))}
+     
+    </section>
+  );
+};
 
-    return (    
-       <section className="movie-list">
-
-      {movies.map((movie) => movie.original_title)}
-      </section>
-      
-      )
-
-
-  }
-
-
-
-        // to render something for each movie need Map, we have to store the jsoan/list of movies so useState is needed, variabel for rendering and and a function to set that variabel.
-        // initial value fro list of movies lika med [].
-        // want to render same comp/html for each Gamepad, but use specific info for each Gamepad.apply, Mag gives us ability to put out muliply pices och info.
+// key={id}  to render something for each movie need Map, we have to store the jsoan/list of movies so useState is needed, variabel for rendering and and a function to set that variabel.
+// initial value fro list of movies lika med [].
+// want to render same comp/html for each Gamepad, but use specific info for each Gamepad.apply, Mag gives us ability to put out muliply pices och info.
+// useffect = only do this one time waint until dependencis scahnge thaen do thia again.....
---
+++ code/src/components/MovieThumb.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+export const MovieThumb = () => {
+
+return (  'MovieThumb'
+
+    // <section className="game-thumb">
+
+    //     </section>
+
+)
+}
+
+
+
+
+
+
+
+
+
+//   name,
+//   background_image,
+//   rating,
+//   dominant_color,
+//   slug,
+// }) => {
+//   return (
+//     <Link to={`/games/${slug}`}>
+//       <section className="game-thumb">
+//         <span>
+//           {name} - {dominant_color}
+//         </span>
+//         <div className="game-thumb-image-wrapper">
+//           <img className="game-thumb-image" src={background_image} alt={name} />
+//           <p className="game-thumb-image-description">{rating}</p>
+//         </div>
+//       </section>
+//     </Link>
+//   );
+// };
