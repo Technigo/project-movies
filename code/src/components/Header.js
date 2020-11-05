329a22902c36c7ea5fc97ba22476961926f63d5a
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-05 18:35:27 +0100

    1. Index.css, FilmCard.css and Header.css: Updated with some changes to styling. 2. Header.js: Added a H1. 3. FilmDetail.css: Added styling for the loader. 4. FilmDetail.js: Added the loading useState and then the if statement for what happens whilst the fetch is processing. It's set to false when the fetch is completed. 5. NotFound.css: Updated the styling as added an svg. 6. NotFound.js: Added svg and h3. 7. Added movie.svg to image file.

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -7,8 +7,9 @@ export const Header = () => {
     const logo = "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg";
 
     return (
-        <header>
-            <img src={logo} alt="imdb logo" className="logo"></img>
+        <header>            
+                <h1 className="H1">Top 20 Ryan Gosling films</h1>
+                <img src={logo} alt="imdb logo" className="logo"></img>           
         </header>
     )
 }
--- code/src/pages/FilmDetail.js
+++ code/src/pages/FilmDetail.js
@@ -8,6 +8,7 @@ import './FilmDetail.css'
 export const FilmDetail = () => {
     const { filmid } = useParams();
 
+    const [loading, setLoading] = useState(true);
     const [FilmDetails, setFilmDetails] = useState({}); 
 
     const fetchFilmId = (filmid) => {
@@ -20,6 +21,7 @@ export const FilmDetail = () => {
             })
                 .then((data) => {
                     setFilmDetails(data);
+                    setLoading(false);
                 })
                 .catch((error) => {
                     console.error(error);
@@ -30,11 +32,22 @@ export const FilmDetail = () => {
         fetchFilmId(filmid);        
     }, [filmid]);
 
+    if (loading === true) {
+        return (
+            <article className="loader-article">
+                <div className="loader-container">
+                    <svg className="film" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M18 4l2 3h-3l-2-3h-2l2 3h-3l-2-3H8l2 3H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4zm-6.75 11.25L10 18l-1.25-2.75L6 14l2.75-1.25L10 10l1.25 2.75L14 14l-2.75 1.25zm5.69-3.31L16 14l-.94-2.06L13 11l2.06-.94L16 8l.94 2.06L19 11l-2.06.94z"/></svg>            
+                    <h3>Loading ...</h3>
+                </div>
+            </article>
+        );          
+    }
+
     const posterImage = `https://image.tmdb.org/t/p/w342${FilmDetails.poster_path}`;
     const backgroundImage = `https://image.tmdb.org/t/p/w1280${FilmDetails.backdrop_path}`;
 
     return (
-        <>
+        <> 
             <article className="film-details-container">
                 <Link to="/" className="back-link">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
