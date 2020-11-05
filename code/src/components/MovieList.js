b4cbc706290e097dd78c0578261bf8e49a5f5365
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-11-05 13:59:25 +0100

    got the map function to work, tried showing orig title etc

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 export const MovieList = () => {
     
-    const MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=9dbb13757543047a90651a50d540380e&language=en-US&page=1";
-   
+   const MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=9dbb13757543047a90651a50d540380e&language=en-US&page=1";
+   const[movies, setMovies] = useState([]);
 
     fetch(MOVIES_URL)
     .then(response => response.json())
-    .then((json) => console.log(json.results))
+    .then((json) => setMovies(json.results));
 
     return (    
-       <section>
-      movies goes here
+       <section className="movie-list">
+
+      {movies.map((movie) => movie.original_title)}
       </section>
       
       )
@@ -22,4 +23,6 @@ export const MovieList = () => {
 
 
 
-        
+        // to render something for each movie need Map, we have to store the jsoan/list of movies so useState is needed, variabel for rendering and and a function to set that variabel.
+        // initial value fro list of movies lika med [].
+        // want to render same comp/html for each Gamepad, but use specific info for each Gamepad.apply, Mag gives us ability to put out muliply pices och info.
