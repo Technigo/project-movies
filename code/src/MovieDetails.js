d36d9ec275046766af90df6b15ef6cfd3cb617ee
Therese <64402455+Thessan@users.noreply.github.com>
2020-11-05 17:16:45 +0100

    deleted map from MovieDetails

--- code/src/MovieDetails.js
+++ code/src/MovieDetails.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
-
 export const MovieDetails = () => {
     const { movieID } = useParams()
-    const [movieDetails, setMovieDetails] = useState([])
+    const [movieDetails, setMovieDetails] = useState()
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/=${movieID}?api_key=b230af0ba5dd0959429a398c958e6e4e&language=en-US&page=1`)
@@ -16,20 +15,18 @@ export const MovieDetails = () => {
 
     return (
         <div className="movieContainer">
-            {movieDetails.map((movieDetails) => ( 
                 <>
                 <div key={movieDetails.id}>
                 <div className="movieDetails1">
-                    <img className="movieDetailsThumbnail" src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`} alt="{movieDetails.title}" />
+                    <img className="movieDetailsThumbnail" src={`https://image.tmdb.org/t/p/w300=${movieDetails.poster_path}`} alt="{movieDetails.title}" />
                 </div>
                     <div className="movieDetails2">
                         <h2 className="movieTitle2"> {movieDetails.title}</h2>
                             <p className="averageVote"> {movieDetails.vote_average} / 10 </p>
                                 <p className="movieDescription"> {movieDetails.overview} </p>
                     </div>
-                    </div>
+                </div>
                 </>
-            ))} 
         </div>
         
     )
