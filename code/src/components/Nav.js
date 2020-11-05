cced4119016b36e71eeaf77977bbd32e784c1e7d
jonna ru <jonna.ru@icloud.com>
2020-11-05 13:07:42 +0100

    menu-icon + cleaning CSS files

--- code/src/components/Nav.js
+++ code/src/components/Nav.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 import useDetectOutsideClick from "./UseDetectOutsideClick";
 import "./nav.css";
 
-const Nav2 = () => {
+// Found how to do this drop down here:
+// https://letsbuildui.dev/articles/building-a-dropdown-menu-component-with-react-hooks
+
+const Nav = () => {
   const dropdownRef = useRef(null);
   const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
   const onClick = () => setIsActive(!isActive);
@@ -12,7 +15,27 @@ const Nav2 = () => {
     <div className="container">
       <div className="menu-container">
         <button onClick={onClick} className="menu-trigger">
-          <img src="https://www.clipartmax.com/png/middle/77-773806_call-610-465-white-hamburger-menu-icon-png.png" />
+          <svg
+            className="icon"
+            id="Capa_1"
+            data-name="Capa 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 124 112"
+          >
+            <title>menu-button</title>
+            <path
+              d="M112,6H12a12,12,0,0,0,0,24H112a12,12,0,0,0,0-24Z"
+              transform="translate(0 -6)"
+            />
+            <path
+              d="M112,50H12a12,12,0,0,0,0,24H112a12,12,0,0,0,0-24Z"
+              transform="translate(0 -6)"
+            />
+            <path
+              d="M112,94H12a12,12,0,0,0,0,24H112a12,12,0,0,0,0-24Z"
+              transform="translate(0 -6)"
+            />
+          </svg>
         </button>
         <nav
           ref={dropdownRef}
@@ -21,22 +44,22 @@ const Nav2 = () => {
           <ul>
             <li>
               <Link to="/">
-                <h6>Popular</h6>
+                <h3>Popular</h3>
               </Link>
             </li>
             <li>
               <Link to="/top">
-                <h6>Top Rated</h6>
+                <h3>Top Rated</h3>
               </Link>
             </li>
             <li>
               <Link to="/new">
-                <h6>New releases</h6>
+                <h3>New releases</h3>
               </Link>
             </li>
             <li>
               <Link to="/upcoming">
-                <h6>Upcoming</h6>
+                <h3>Upcoming</h3>
               </Link>
             </li>
           </ul>
@@ -46,4 +69,4 @@ const Nav2 = () => {
   );
 };
 
-export default Nav2;
+export default Nav;
