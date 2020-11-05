4ac673a05904a8962477cc9281dcb3c4df7eff9d
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-05 16:41:01 +0100

    Updated App.js: 1. Added react router redirect. 2. Imported header component. 3. Changed the path of the FilmList to pages as I moved it to there. 4. Imported the NotFound component. 5. Added the header component tag into the browserrouter tags. 6. Added a new route for the NotFound component with a path of 404. 7. Added the redirect which redirects if the user inputs an incorrect url to this page which links back to the filmList page.

--- code/src/App.js
+++ code/src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import { FilmList } from 'components/FilmList';
+import { Header } from 'components/Header';
+import { FilmList } from 'pages/FilmList';
 import { FilmDetail } from 'pages/FilmDetail';
+import { NotFound } from 'pages/NotFound';
+
 
 export const App = () => {
 
   return (
     <>
     <BrowserRouter>
-      <main>        
+      <Header /> 
+      <main>       
         <Switch>            
           <Route path="/" exact>
             <FilmList />          
           </Route>
-          <Route path="/films/:filmid" exact>
+          <Route path="/films/:filmid">
             <FilmDetail />          
           </Route>
+          <Route path="/404">
+            <NotFound />          
+          </Route>
+          <Redirect to="/404"
+          />
         </Switch>      
       </main>
     </BrowserRouter>
     </>
   );
 };
+
+/*NotFound is a new page that the user get's redirected to if they enter a non exact url. The <redirect/> makes the redirect happen to the NotFound component/page. React will compare which paths have been specified so you don't need to write the from="/path name" attribute for the redirect */
