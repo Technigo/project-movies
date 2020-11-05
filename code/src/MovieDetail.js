5c8b73a675554a47185adab7f69a0f0bf438c9e2
Olof Lindahl <olindahl@gmail.com>
2020-11-05 08:49:19 +0100

    all functionality blue level in place

--- code/src/MovieDetail.js
+++ code/src/MovieDetail.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import {ReactComponent as BackLogo} from "./images/backLogo.svg";
+import { useParams, useHistory, Link } from "react-router-dom";
+
 
 export const MovieDetail = () => {
   const { movieId } = useParams();
   const [detail, setDetail] = useState({});
+  const history = useHistory();
+
+  console.log(history)
 
   const MOVIE_URL =
     "https://api.themoviedb.org/3/movie/{movie_id}?api_key=a00dc273fb1eaa2bb4a4e6fed9fe4289&language=en-US";
@@ -16,6 +21,8 @@ export const MovieDetail = () => {
       .then((data) => setDetail(data));
   }, [movieId]);
 
+  const handleClick = () => history.push("/")
+
   return (
     <section className="detail-page">
       <div
@@ -24,6 +31,9 @@ export const MovieDetail = () => {
           backgroundImage: `url(https://image.tmdb.org/t/p/w1280${detail.backdrop_path})`,
         }}
       >
+<Link className="back-link" onClick={handleClick}><BackLogo style={{ width: "30px" }}/>
+      Movies
+    </Link>
         <div className="detail-summary">
           <img
             className="detail-poster"
