a199d0d5716968afef766780e2c21fd8b0a270bd
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-11-05 21:56:08 +0100

    title on top now

--- code/src/components/MovieThumb.js
+++ code/src/components/MovieThumb.js
@@ -6,7 +6,7 @@ export const MovieThumb = ({name, background_image, voted, key}) => {
   <Link to={`/movie/${key}`}>
    <section className="movie-thumb">
      <h1 className="movie-name">{name}</h1> 
-      <div className="movie-thumb-image_wrapper">
+      <div className="movie-thumb-image-wrapper">
     <img className="movie-thumb-image" src={`http://image.tmdb.org/t/p/w300/${background_image}`} alt={name}/>
 <p className="movie-thumb-image-voted">{voted} ⭐️ </p>
       </div>
