bda63f3da29eb00caa9fc49ccc99c2c94808bd85
Sara Sundblom <sundblomsara@gmail.com>
2020-11-05 11:23:20 +0100

    made background poster gradient so summary will be easier to read

--- code/src/MovieDetails.js
+++ code/src/MovieDetails.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import "./css/MovieDetails.css"
+import { BackButton } from 'BackButton.js'
 
-export const MovieDetails = ({ title, overview, vote_average, backdrop_path, poster_path }) => {
+export const MovieDetails = ({ title, overview, vote_average, backdrop_path, poster_path, backbutton }) => {
     const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p'
     const BACKDROP_SIZE = '/w1280'
     const POSTER_SIZE = '/w300'
@@ -12,15 +13,16 @@ export const MovieDetails = ({ title, overview, vote_average, backdrop_path, pos
 
     return (
         <section className='movie-details'>
-            <img className="background" src={`${IMAGE_BASE_URL + BACKDROP_SIZE + backdrop_path}`} alt='backdrop'></img>
-
+            <div className="overlay">
+                <img className="background" src={`${IMAGE_BASE_URL + BACKDROP_SIZE + backdrop_path}`} alt='backdrop'></img>
+            </div>
 
             <div className="movie-info">
                 <img className="poster" src={`${IMAGE_BASE_URL + POSTER_SIZE + poster_path}`} alt='poster'></img>
 
                 <div className="details">
-                    <Link to='/'>
-                        <p className="back-button">Movies</p>
+                    <Link className="back-button" to='/'>
+                        <BackButton /> Movies
                     </Link>
 
                     <h3>{title} <span className="vote">{vote_average} / 10</span></h3>
