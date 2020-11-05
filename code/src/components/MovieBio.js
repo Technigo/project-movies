15c865635b3829123780fc60692bb75091257d16
Moa Blomkvist <moa@birgerochblom.se>
2020-11-05 22:03:36 +0100

    style moviedetails

--- code/src/components/MovieBio.js
+++ code/src/components/MovieBio.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './moviebio.css'
 
-export const MovieBio = ({ title, poster_path, overview }) => {
+export const MovieBio = ({ title, vote_average, poster_path, overview }) => {
   return (
 <article className="bio-section">
   <img className="poster-detail-image" src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt={title}/>
-  <h2>{title}</h2>
-  <p>{overview}</p>
+  <div className="bio-container">
+    <h1 className="detail-title">{title} <span className="rating">{vote_average}/10</span> </h1>
+    <p className="detail-overview">{overview}</p>
+  </div>
 </article>
 )};
