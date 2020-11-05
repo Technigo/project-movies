525a6f2e4d37a0f43f39d835bc0c8237a4b91214
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2020-11-05 10:19:24 +0100

    added styling to notfound page

--- code/src/pages/NotFound.js
+++ code/src/pages/NotFound.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
 
   return (
-    <div className="not-found-container">
-      <h2>Oops! The page you're looking for couldn't be found.</h2>
-      <Link to='/'>
-        <img className='back-arrow' alt='back arrow' src='../left-arrow.svg'/>
-        Movies
-      </Link>
+    <div className='not-found-container'>
+      <div className='not-found-wrapper'>
+        <h2>Oops! The page you're looking for is not here..</h2>
+        <Link className='back-link' to='/'>
+          <img className='back-arrow' alt='back arrow' src='../left-arrow.svg'/>
+          Movies
+        </Link>
+      </div>
     </div>
   )
 }
