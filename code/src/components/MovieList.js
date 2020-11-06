ced24fb462a81b481ccaa95259be4fd3f717fc67
MindstormingAB <s.elander@mindstorming.se>
2020-11-06 19:27:12 +0100

    NotFound and Loading on MovieDetails level

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 import MovieCard from './MovieCard';
+import Loading from './Loading';
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // const api_key = '54ee7f54ed9c2538cd37d709a09a6ca2';
   // const POPULAR_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
@@ -21,19 +23,28 @@ const MovieList = () => {
   };
 
   useEffect(() => {
+    setLoading(true);
     fetchMovies();
   }, []);
 
-  return (
-    <section className="popular-page">
-      {movies.map(movie => (
-        <MovieCard
-          key={movie.id}
-          {...movie}
-        />
-      ))}
-    </section>
-  );
+  useEffect(() => {
+    setLoading(false);
+  }, [movies]);
+
+  if (loading) {
+    return <Loading />
+  } else {
+    return (
+      <section className="popular-page">
+        {movies.map(movie => (
+          <MovieCard
+            key={movie.id}
+            {...movie}
+          />
+        ))}
+      </section>
+    );
+  }
 };
 
 export default MovieList;
