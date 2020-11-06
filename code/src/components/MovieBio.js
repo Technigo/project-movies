9997ccbf0dcab06f4251785dd53a8d9ee011a049
Moa Blomkvist <moa@birgerochblom.se>
2020-11-06 14:38:05 +0100

    style

--- code/src/components/MovieBio.js
+++ code/src/components/MovieBio.js
@@ -7,7 +7,7 @@ export const MovieBio = ({ title, vote_average, poster_path, overview }) => {
 <article className="bio-section">
   <img className="poster-detail-image" src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt={title}/>
   <div className="bio-container">
-    <h1 className="detail-title">{title} <span className="rating">{vote_average}/10</span> </h1>
+  <h1 className="detail-title">{title} <span className="rating">{vote_average}/10</span> </h1>
     <p className="detail-overview">{overview}</p>
   </div>
 </article>
