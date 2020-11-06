79c3119ac992827fabc28ab2ceeb560c9b0fb2c0
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2020-11-06 22:24:28 +0100

    second commit

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,34 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+
+import './moviedetails.css'
 
 
 export const MovieDetails = () => {
+  
   const { movieId } = useParams();
 
-  // const API_KEY = "ab5cef50889fc06c1a1e75470eefe28b";
-  const URL_DETAILS =`https://api.themoviedb.org/3/movie/${movieId}?api_key=ab5cef50889fc06c1a1e75470eefe28b&language=en-US`;
+  const API_KEY = "ab5cef50889fc06c1a1e75470eefe28b";
+  const URL_DETAILS =`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
   
   const [movieDetails, setMovieDetails] = useState({});
-
 
   useEffect(() => {
     fetch(URL_DETAILS)
     .then(res => res.json())
-    .then(data => setMovieDetails(data));
+    .then(data => {
+      setMovieDetails(data)
+  });
   }, [URL_DETAILS,movieId]);
 
   return (
-    <section className="details_page">
-    <div className="details_backgrd" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }} alt={movieDetails.title}>
-      <div className="details_info">
-         {/* <img src={`(https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} alt={movieDetails.title} className="movie-poster" /> */}
-         <div className="details_description">
-            <h1>{movieDetails.title}</h1>
-            <p>{movieDetails.overview}</p>
-         </div>
-      </div>
+    <article className="details_page">
+     <Link className="backLink" to={`/`}>
+    <div className="details_background" 
+          style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }} 
+          alt={movieDetails.title}>
+      <div className="summary">
+         <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt={movieDetails.title} />
+           <div className="details_page_details">
+              <h1>{movieDetails.title}</h1>
+              <span className="rating">{movieDetails.vote_average}/10</span>
+              <p>{movieDetails.overview}</p>
+            </div>
+        </div>
     </div>
-    </section>
+      </Link>
+    </article>
   )
 };
 
--- code/src/pages/MoviesList.js
+++ code/src/pages/MoviesList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import './list.css'
+import './movielist.css'
 
 const POPLIST_API = "https://api.themoviedb.org/3/movie/popular?api_key=ab5cef50889fc06c1a1e75470eefe28b&language=en-US&page=1";
 
@@ -14,20 +14,20 @@ export const MoviesList = () => {
       .then(res => res.json())
       .then(data => {
         // console.log(data);
-        setMoviesList(data.results) //get list of movies frm api to set movies
+        setMoviesList(data.results) //get list of movies frm api and pute data in movielist state 
       });
   }, [])
 
   return (
-
+//map over state to render
     <div className="movies_list">
       {moviesList.map(({id, poster_path, title, release_date}) => (
         <div key={id} className="movie_wrapper">
           <Link key={moviesList.id} to={`movies/${id}`}>
             <img className="movies_list_img" src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt="movie poster" />  
-              <div className="movie_info">
-                <h2>{title}</h2>
-                  <p>Released:{release_date}</p>
+              <div className="movie_details">
+                <h1>{title}</h1>
+                  <p>Released {release_date}</p>
               </div>
           </Link>
         </div>
