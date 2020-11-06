6e4f3b7d807326c945a6aa392cfb72941842f842
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-11-06 14:26:45 +0100

    changed useEffect

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -10,41 +10,41 @@ import "components.css/movieDetails.css"
 export const MovieDetails = () => {
   const {id} = useParams();
 
-  const MOVIE_DETAILS_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=636ed5609cbdb1c9a92992698d99fea5`;
   const [movieDetails, setMovieDetails] = useState({});
   
-  const getMovieDetails = () => {
+  const getMovieDetails = (movieId) => {
+    const MOVIE_DETAILS_URL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=636ed5609cbdb1c9a92992698d99fea5`;
     fetch(MOVIE_DETAILS_URL)
       .then(response => response.json())
       .then(json => {
         setMovieDetails(json)
       })
   }
-  
-  useEffect (()=> {
-    getMovieDetails()
-  },[id]);  
 
-  // const movieMatch = movieDetails.find(movieDetail => movieDetail.id = id)
+  useEffect (()=> {
+    getMovieDetails(id)
+  },[id]);  
+console.log(movieDetails);
+  // const movieMatch = movieDetails.find(detail => detail.id = id)
   // if(!movieMatch) {
   //   return (
   //   <div>Not found</div>
   //   )
   // }
 
-return(
-  <article className="movie-details"> 
-    <div className="movie-details-background">
-      <img className="movie-details-backdrop-image" src={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`} alt={movieDetails.title}/>
-      <div className="movie-details-container">
-        <img className="movie-details-poster" src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`} alt={movieDetails.title}/>
-        <div className="movie-details-information">
-          <h1>{movieDetails.title}
+  return(
+    <article className="movie-details"> 
+      <div className="movie-details-background">
+        <img className="movie-details-backdrop-image" src={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`} alt={movieDetails.title}/>
+        <div className="movie-details-container">
+          <img className="movie-details-poster" src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`} alt={movieDetails.title}/>
+          <div className="movie-details-information">
+            <h1>{movieDetails.title}
             <span className="vote-red">{movieDetails.vote_average}</span></h1>
-          <p className="movie-details-overview">{movieDetails.overview}</p>
+            <p className="movie-details-overview">{movieDetails.overview}</p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</article>
+    </article>
 )
 }
