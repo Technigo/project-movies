ef9c881183460d025d8d1ab0f36ed67c952763db
idsintehittapa <jblom15@gmail.com>
2020-11-06 10:06:00 +0100

    added _redirects to get my custom NotFound page to work

--- code/src/pages/PageNotFound.js
+++ code/src/pages/PageNotFound.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Footer } from 'components/Footer'
 
 import { ReactComponent as Error } from "media/404_error.svg"
 
@@ -8,9 +9,12 @@ import './PageNotFound.css'
 
 export const PageNotFound = () => {
   return (
+    <>
     <section className='not-found-wrapper'>
       <h1 className='not-found-title'>This is not the movies you are looking for...</h1>
       <div className='error-icon'><Error /></div>
     </section>
+    <Footer />
+    </>
   )
 }
