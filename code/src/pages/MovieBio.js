0dd9265425726e330ae3a630eebcc1f2d9db2e5b
caroline birgersson <caroline@birgerochblom.se>
2020-11-06 18:40:53 +0100

    added media quierys

--- code/src/pages/MovieBio.js
+++ code/src/pages/MovieBio.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useHistory } from "react-router-dom";
 
 import { BackButton } from "components/js/BackButton";
 
@@ -9,6 +9,7 @@ export const MovieBio = () => {
   const { id } = useParams();
   const [bio, setBio] = useState({});
   const API_KEY = "88b00ce6c0c72d59de65e4b1fcce3a85";
+  const history = useHistory();
 
   useEffect(() => {
     const URL_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
@@ -23,13 +24,13 @@ export const MovieBio = () => {
   }, [id]);
 
   return (
-    <section className="about-section">
+    <section className="bio-section">
       <img
         className="background-image"
         src={`https://image.tmdb.org/t/p/original${bio.backdrop_path}`}
         alt={bio.title}
       ></img>
-      <Link className="back-button" to='/'>
+      <Link className="back-button" onClick={() => history.goBack()}>
         <BackButton />All movies
       </Link>
       <div className="content-container">
