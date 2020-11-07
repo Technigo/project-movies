096fcb0ed3482f3394a18f9a8f0a986607ce8f98
Evelina Berg <evelina.berg@outlook.com>
2020-11-07 18:07:17 +0100

    Poster image renders on detail page.

--- code/src/components/TvShowDetails.js
+++ code/src/components/TvShowDetails.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { backgroundImageURL } from '../urls.js'
+import { BACKGROUND_IMAGE_URL, POSTER_IMAGE_URL } from '../urls.js';
 
 
-export const TvShowDetails = ({ backdrop_path }) => {
+export const TvShowDetails = ({ name, backdrop_path, poster_path }) => {
     console.log(backdrop_path);
     return (
         <section 
           className="detail--container" 
           style={{
-            backgroundImage: `linear-gradient(rgba(0,0,0, .7) 75%, rgba(0,0,0,.9) 100%), 
-                              url(${backgroundImageURL}${backdrop_path}) `
-            }} >
- 
+          backgroundImage: `linear-gradient(rgba(0,0,0, .7) 75%, 
+                            rgba(0,0,0,.9) 100%), 
+                            url(${BACKGROUND_IMAGE_URL}${backdrop_path})`
+          }} 
+        >
+          <article className="detail_info--container">
+              <img
+                className="detal_info__image"
+                src={`${POSTER_IMAGE_URL}${poster_path}`}
+                alt={name}
+              />
+          </article>
         </section>
     )
 }
--- code/src/urls.js
+++ code/src/urls.js
@@ -2,7 +2,8 @@
 export const API_URL = 'https://api.themoviedb.org/3/tv/';
 
 export const API_IMG_URL = 'http://image.tmdb.org/t/p/';
-export const backgroundImageURL = `${API_IMG_URL}w1280`; 
+export const BACKGROUND_IMAGE_URL = `${API_IMG_URL}w1280`; 
+export const POSTER_IMAGE_URL = `${API_IMG_URL}w342`;
 
 
 
