3708ed312b3b9d4f3723988c3be909cec6c1d40a
mats-johansen <johansenmats89@gmail.com>
2020-11-07 14:40:58 +0100

    ffasda

--- code/src/MovieList.js
+++ code/src/MovieList.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { apiKey } from './Key';
 import { Link } from 'react-router-dom';
-
 import './movielist.css';
 
 export const MovieList = () => {
