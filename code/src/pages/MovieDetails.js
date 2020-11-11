1a065025be9f76fabd5ee132980fc7f6cae28a3e
trangtara <65237997+trangtara@users.noreply.github.com>
2020-11-11 21:09:17 +0100

    Update MovieDetails.js

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -14,7 +14,6 @@ const MovieDetails = () => {
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=fd00bf32c04d62eccaf294e2d6aa9fa6&language=en-US`)
-    
     .then((res) => res.json())
     .then((json) => {
       console.log("json", json)
