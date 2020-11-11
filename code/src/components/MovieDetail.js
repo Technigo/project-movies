143405d8abd05bf3d1cb07e21353d5d4c4def83e
Moa <63230455+mhammarback@users.noreply.github.com>
2020-11-11 17:37:10 +0100

    added changes after code review

--- code/src/components/MovieDetail.js
+++ code/src/components/MovieDetail.js
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { API_KEY } from '../key';
 import Icon from './back_button.svg'
 
+
 export const MovieDetail = () => {
     const { movieId } = useParams();
     const [movies, setMovies] = useState([]);
@@ -18,29 +19,32 @@ export const MovieDetail = () => {
         .catch((error) => {
             console.error('Request failed', error)
         })
-}, [movieId, DETAILS_API])
-
+  }, [movieId, DETAILS_API]);
 
 return(
 <>
-    <article className ="details-page">
-      <section className="detail-backdrop" style={{  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%),  url(https://image.tmdb.org/t/p/w1280${movies.backdrop_path})` }}>
-        <div className="movie-detail-content">  
-          <div className="movie-detail-back">
-            <Link to ="/" className="link" exact='true'>
-              <img className="back-icon" src={Icon} style={ {width:"20px"} }  alt="icon" /> 
-              <div className="back-text">Movies</div>
-            </Link>
-                <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`} alt={movies.title} /> 
-          </div>  
+  <article className ="details-page">
+    <section className="detail-backdrop" style={{  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%),  url(https://image.tmdb.org/t/p/w1280${movies.backdrop_path})` }}>
+      <div className="movie-detail-content">
+        <div className="movie-detail-back">
+          <Link to ="/" className="link" exact='true'>
+            <img className="back-icon" src={Icon} style={ {width:"20px"} }  alt="icon" /> 
+            <div className="back-text">Movies</div>
+          </Link>
+            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`} alt={movies.title} /> 
+        </div>  
           <div className="movie-detail-text"> 
-            <h1 className="movie-detail-title">{movies.title}
-            <span className="rating">{movies.vote_average}/10 </span></h1> 
-            <p className="overview">{movies.overview}</p>     
-          </div>
-        </div>
-      </section>
-    </article>  
+            <div className="movie-detail-title">
+              <a href={movies.homepage}>
+              <h1>{movies.title}</h1>
+              </a>
+              <span className="rating">{movies.vote_average}/10 </span> 
+            </div>
+              <p className="overview">{movies.overview}</p>     
+            </div>
+      </div>
+    </section>
+  </article>  
 </>
   );
 };
