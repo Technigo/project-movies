da33cc5ba39c74bc6ec8a57286a4e421ab8bd02e
Anna <hasselstromanna@gmail.com>
2020-11-19 09:03:10 +0100

    Typo check.

--- code/src/App.js
+++ code/src/App.js
@@ -5,7 +5,6 @@ import { PopularList } from 'pages/PopularList';
 import { MovieDetails } from 'pages/MovieDetails';
 import { NotFound } from 'pages/NotFound';
 
-
 export const App = () => {
   
   return (
@@ -15,14 +14,13 @@ export const App = () => {
           <Route path="/" exact>
             <PopularList />
           </Route>
-          <Route path="/title/:movieId">
+          <Route path="/title/:movieId" exact>
             <MovieDetails />
           </Route>
           <Route path="/404">
             <NotFound />
           </Route>
-          <Redirect
-            to="/404"
+          <Redirect to="/404"
           />
         </Switch>
       </BrowserRouter>
