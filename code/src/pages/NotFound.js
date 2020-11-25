9c49a0863eed5cfcee442a386f500b9bed4888cb
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-25 15:00:42 +0100

    changing out anchor tag for a Link tag

--- code/src/pages/NotFound.js
+++ code/src/pages/NotFound.js
@@ -1,11 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
     <div className="not-found-header"> WRONG WAY, <br /> GO BACK!
       <p className="not-found-smaller">This page doesn't exist.</p>
-      <p className="not-found"><a href="/" tabIndex="0">Take me back home.</a></p>
+      <p className="not-found">
+        <Link to="/">
+          Take me back home.
+        </Link>
+      </p>
     </div>
   )
 }
