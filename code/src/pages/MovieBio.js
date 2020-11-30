65d31c79f23a5367f653acbe2fc36f0263dd377b
caroline birgersson <caroline@birgerochblom.se>
2020-11-30 18:11:06 +0100

    changes after pull request

--- code/src/pages/MovieBio.js
+++ code/src/pages/MovieBio.js
@@ -1,53 +1,57 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link, useHistory } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
-import { BackButton } from "components/js/BackButton";
+import { BackButton } from "../components/js/BackButton";
 
 import "components/css/moviebio.css";
 
 export const MovieBio = () => {
+  const API_KEY = "88b00ce6c0c72d59de65e4b1fcce3a85";
+
   const { id } = useParams();
   const [bio, setBio] = useState({});
-  const API_KEY = "88b00ce6c0c72d59de65e4b1fcce3a85";
-  const history = useHistory();
 
   useEffect(() => {
     const URL_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
     fetch(URL_MOVIE_DETAILS)
-      .then((response) => response.json())
-      .then((json) => {
+      .then(response => response.json())
+      .then(json => {
         setBio(json);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error("Bad request", error);
       });
   }, [id]);
 
   return (
-    <section className="bio-section">
-      <img
-        className="background-image"
-        src={`https://image.tmdb.org/t/p/original${bio.backdrop_path}`}
-        alt={bio.title}
-      ></img>
-      <Link className="back-button" onClick={() => history.goBack()}>
-        <BackButton />
-        All movies
-      </Link>
-      <div className="content-container">
-        <img
-          className="poster-image"
-          src={`https://image.tmdb.org/t/p/w342${bio.poster_path}`}
-          alt={bio.title}
-        ></img>
-        <div className="text-container">
-          <h1>
-            {bio.title}
-            <span className="rating">{bio.vote_average}/10</span>
-          </h1>
-          <p className="movie-description">{bio.overview}</p>
-        </div>
-      </div>
-    </section>
+    <>
+      {bio && bio.backdrop_path && (
+        <section className="bio-section">
+          <img
+            className="background-image"
+            src={`https://image.tmdb.org/t/p/original${bio.backdrop_path}`}
+            alt={bio.title}
+          />
+          <Link className="back-button" to="/">
+            <BackButton />
+            All movies
+          </Link>
+          <div className="content-container">
+            <img
+              className="poster-image"
+              src={`https://image.tmdb.org/t/p/w342${bio.poster_path}`}
+              alt={bio.title}
+            />
+            <div className="text-container">
+              <h1>
+                {bio.title}
+                <span className="rating">{bio.vote_average}/10</span>
+              </h1>
+              <p className="movie-description">{bio.overview}</p>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
--- code/src/pages/MovieStartPage.js
+++ code/src/pages/MovieStartPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { MovieCard } from "../components/js/MovieCard";
 
-import "..//components/css/moviestartpage.css";
+import "../components/css/moviestartpage.css";
 
 export const MovieStartPage = () => {
   const API_KEY = "88b00ce6c0c72d59de65e4b1fcce3a85";
@@ -12,13 +12,13 @@ export const MovieStartPage = () => {
     const URL_MOVIES = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
     fetch(URL_MOVIES)
-      .then((response) => response.json())
-      .then((json) => setMovie(json.results));
+      .then(response => response.json())
+      .then(json => setMovie(json.results));
   }, []);
 
   return (
     <section className="movie-start-page">
-      {movies.map((movie) => (
+      {movies.map(movie => (
         <MovieCard key={movie.id} {...movie} />
       ))}
     </section>
