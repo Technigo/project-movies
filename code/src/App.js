c48e3dfd55e9937c2ee5fca4e028d0ea65cba336
Pauan8 <pauline.j.andersson@vgregion.se>
2021-03-29 20:57:45 +0200

    fetched popular movies and did css

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import PopularList from './components/PopularList'
+
+const API_KEY = "9e2d991bdc86d11d2a7e1e110ef63aac"
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <PopularList />
     </div>
   )
 }
