ad82396e71498fec51cecd24430dff3c5edd6c0a
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-30 11:24:47 +0200

    Added a new fetch on the MovieDetails page and displayed the movie title and poster image.

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-const MovieDetails = () => {
+const MovieDetails = ({ match }) => {
+  const [movieDetails, setMovieDetails] = useState({});
+
+  const API_DETAILS_URL = `https://api.themoviedb.org/3/movie/${match.params.movieID}?api_key=180969109de0f1c45ba2c80ba35bd8c7&language=en-US`;
+
+  useEffect(() => {
+    fetch(API_DETAILS_URL)
+      .then((response) => response.json())
+      .then((json) => setMovieDetails(json));
+  }, []);
+
+  console.log(movieDetails);
+
   return (
     <div>
-      <h1>Movie Details</h1>
+      <h1>{movieDetails.title}</h1>
+      <img
+        className="movie-thumb-img"
+        src={`https://image.tmdb.org/t/p/w780${movieDetails.poster_path}`}
+        alt={movieDetails.title}
+      />
     </div>
   );
 };
