fdf36c9c29754d6b1e9756bba770c3454044a101
Mir Albertioni <fethullah.dincer@gmail.com>
2021-03-30 16:35:29 +0200

    work in progress

--- code/src/pages/MovieDetail.js
+++ code/src/pages/MovieDetail.js
@@ -14,18 +14,36 @@ const MovieDetail = () => {
     console.log(movies)
     return (
         <div className='details-wrapper' 
-        style={{
-            backgroundImage: "url(https://image.tmdb.org/t/p/w1280" + movies.backdrop_path
-            
+            style={{
+                backgroundImage: "url(https://image.tmdb.org/t/p/w1280" + movies.backdrop_path
             }}
             >
-            <img src={`https://image.tmdb.org/t/p/w300${movies.poster_path}`} alt={movies.title} />
-            <h1>{movies.original_title}</h1>
-            <h3>{movies.overview}</h3>
-            <h4>{movies.vote_average}</h4>
             <Link to={`/`}>       
-                <button>BACK</button>
+                <div className="back-btn">
+                <div className="back">
+                <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z" fill="#fff" fill-rule="evenodd"></path></svg>
+                
+                </div>
+                </div>
             </Link>
+            <div className="mobile-version">
+                <img src={`https://image.tmdb.org/t/p/w300${movies.poster_path}`} alt={movies.title} />
+                <h1>{movies.original_title} <span>{movies.vote_average}/10</span></h1>
+                <p>{movies.overview}</p>
+            </div>
+
+            <div className="desktop-version">
+                <div className="information-wrapper">
+                    <div className="image-wrapper">
+                        <img src={`https://image.tmdb.org/t/p/w300${movies.poster_path}`} alt={movies.title} />
+                    </div>
+                    <div className="text-wrapper">
+                        <h1>{movies.original_title}<span>{movies.vote_average}/10</span></h1>
+                        <p>{movies.overview}</p>
+                    </div>
+                </div>
+            </div>
+
            
         </div>
     )
