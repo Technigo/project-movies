8b7d826ceea8d1e4e46fc879e0cf57202682d3f9
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-04-01 00:06:04 +0200

    Added some styling

--- code/src/components/MovieThumb.js
+++ code/src/components/MovieThumb.js
@@ -4,15 +4,15 @@ import {Link} from "react-router-dom"
 export const MovieThumb = ({name, poster_image, rating, released, id}) =>{
   return (
     <Link to={`/movies/${id}`}>
-    <section className="movie-thumb">
-      {name} - {released}
+    <section className="movie-thumb-container">
       <div className="movie-thumb-image-wrapper">
-        <img className="movie-thumb-image" src={`https://image.tmdb.org/t/p/w342${poster_image}`} alt={name}></img>
-        <p className="movie-thumb-iamge-description">{rating}</p>
+        <img className="movie-thumb-image" src={`https://image.tmdb.org/t/p/w500${poster_image}`} alt={name}></img>
+        <div className="text-container">
+          <h1 className="movie-title">{name}</h1> 
+          <p className="movie-released">Released {released}</p>
+        </div>
       </div>
     </section>
   </Link>
   );
 }
-
-/*`https://image.tmdb.org/t/p/w342${background_image}`*/
--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -6,34 +6,24 @@ export const MovieDetails = () => {
   const DETAILS_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=40162efa089492073c3b7dbba9617e0b&language=en-US`;
   const [movieDetails, setMovieDetails] = useState([]);
 
-  console.log(DETAILS_URL)
-  // try to add a specific id to the url and see what happens. And cosnole.log url
-
-
   useEffect(() => {
     fetch(DETAILS_URL)
     .then(response => response.json())
     .then(json => setMovieDetails(json))
-  }, [id]); //Have URL instead of id?
+  }, [id]);
 
   return(
     <> 
-    <section className="movies-list">
-    <div style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }}>
-    <img className="movie-thumb-image" src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt={movieDetails.title}></img>
-    {movieDetails.title} - {movieDetails.vote_average}
-      <div className="movie-thumb-image-wrapper">
-        <p className="movie-thumb-image-description">{movieDetails.overview}</p>
+      <div className="movie-detail-container" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }}>
+        <img className="movie-detail-image" src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt={movieDetails.title}></img>
+        <div className="detail-text-container">
+          <span className="title-headings">
+            <h1 className="movie-detail-title">{movieDetails.title}</h1>
+            <p className="movie-rating">{movieDetails.vote_average}/10</p>
+          </span>
+          <p className="movie-overview">{movieDetails.overview}</p>
+        </div>
       </div>
-    </div>
-    </section>
     </>
   )
-
 }
-
-
-/* name={movie.title}
-background_image={movie.poster_path}
-released={movie.release_date}
-id={movie.id} */
