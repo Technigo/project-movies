2c1f4b6c3699272b6e8a1e219f1843839d8df37f
mmolliss <malin.vannesjo@gmail.com>
2021-03-31 21:13:19 +0200

    Fixed gradient on details page

--- code/src/pages/MovieDetails.jsx
+++ code/src/pages/MovieDetails.jsx
@@ -25,11 +25,13 @@ export const MovieDetails = () => {
 
     }, [MOVIE_URL])
 
+    // https://image.tmdb.org/t/p/w342/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg
 
     return (
         <div >
-            
-            <img className='background-image' src={`https://image.tmdb.org/t/p/w1280${movies.backdrop_path}`}/>
+            <div className='background-image' style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/w1280${movies.backdrop_path})`
+            }}></div>
             <Link className="back-arrow" to={`/`}>
                 <FaChevronCircleLeft/>
                 <span className="icon-text">Movies</span>
