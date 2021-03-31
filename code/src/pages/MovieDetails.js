7a02b09453ea94cf37901db7a9312ab3e3ce9ea3
sirihemsley <siri.hemsley@gmail.com>
2021-03-31 10:04:56 +0200

    Changed temporaroly to including full url in fetch so that it works for now

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 
-import { DETAIL_API } from '../reusables/urls'
+// import { DETAIL_API } from '../reusables/urls'
 
 export const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState([])
 
   const { id } = useParams()
 
+  // useEffect(() => {
+  //   fetch(DETAIL_API({id}))
+  //     .then(res => res.json())
+  //     .then(json => setMovieDetails(json))
+  //     .catch(err => console.err(err))
+  // },[id])
+
   useEffect(() => {
-    fetch(DETAIL_API({id}))
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=bf4522dc489e8ffdfd36be08819077b1&language=en-US`)
       .then(res => res.json())
       .then(json => setMovieDetails(json))
       .catch(err => console.err(err))
