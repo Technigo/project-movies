76d7782837690c0d22129397ffcdaac27d836108
mmolliss <malin.vannesjo@gmail.com>
2021-04-01 22:10:55 +0200

    Fixed typo

--- code/src/components/MovieCard.jsx
+++ code/src/components/MovieCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import './MovieCard.css'
 
-export const MovieCard = ({ title, releaseData, posterPath }) => {
+export const MovieCard = ({ title, releaseDate, posterPath }) => {
   return (
     <>
       <img className="card-image" src={`https://image.tmdb.org/t/p/w500${posterPath}`} alt="" />
       <div className="title-date">
         <h1>{title}</h1>
-        <p>Released {releaseData}</p>
+        <p>Released {releaseDate}</p>
       </div>
     </>
   )
