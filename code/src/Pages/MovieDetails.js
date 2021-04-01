8809e8a597396c85328d9b47ad2ed01b39f5aada
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-04-01 20:35:44 +0200

    styled details page, movie poster container properly places, backdrop image adjusted

--- code/src/Pages/MovieDetails.js
+++ code/src/Pages/MovieDetails.js
@@ -17,27 +17,43 @@ useEffect(() => {
     
 }, [MOVIE_DETAILS_URL]);
 
+// if( details === undefined){
+//     return <></>;
+//   }
+
     return (
-        <section className="movie-details-container">
-            <Link to="/" className="BackButton">
+        <article className="movie-details-container">
+            <Link to="/" className="back-button">
                 <BackButton />
             </Link>
+                {/* <div
+                className="backdrop-image"
+                style={{backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), 
+                url(https://image.tmdb.org/t/p/original/${details.backdrop_path})`}} 
+                alt={details.title}
+                > */}
+
             <img 
+            className="backdrop-image"
             src={`https://image.tmdb.org/t/p/original/${details.backdrop_path}`} 
             alt={details.title} 
             />
-            <div className="poster-container">
-                <img 
-                className="poster-image"
-                src={`https://image.tmdb.org/t/p/original/${details.poster_path}`} 
-                alt={details.title} />
-                <div>
-                <h1>{details.title}</h1>
-                <p>{details.vote_average}/10</p>
-                <p>{details.overview}</p>
-                </div>
-            </div>
-        </section>
+                <div className="poster-container">
+                    <img 
+                    className="poster-image"
+                    src={`https://image.tmdb.org/t/p/original/${details.poster_path}`} 
+                    alt={details.title} />
+                    <div className="movie-infomation-container">
+                        <h1>
+                            {details.title} 
+                        <span className="movie-rating">
+                            {details.vote_average}/10
+                        </span>
+                        </h1>
+                        <p>{details.overview}</p>
+                    </div>
+                </div>            
+        </article>
         )
 }
 
