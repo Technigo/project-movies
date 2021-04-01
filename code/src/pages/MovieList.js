82ba1440675f4882baea0fc229922a2be6662fcb
sirihemsley <siri.hemsley@gmail.com>
2021-04-01 11:30:57 +0200

    Added more lists to the start page with own favorites

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { MOVIE_LIST_API, SIRI_LIST_API, IDA_LIST_API } from '../reusables/urls'
+
+
 import { MovieCard } from '../components/MovieCard'
-import { MOVIE_LIST_API } from '../reusables/urls'
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([])
+  const [sirisMovies, setSirisMovies] = useState([])
+  const [idasMovies, setIdasMovies] = useState([])
 
   useEffect(() => {
     fetch(MOVIE_LIST_API)
@@ -13,15 +17,50 @@ export const MovieList = () => {
       .then(json => setMovies(json.results))
       .catch(err => console.err(err))
   },[])
+  
+    useEffect(() => {
+      fetch(SIRI_LIST_API)
+        .then(res => res.json())
+        .then(json => setSirisMovies(json.items))
+        .catch(err => console.err(err))
+    },[])
+
+    useEffect(() => {
+      fetch(IDA_LIST_API)
+        .then(res => res.json())
+        .then(json => setIdasMovies(json.items))
+        .catch(err => console.err(err))
+    },[])
 
   return (
+    <>
     <section className="movie-list-wrapper">
-      {movies.map(movie => (
-        <Link key={movie.id} to={`/movies/${movie.id}`}>
+      <h2 className="list-title">Popular Movies</h2>
+      <section className="movie-list">
+        {movies.slice(0, 8).map(movie => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <MovieCard {...movie} />
+          </Link>
+        ))}
+      </section>
+      <h2 className="list-title">Siri's Favourites</h2>
+      <section className="movie-list">
+        {sirisMovies.map(movie => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
           <MovieCard {...movie} />
         </Link>
-      ))}
+        ))}
+      </section>
+      <h2 className="list-title">Ida's Favourites</h2>
+      <section className="movie-list">
+        {idasMovies.map(movie => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+          <MovieCard {...movie} />
+        </Link>
+        ))}
+      </section>
     </section>
+    </>
   )
 
 }
