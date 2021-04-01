93bf6fce358bcc761b65f3a892bb0940a0a242d0
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-04-01 12:32:05 -0600

    Added data  details to MovieDetail comp

--- code/src/pages/MovieDetail.js
+++ code/src/pages/MovieDetail.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect} from "react"
-import { useParams, Link } from "react-router-dom"
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
 
-//NEXT STEPS:
-//Adding details:
-// original_title, vote_average, poster_path, overview}
-
+import arrow from "../components/back-arrow.png"
 
 export const MovieDetail = () => {
-    const { id } = useParams()
-    const [movieDetail, setMovieDetail] = useState ()
-    const MOVIE_DETAIL_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=2c2891737dd5ffa64a8858cdba25ddfe&language=en-US`
+  const { id } = useParams()
+  const [movieDetail, setMovieDetail] = useState ([])
+  const MOVIE_DETAIL_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=2c2891737dd5ffa64a8858cdba25ddfe&language=en-US`
 
-    useEffect(() => {
-      fetch(MOVIE_DETAIL_URL)
-      .then((res) => res.json())
-      .then((json) => console.log(json))  
-    }, [MOVIE_DETAIL_URL])
-
-
+  useEffect(() => {
+    fetch(MOVIE_DETAIL_URL)
+    .then((res) => res.json())
+    .then((json) => {
+      setMovieDetail(json)
+      console.log(json) 
+    })
+  }, [MOVIE_DETAIL_URL])
+    
     return (
         <section className = "movie-detail">
-
-            <h1>Cool it's linked!</h1>
+          <div className="movie-details-container">
+            <h2 className="movie-title">{movieDetail.original_title}</h2>
+            <p className="movie-title">{movieDetail.vote_average}/10</p>
+            <p className="movie-title">{movieDetail.overview}</p>
+            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w300${movieDetail.poster_path}`} alt="movie poster"/>
+          </div>
         </section>
-        
     )
 }
