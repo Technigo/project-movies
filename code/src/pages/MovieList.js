7141fbc8af0fca79afaf0f8ef757b03038ba1537
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-04-01 10:52:48 +0200

    fix mobile view for details, remove comments, check indentation

--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom'
 
 import { POPULAR_API_URL } from '../reusable/urls'
 
-//backdrop: https://image.tmdb.org/t/p/w1280{movie.backdrop_path}
-//Poster: https://image.tmdb.org/t/p/w342{movie.poster_path}
-
 export const MovieList = () => {
     const [movieList, setMovieList] = useState([])
 
@@ -22,20 +19,19 @@ export const MovieList = () => {
         <>
         <section className="movie-list-container">
             {movieList.map((movie) => (
-                    <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-card-container">
-                        <img className="poster-image" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title}/>
-                            <div className="image-overlay-text">
-                                <h1>
-                                    {movie.original_title}
-                                </h1>
-                                <h2>
-                                    {movie.release_date}
-                                </h2>
-                            </div>
-                    </Link>
+                <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-card-container">
+                    <img className="poster-image" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.original_title}/>
+                        <div className="image-overlay-text">
+                            <h1>
+                                {movie.original_title}
+                            </h1>
+                            <h2>
+                                {movie.release_date}
+                            </h2>
+                        </div>
+                </Link>
             ))}
         </section>
         </>
     )
-
 }
