acaa3298c9f852e3ed45d1460aa1191c629cc674
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-04-02 10:59:59 +0200

    Rendered movie list on the page

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const App = () => {
+  const MOVIES_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=f8d93b3169765fb54ceb97a483ec4821&language=en-US&page=1'
+  const [movieList, setMovieList] = useState([])
+  
+  useEffect(() => {
+    fetch(MOVIES_URL)
+      .then((response) => response.json())
+      .then((json) => setMovieList(json.results))
+  },[])
+
+  console.log(movieList)
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <section>
+      {movieList.map((movie) => (
+        <div key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt='{movie.title}'/>
+          <h1>{movie.title}</h1>
+          <h3>{movie.release_date}</h3>
+        </div>
+      ))}
+    </section>
   )
+  
 }
