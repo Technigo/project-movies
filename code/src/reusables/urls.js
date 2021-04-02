b5e6714b32cab3c83f2262c8df789f66a630621c
Theresehag <tezzinthecity@gmail.com>
2021-04-02 23:37:41 +0200

    remove extra text in urls

--- code/src/reusables/urls.js
+++ code/src/reusables/urls.js
@@ -1,7 +1,2 @@
 const api_key='d54701f7b694b0188ac7819c5b24df16';
-<<<<<<< HEAD
 export const MOVIES_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
-=======
-export const MOVIES_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
-
->>>>>>> 0d623d263bcdaddd63f9a2c5352d6ec37af7ee19
