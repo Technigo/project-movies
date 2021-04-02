fb30415669e9b0d7cb2ea4450b1a825205be9ab0
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-04-02 13:48:25 +0200

    Started with the companies pages

---
+++ code/src/pages/CompanyPage.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+
+import {useParams} from 'react-router-dom'
+
+const CompanyPage = () => {
+  return (
+    <div>Company</div>
+  )
+}
+
+export default CompanyPage
---
+++ code/src/pages/MovieList.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+import { URL } from '../reusables/urls'
+
+import MovieThumb from '../components/MovieThumb.js'
+
+const MovieList = ({ movieListType }) => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(URL(movieListType))
+      .then(response => response.json())
+      .then(receivedMovies => setMovies(receivedMovies.results))
+  }, [movieListType])
+  console.log(movies)
+  return (
+    <div className="movies-container">
+      {movies.map(movie => <MovieThumb key={movie.id} {...movie} />)}
+
+    </div>
+  )
+}
+
+export default MovieList;
