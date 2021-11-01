47b5ea17bc53d05bdd423b71ca98fb8d2fc4ee14
Sabrina Zancotti <zancotti@gmail.com>
2021-11-01 18:25:57 +0100

    Added opacity on the poster container on onMouseEnter.

--- code/src/components/MovieList/MovieListItem.js
+++ code/src/components/MovieList/MovieListItem.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieListItem.css";
 
 export const MovieListItem = ({ movie, imageInformation }) => {
+  const [imageOpacityStyle, setImageOpacityStyle] = useState({ opacity: 1 });
   console.log(imageInformation);
   const imagePath = `${imageInformation.base_url}${imageInformation.poster_sizes[6]}${movie.poster_path}`;
   console.log(imagePath);
+
   return (
     <>
-      <div className="poster-container">
-        <img src={imagePath} alt={`poster for ${movie.original_title}`}></img>
+      <div
+        className="poster-container"
+        onMouseEnter={() => setImageOpacityStyle({ opacity: 0.4 })}
+        onMouseLeave={() => setImageOpacityStyle({ opacity: 1 })}
+      >
+        <img
+          src={imagePath}
+          alt={`poster for ${movie.original_title}`}
+          style={imageOpacityStyle}
+        ></img>
       </div>
     </>
   );
