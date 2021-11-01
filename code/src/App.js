34568b69fda3332249156e816709a28dd2ed18f2
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-11-01 14:58:50 +0100

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { API_LIST } from './utils/urls'
 
 export const App = () => {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetch(API_LIST)
+      .then((res) => res.json())
+      .then((data) => setList(data.results))
+  }, [])
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <section className='movies-container'>
+      {list.map((movie) => (
+        <div className='movie-card' key={movie.id}>
+          {console.log(movie)}
+          <img
+            src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <div className='overlay'>
+            <div className='movie-data'>
+              <h1>{movie.title}</h1>
+              <p>Released {movie.release_date}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
   )
+
+  // src={`https://image.tmdb.org/t/p/w780${movie.backdrop_path}`}
 }
---
+++ code/src/utils/urls.js
@@ -0,0 +1,2 @@
+const API_KEY = '1cd9c12b0f59437cb1f892337285c32e'
+export const API_LIST = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
