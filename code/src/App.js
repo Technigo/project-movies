acb53f6a4bc36492be8d443e0bdc002d7b464fd0
Gustav Frid <gustav.frid@gmail.com>
2021-11-01 20:31:26 +0100

    rendering movies images

--- code/src/App.js
+++ code/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MovieList } from './components/MovieList'
 import { MovieDetails } from './components/MovieDetails'
 import { API_URL } from './utils/url'
 
 export const App = () => {
-  console.log(API_URL)
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(res => {
+        setMovies(res.results)
+        console.log(res.results)
+      })
+  }, [])
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' render={() => <MovieList />} />
+        <Route exact path='/' render={() => <MovieList movies={movies} />} />
         <Route path='/details' render={() => <MovieDetails />} />
       </Switch>
     </BrowserRouter>
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { POSTER_URL } from 'utils/url'
 
-export const MovieList = () => {
-  return <div>Movies</div>
+export const MovieList = ({ movies }) => {
+  return (
+    <section style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {movies.map(movie => (
+        <Link to='/' key={movie.id}>
+          <img src={POSTER_URL(movie.poster_path)} alt={movie.title} />
+        </Link>
+      ))}
+    </section>
+  )
 }
