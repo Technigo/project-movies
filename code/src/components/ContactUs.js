b275bf6a1d595c505625e7b57c8565dc99becad0
MarySnopok <marysnopok8@gmail.com>
2021-11-01 16:03:46 +0100

    adding rel=noopener noreferrer to the link

--- code/src/components/ContactUs.js
+++ code/src/components/ContactUs.js
@@ -6,11 +6,11 @@ export const ContactUs = () => {
   return (
     <div className="contact-us">
       Developed by
-      <a className="github-link" href="https://github.com/madeleinesvensson">
+      <a className="github-link" href="https://github.com/madeleinesvensson" target="_blank" rel="noopener noreferrer">
         <span>Madeleine Svensson</span>
       </a>
       <span>&</span>
-      <a className="github-link" href="https://github.com/MarySnopok">
+      <a className="github-link" href="https://github.com/MarySnopok" target="_blank" rel="noopener noreferrer">
         <span>Mary Snopok</span>
       </a>
     </div>
