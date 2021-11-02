a3ca5d4c94e8bc33cb97492b8555770d52da12a8
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-11-02 10:17:49 +0100

    update MovieDetails.js

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 const MovieDetails = () => {
   // const params = useParams()
@@ -7,15 +7,29 @@ const MovieDetails = () => {
   const [details, setDetails] = useState({})
   // console.log(params)
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=1cd9c12b0f59437cb1f892337285c32e&language=en-US`).then((res) => res.json()).then((data) => setDetails(data))
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=1cd9c12b0f59437cb1f892337285c32e&language=en-US`)
+      .then((res) => res.json())
+      .then((data) => setDetails(data))
   }, [movieId])
 
-  // console.log(details.title)
+  console.log('details:', details)
   return (
-    <section className='movie-container'>
-      <h1>{details.title}</h1>
-      {/* <img src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`} alt={details.title} /> */}
-    </section>
+    <>
+      <Link to='/'>
+        <span>&#10688; Movies</span>
+      </Link>
+      <section className='movie-container'>
+        {details.backdrop_path !== undefined && (
+          <>
+            {console.log('details backdrop:', details.backdrop_path)}
+            <h1>{details.title}<span>{details.vote_average}</span></h1>
+            <p>{details.overview}</p>
+            <img src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`} alt={details.title} />
+            <img src={`https://image.tmdb.org/t/p/w780${details.poster_path}`} alt={details.title} />
+          </>
+        )}
+      </section>
+    </>
   )
 }
 
