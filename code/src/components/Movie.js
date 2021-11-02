8552d3a5fa66efd6cdb78750f2abb2c753824a53
Sarah Mottram <smelbows@gmail.com>
2021-11-02 14:24:37 +0100

    added movie details on all movies page and styled with hover effect

--- code/src/components/Movie.js
+++ code/src/components/Movie.js
@@ -4,6 +4,7 @@ const Movie = ({ movie }) => {
   return (
     <div className="movie-container">
       <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title}></img>
+      <div className="details-overlay"><h1>{movie.title}</h1><p>Released {movie.release_date}</p></div>
     </div>
   );
 };
