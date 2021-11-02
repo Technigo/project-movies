9acdcc952d697a323729a57ec55dca2841231553
Никитина Юля <nik.iuliiasw@gmail.com>
2021-11-02 11:17:16 +0100

    reached blue level

--- code/src/pages/Details/Details.js
+++ code/src/pages/Details/Details.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { FaChevronCircleLeft } from 'react-icons/fa'
 
 import { DETAILS_URL } from "utils/urls";
 import "./Details.css";
@@ -19,20 +20,21 @@ const Details = () => {
     <div
       className="background-img"
       style={{
-        backgroundImage: `url(
-          https://image.tmdb.org/t/p/w1280/${movieDetail.backdrop_path})`,
+        backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 20%, rgba(0,0,0) 100%), url(
+        https://image.tmdb.org/t/p/w1280/${movieDetail.backdrop_path})`,
       }}
     >
+      <Link className="back-arrow" to="/"><FaChevronCircleLeft />Movies</Link>
       {console.log(movieDetail.backdrop_path)}
       <article className="movie-card-details">
         <img
           src={`https://image.tmdb.org/t/p/w342/${movieDetail.poster_path}`}
-          alt={movieDetail.title}
+          alt={movieDetail.title} className="movie-image-small"
         />
         <div className="movie-details">
           <h1>
             {movieDetail.title}
-            <span className="ranking">{movieDetail.vote_average}</span>
+            <span className="ranking">{movieDetail.vote_average}/10</span>
           </h1>
           <p>{movieDetail.overview}</p>
         </div>
