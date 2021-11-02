e03c0651ed4952e4e49032f96566d68ec3ee0528
A1eksa <80787783+A1eksa@users.noreply.github.com>
2021-11-02 23:15:31 +0100

    grid

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -3,27 +3,25 @@ import { Link } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
   return (
-    <div className='movie-container'>
-      <section>
-        {movies.map((movie) => (
-          <Link to={`/details/${movie.id}`}>
-            <div className='movie-card'>
-              <img
-                className='movie-img'
-                src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <div className='overlay'>
-                <div className='overlay-text'>
-                  <h1 key={movies.id}>{movie.original_title}</h1>
-                  <h2>Released:{movie.release_date}</h2>
-                </div>
+    <section className='movie-container'>
+      {movies.map((movie) => (
+        <Link to={`/details/${movie.id}`}>
+          <div className='movie-card'>
+            <img
+              className='movie-img'
+              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className='overlay'>
+              <div className='overlay-text'>
+                <h1 key={movies.id}>{movie.original_title}</h1>
+                <h2>Released:{movie.release_date}</h2>
               </div>
             </div>
-          </Link>
-        ))}
-      </section>
-    </div>
+          </div>
+        </Link>
+      ))}
+    </section>
   );
 };
 
