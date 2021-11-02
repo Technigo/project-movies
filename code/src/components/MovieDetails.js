b2f513091488ee274f908ee6e513f1b1cc12bdda
JohannaMJ <johanna.mannestal@gmail.com>
2021-11-02 17:25:22 +0100

    movielist and details components created

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { MOVIEDETAILS_URL } from '../utils/urls';
+
 const MovieDetails = () => {
+  const [movieDetails, setMovieDetails] = useState([]);
   const { id } = useParams();
+
+  useEffect(() => {
+    fetch(MOVIEDETAILS_URL(id))
+      .then((res) => res.json())
+      .then((data) => setMovieDetails(data));
+  }, []);
+
+  console.log(movieDetails);
 
   return (
     <div>
       <h2>Movie Details component</h2>
+      <h3>{movieDetails.original_title}</h3>
+      <h4>{movieDetails.vote_average}/10</h4>
+      <p>{movieDetails.overview}</p>
+      <img
+        src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`}
+        alt={movieDetails.title}
+      />
     </div>
   );
 };
--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,10 +1,11 @@
-const APIKey = '39ef0794788e9f0d1001295657696692';
-export const MOVIELIST_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`;
+const APIkey = '39ef0794788e9f0d1001295657696692';
+export const MOVIELIST_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${APIkey}&language=en-US&page=1`;
 
-//API to movie details (needs movie-id):
-//https://api.themoviedb.org/3/movie/{movie_id}?api_key=39ef0794788e9f0d1001295657696692&language=en-US
-//https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US
+export const MOVIEDETAILS_URL = (id) =>
+  `https://api.themoviedb.org/3/movie/${id}?api_key=${APIkey}&language=en-US`;
 
 //API to backgrops/posters
 //https://api.themoviedb.org/3/configuration?api_key=39ef0794788e9f0d1001295657696692
 //https://api.themoviedb.org/3/configuration?api_key={api_key}
+
+//https://api.themoviedb.org/3/movie/580489?api_key=39ef0794788e9f0d1001295657696692&language=en-US
