5c25e8e411f600735aab6dbe582a2fbf4c04fcb9
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-11-02 15:01:31 +0100

    Added more styling

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -3,6 +3,9 @@ import { useParams, NavLink } from 'react-router-dom';
 
 import { DETAILS_URL } from '../utils/urls';
 
+import "../components/Details.css"
+
+
 const Details = () => {
 	const [details, setDetails] = useState({});
 
@@ -18,17 +21,29 @@ const Details = () => {
 
 
 	const divStyle = {
-		backgroundImage: 'url(https://image.tmdb.org/t/p/w1280'+ details.backdrop_path + ')'
+		
 	  };
 
 	return( 
-	<div className="details" style={divStyle}>{details.original_title}
+	<div className="detailsPage" style={divStyle}>
 			<nav>
-				<NavLink exact to="/" activeClassName="my-custom-class">
+				<NavLink className="back-link" exact to="/" activeClassName="my-custom-class">
 					Back
 				</NavLink>
 				</nav>
+				<div className="background" style={{ backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${details.backdrop_path})` }}>
+				<div className="summary">
+            <img src={`https://image.tmdb.org/t/p/w780${details.poster_path}`}  alt={details.title} />
+            <div className="details">
+              <h1>{details.original_title} <span className="rating">{details.vote_average}/10</span></h1>
+              <p>{details.overview}</p>
+          </div>
 	</div>
+	</div>
+
+	</div>
+
+	
 	);
 };
 
