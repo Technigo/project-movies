a5c5b2e96885b2a0747454c4d2dcea16ef36c32a
Sabrina Zancotti <zancotti@gmail.com>
2021-11-02 11:53:31 +0100

    Added the overlay on hover and redid the css to styled components

--- code/src/components/MovieList/MovieList.js
+++ code/src/components/MovieList/MovieList.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { MovieListItem } from "./MovieListItem";
-import "./MovieList.css";
+import styled from "styled-components";
+
+// -------------------css---------------------------
+
+const MovieListContainer = styled.div`
+  display: grid;
+  flex-wrap: center;
+  grid-template-columns: repeat(2, auto);
+  row-gap: 10px;
+
+  @media (min-width: 668px) and (max-width: 1024px) {
+    justify-content: space-evenly;
+  }
+  @media only screen and (min-width: 1025px) {
+    justify-content: space-evenly;
+    grid-template-columns: repeat(4, auto);
+  }
+`;
 
 export const MovieList = ({ movieList, imageInformation }) => {
   return (
-    <section className="movie-list-container">
+    <MovieListContainer>
       {movieList.map((movie) => {
         return (
           <MovieListItem
@@ -14,6 +31,6 @@ export const MovieList = ({ movieList, imageInformation }) => {
           />
         );
       })}
-    </section>
+    </MovieListContainer>
   );
 };
--- code/src/components/MovieList/MovieListItem.js
+++ code/src/components/MovieList/MovieListItem.js
@@ -1,25 +1,62 @@
 import React, { useState } from "react";
-import "./MovieListItem.css";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const PosterContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PosterContainerImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const TextContainer = styled.div`
+  position: absolute;
+  display: none;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  color: white;
+  justify-content: flex-end;
+  flex-direction: column;
+  padding: 20px;
+`;
 
 export const MovieListItem = ({ movie, imageInformation }) => {
   const [imageOpacityStyle, setImageOpacityStyle] = useState({ opacity: 1 });
+  const [textDivStyle, setTextDivStyle] = useState({ diplay: "none" });
   console.log(imageInformation);
   const imagePath = `${imageInformation.base_url}${imageInformation.poster_sizes[6]}${movie.poster_path}`;
-  console.log(imagePath);
 
   return (
     <>
-      <div
-        className="poster-container"
-        onMouseEnter={() => setImageOpacityStyle({ opacity: 0.4 })}
-        onMouseLeave={() => setImageOpacityStyle({ opacity: 1 })}
-      >
-        <img
-          src={imagePath}
-          alt={`poster for ${movie.original_title}`}
-          style={imageOpacityStyle}
-        ></img>
-      </div>
+      <Link to={`/movies/${movie.id}`}>
+        <PosterContainer
+          onMouseEnter={() => {
+            setImageOpacityStyle({ opacity: 0.25 });
+            setTextDivStyle({ display: "flex" });
+          }}
+          onMouseLeave={() => {
+            setImageOpacityStyle({ opacity: 1 });
+            setTextDivStyle({ diplay: "none" });
+          }}
+        >
+          <TextContainer style={textDivStyle}>
+            <h1>{movie.title}</h1>
+            <span>Release date: {movie.release_date}</span>
+          </TextContainer>
+          <PosterContainerImage
+            src={imagePath}
+            alt={`poster for ${movie.original_title}`}
+            style={imageOpacityStyle}
+          ></PosterContainerImage>
+        </PosterContainer>
+      </Link>
     </>
   );
 };
