e4314c9acfb04042a484732c34c092b507ce5793
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-11-02 19:52:33 +0100

    error message handeler added but does not work yet

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -4,26 +4,39 @@ import { API_MOVIE_DETAILS } from "./Links";
 import { BackButton } from "./BackButton";
 import "./MovieDetails.css";
 import { Loader } from "./Loader";
+import { Link } from "react-router-dom";
+import "../index.css";
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
   const [details, setDetails] = useState({});
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setLoading(true);
     fetch(API_MOVIE_DETAILS(movieId))
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Something is wrong");
+        }
+      })
       .then((data) => {
         console.log(data);
         setDetails(data);
         setLoading(false);
+      })
+      .catch((error) => {
+        console.dir(error);
+        setError(error.message);
       });
   }, [movieId, setLoading]);
 
   return (
     <>
-      {(loading && (
+      {loading && (
         <div className="grid">
           <div className="loader-wrapper">
             <Loader color={"goldenrod"} width={120} height={120} />
@@ -36,24 +49,41 @@ export const MovieDetails = () => {
             </p>
           </div>
         </div>
-      )) || (
-        <section className="movie-wrapper">
-          <div className="gradient">
-            <img className="background-image" src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`} alt={`still from ${details.title}`} />
-          </div>
-          <div className="movie-details">
-            <BackButton />
-            <img className="poster" src={`https://image.tmdb.org/t/p/w342${details.poster_path}`} alt={`poster from ${details.title}`} />
-            <div className="text-details">
-              <div className="title-wrapper">
-                <h1>{details.title}</h1>
-                <span className="movie-rating">{details.vote_average}</span>
-              </div>
-              <p>{details.overview}</p>
-            </div>
-          </div>
-        </section>
       )}
+      {error && (
+        <div className="error-container">
+          <p className="error-p">
+            This is an error page. This happend because of {error}
+          </p>
+          <Link to="/">
+            <button className="go-back-button">To movies</button>
+          </Link>
+        </div>
+      )}
+      <section className="movie-wrapper">
+        <div className="gradient">
+          <img
+            className="background-image"
+            src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`}
+            alt={`still from ${details.title}`}
+          />
+        </div>
+        <div className="movie-details">
+          <BackButton />
+          <img
+            className="poster"
+            src={`https://image.tmdb.org/t/p/w342${details.poster_path}`}
+            alt={`poster from ${details.title}`}
+          />
+          <div className="text-details">
+            <div className="title-wrapper">
+              <h1>{details.title}</h1>
+              <span className="movie-rating">{details.vote_average}</span>
+            </div>
+            <p>{details.overview}</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -4,18 +4,29 @@ import moment from "moment";
 import { API_MOVIE_LIST } from "./Links";
 import { OverLay } from "./OverLay";
 import { Loader } from "./Loader";
+import "../index.css";
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setLoading(true);
     fetch(API_MOVIE_LIST)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("something went wrong");
+        }
+      })
       .then((json) => {
         setMovies(json.results);
         setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
       });
   }, [setLoading]);
 
@@ -33,9 +44,23 @@ export const MovieList = () => {
           </p>
         </div>
       )}
+      {error && (
+        <div className="error-container">
+          <p className="error-p">
+            This is an error page. This happend because of {error}
+          </p>
+          <Link to="/">
+            <button className="go-back-button">To movies</button>
+          </Link>
+        </div>
+      )}
+
       {movies.map((movie) => (
         <div className="movie-container" key={movie.id}>
-          <img alt={`${movie.original_title} `} src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} />
+          <img
+            alt={`${movie.original_title} `}
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+          />
           <Link to={`/movieDetails/${movie.id}`}>
             <OverLay>
               <h2>{movie.original_title}</h2>
