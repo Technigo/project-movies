2c344a82095c2ee1008e4c10e776f6f889c14b13
JohannaMJ <johanna.mannestal@gmail.com>
2021-11-02 18:10:34 +0100

    back arrow from details to list added

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { BackIcon } from './BackIcon';
 
 import { MOVIEDETAILS_URL } from '../utils/urls';
 
@@ -12,18 +13,26 @@ const MovieDetails = () => {
     fetch(MOVIEDETAILS_URL(id))
       .then((res) => res.json())
       .then((data) => setMovieDetails(data));
-  }, []);
+  }, [id]);
 
   console.log(movieDetails);
 
   return (
     <div>
       <h2>Movie Details component</h2>
+      <Link to='/' className='backLink'>
+        <BackIcon /> Movies
+      </Link>
       <h3>{movieDetails.original_title}</h3>
       <h4>{movieDetails.vote_average}/10</h4>
       <p>{movieDetails.overview}</p>
+
       <img
         src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`}
+        alt={movieDetails.title}
+      />
+      <img
+        src={`https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path}`}
         alt={movieDetails.title}
       />
     </div>
