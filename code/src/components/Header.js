880d7468898f9d605fcf0714a30b68f3f73b074d
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-11-03 15:32:41 +0100

    added soc med icons

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
 import './Header.css'
 import { Link } from "react-router-dom";
+import facebookIcon from '../assets/facebook-icon.png'
+import twitterIcon from '../assets/twitter-icon.png'
+import youtubeIcon from '../assets/youtube-icon.png'
 
 const Header = () => {
     return (
         <section className="header-container">
             <div>
                 <Link to='/'><p className="header-title">Movieflix</p></Link>
-                <img src='code/src/assets/facebook-icon.png'></img>
             </div>
-        </section>
+            <div className="media-icons-container">
+                <img className="media-icons" src={facebookIcon} alt=" facebook-icon"></img>
+                <img className="media-icons" src={twitterIcon} alt="twitter-icon"></img>
+                <img className="media-icons" src={youtubeIcon} alt="youtube-icon"></img>
+            </div>
+        </section >
     )
 }
 
