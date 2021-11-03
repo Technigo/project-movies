430a3f68b980c469bb44d07468a03a19d5628ea9
Jsfrulle <Jessicamhermansson@gmail.com>
2021-11-03 20:57:13 +0100

    fix netlifly

--- code/src/Toprated.js
+++ code/src/Toprated.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Loading from "components/Loading";
 import { TopRatedUrl } from "Urls";
-import Moment from "react-moment";
+
 
 const Toprated = () => {
   const [movies, setMovies] = useState([]);
