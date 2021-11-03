32d3b934046d79eaeca9fa58817910e7123e5e0f
Harry Bäcklin <83465217+Groovedharry@users.noreply.github.com>
2021-11-03 13:13:33 +0100

    start of css and changed text

--- code/src/components/Movielist.js
+++ code/src/components/Movielist.js
@@ -3,23 +3,22 @@ import { Link } from "react-router-dom";
 
 const movieList = ({ movies }) => {
 return (
-    <section className="wrapper">
+    <section className="popular-wrapper">
     {movies.map((movie) => (
         <div key={movie.id}>
         <Link to={`/movies/${movie.id}`}>
             <img
             src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-            alt="{movie.title} poster"
+            alt="{movie.poster_path}"
             ></img>
         </Link>
 
-        <div className="movie-info">
-            <p>{movie.title}</p>
+        <div className="movie-details">
+            <h1>{movie.title}</h1>
             <p>{movie.release_date}</p>
         </div>
-
         </div>
-    ))}
+        ))}
     ;
     </section>
 );
