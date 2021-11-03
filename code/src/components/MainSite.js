e14fce41ea7c76cce102b50f08ea79df51076c5e
Elsa-Johanna <elsa.bjelkedal@gmail.com>
2021-11-03 17:07:24 +0100

    gotta fix the genre

--- code/src/components/MainSite.js
+++ code/src/components/MainSite.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const MainSite = ({ movies }) => {
   return (
-    <div>
-      <section className="movie-box">
-        {movies.map((movie) => (
-          <Link key={movie.id} to={`/details/${movie.original_title}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              alt={movie.original_title}
-            />
+    <div className="movie-box">
+      {movies.map((movie) => (
+        <Link key={movie.id} to={`/details/${movie.id}`}>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt={movie.original_title}
+          />
+          <div className="movie-info">
             <h1>{movie.original_title}</h1>
             <p>Released {movie.release_date}</p>
-          </Link>
-        ))}
-      </section>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
