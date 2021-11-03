4c1b8ce24c22ad684fcc4a30fa7b795d9cde5139
Darya <darya.lapata@gmail.com>
2021-11-03 09:08:23 +0100

    deleted console.log

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -4,7 +4,7 @@ import { Link }  from 'react-router-dom'
 import './movieList.css'
 
 export const MovieList = ({movieList}) => {
-	console.log(movieList)
+
     return (
 
       <section className="movie-container"> 
