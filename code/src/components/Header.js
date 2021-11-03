ebf2928854c26cb66b3298b3bcc302f91361d90d
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-11-03 15:10:48 +0100

    added error message animation

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './Header.css'
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <section className="header-container">
             <div>
-                <p className="header-title">Movieflix</p>
+                <Link to='/'><p className="header-title">Movieflix</p></Link>
             </div>
         </section>
     )
