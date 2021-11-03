0142827517027e546b2a665a11b05fd49d98efcf
fatigr26 <fatigr2692@gmail.com>
2021-11-03 17:47:34 +0100

    added some styling

--- code/src/App.js
+++ code/src/App.js
@@ -4,34 +4,25 @@ import MoviesList from "components/MoviesList";
 import MoviesDetails from "components/MoviesDetails";
 import { LIST_URL } from "./utils/urls";
 
-
 export const App = () => {
-  const [movies, setMovies] = useState([]) // we expect to get data from an array, that's why we use []
+  const [movies, setMovies] = useState([]); // we expect to get data from an array, that's why we use []
 
-useEffect(() => {
-  fetch(LIST_URL)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.results)) 
-  }, [])
+  useEffect(() => {
+    fetch(LIST_URL)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, []);
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path ="/"> {/*//this means that it is the first page that displays*/}
-          <MoviesList 
-            films={movies}                        
-          />
+        <Route exact path="/">  {/*//this means that it is the first page that displays*/}
+          <MoviesList films={movies} />
         </Route>
-        <Route path ="/details/:moviesId">
-          <MoviesDetails
-                        
-          />
+        <Route path="/details/:moviesId">
+          <MoviesDetails />
         </Route>
       </Switch>
     </BrowserRouter>
   );
 };
-
-
-
-
--- code/src/components/MoviesList.js
+++ code/src/components/MoviesList.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const MoviesList = ({ films }) => {
-    return (
-        <>
-        {films.map((movie) => (
-            <Link to={`/details/${movie.id}`}>
-            
-                <div className="overlay"></div>
-                <div key={movie.id} className="movie-container">
-                    <div className="hover-movie">
-                        <h1>{movie.original_title}</h1>
-                        <p>{movie.release_date}</p>
-                    </div>
-                <img src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt="picture"/>   
-                </div>
-            </Link>
-            
-        ))}          
-        </>
-    )
+  return (
+    <div className="main-container">
+      {films.map((cat) => (
+
+        <Link to={`/details/${cat.id}`} className="movie-link">
+          
+          <div key={cat.id} className="movie-container">
+            <div className="overlay">
+              <section className="text-wrapper">
+                <h1 className="movie-title">{cat.original_title}</h1>
+                <p className="release-date">{cat.release_date}</p>
+              </section>
+            </div>
+            <img
+              src={`https://image.tmdb.org/t/p/w780${cat.poster_path}`}
+              alt="picture"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default MoviesList;
