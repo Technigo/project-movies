4e3d93e04ee075778d77625477dfad360a1a895c
isomoth <ic.gonzalez35@hotmail.com>
2021-11-03 21:02:38 +0100

    styled button and created link for it

--- code/src/components/MovieDetails/MovieDetails.js
+++ code/src/components/MovieDetails/MovieDetails.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { DETAILS_URL } from "utils/urls";
-import "./MovieDetails.css";
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { FaChevronCircleLeft } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import { DETAILS_URL } from 'utils/urls';
+import './MovieDetails.css';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -12,31 +14,47 @@ const MovieDetails = () => {
       .then((res) => res.json())
       .then((json) => {
         setMovieDetails(json);
-        console.log("DETAILS", json);
+        console.log('DETAILS', json);
       });
   }, [movieId]);
 
   return (
     <section>
       <div
-        className="background-image"
+        className='background-image'
         style={{
           backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})`,
         }}
       >
-        <button>Movies</button>
-        <div className="info-summary">
+        <Link to='/' className='backLink' style={{ textDecoration: 'none' }}>
+          <div className='button-span'>
+            <IconContext.Provider
+              value={{
+                color: 'white',
+                className: 'button-chevron',
+                verticalAlign: 'middle',
+              }}
+            >
+              <div>
+                <FaChevronCircleLeft size={20} />
+              </div>
+            </IconContext.Provider>
+
+            <button>Movies</button>
+          </div>
+        </Link>
+        <div className='info-summary'>
           <img
-            className="movie-poster"
+            className='movie-poster'
             src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`}
-            alt="{movieDetails.title} poster"
+            alt='{movieDetails.title} poster'
           ></img>
-          <div className="summary-wrapper">
-            <div className="name-rate">
-              <h3 className="info-details">{movieDetails.title}</h3>
-              <p className="votes">{movieDetails.vote_average}/10</p>
+          <div className='summary-wrapper'>
+            <div className='name-rate'>
+              <h3 className='info-details'>{movieDetails.title}</h3>
+              <p className='votes'>{movieDetails.vote_average}/10</p>
             </div>
-            <p className="info-details">{movieDetails.overview}</p>
+            <p className='info-details'>{movieDetails.overview}</p>
           </div>
         </div>
       </div>
