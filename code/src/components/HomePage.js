360cfefb741d654ab9fa4a5a5f1d69fdbe8116ed
ruruahn <81482699+ruruahn@users.noreply.github.com>
2021-11-03 16:11:33 +0100

    updated css on <p> element

--- code/src/components/HomePage.js
+++ code/src/components/HomePage.js
@@ -2,36 +2,38 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 const HomePage = ({ userApiKey, setUserApiKey }) => {
-  const history = useHistory();
+	const history = useHistory();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    history.push("/all-movies");
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		history.push("/all-movies");
+	};
 
-  const handleInputChange = (e) => {
-    setUserApiKey(e.target.value);
-  };
-  return (
-    <div className="main">
-      <h1>Welcome to project-movies</h1>
-      <p>Log-in below using your TMDB API key. Don't have one? Sign up here:</p>
-      <form className="api-form" onSubmit={handleSubmit}>
-        <label className="api-label">
-          Enter Key
-          <input
-            className="api-input"
-            type="text"
-            placeholder="Your API key here"
-            name="api-key"
-            value={userApiKey}
-            onChange={handleInputChange}
-          ></input>
-        </label>
-        <button className="submit-button" type="submit">Submit</button>
-      </form>
-    </div>
-  );
+	const handleInputChange = (e) => {
+		setUserApiKey(e.target.value);
+	};
+	return (
+		<div className="main">
+			<h1>Welcome to project-movies</h1>
+			<p>Log-in below using your TMDB API key. Don't have one? Sign up here:</p>
+			<form className="api-form" onSubmit={handleSubmit}>
+				<label className="api-label">
+					Enter Key
+					<input
+						className="api-input"
+						type="text"
+						placeholder="Your API key here"
+						name="api-key"
+						value={userApiKey}
+						onChange={handleInputChange}
+					></input>
+				</label>
+				<button className="submit-button" type="submit">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default HomePage;
--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -1,27 +1,42 @@
 import React, { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import styled from "styled-components";
 
 const MovieDetails = ({ allMovies }) => {
 	const { movieId } = useParams();
 	const matchingMovie = allMovies.find((movie) => movie.id == movieId);
-	const styles = {
-		backgroundImage: `https://image.tmdb.org/t/p/w1280${matchingMovie.backdrop_path}`,
-    width: "100%",
-    display: "block",
+
+	const BackgroundStyles = {
+		backgroundImage: `url(https://image.tmdb.org/t/p/w1280${matchingMovie.backdrop_path})`,
+		display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+		backgroundSize: "cover",
+		backgroundRepeat: "no-repeat",
+		minHeight: "100vh",
 	};
+
 	return (
 		<article>
-			<Link to="/all-movies" className="back-button">
-				<button>Movies</button>
+			<Link className="back-link" to="/all-movies">
+      <img src="https://img.icons8.com/material-outlined/24/000000/circled-left--v2.png"/>Movies
 			</Link>
-			<div className="detail-section" style={styles}>
-				<img
+			<div style={BackgroundStyles}>
+      <div className="summary-section">
+				<img className="poster"
 					src={`https://image.tmdb.org/t/p/w342${matchingMovie.poster_path}`}
 				/>
-				<div className="summary-section">
-					<h1>{matchingMovie.title}</h1>
-					<h2>{matchingMovie.vote_average}/10</h2>
-					<p>{matchingMovie.overview}</p>
+        <div className="details">
+					<h1 className="movie-title">
+            {matchingMovie.title}
+            <span className="ratings">
+              {matchingMovie.vote_average}/10
+              </span>
+            </h1>
+					<p className="movie-description">
+            {matchingMovie.overview}
+            </p>
+          </div>
 				</div>
 			</div>
 		</article>
