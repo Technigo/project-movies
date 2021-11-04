fd89c8f475459b44c203331a9197bbd62740ca06
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-11-04 15:50:49 +0100

    added error catch on homepage

--- code/src/App.js
+++ code/src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { LIST_URL } from 'utils/urls'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import Header from 'components/Header'
 import MovieList from './components/MovieList'
 import MovieDetails from './components/MovieDetails'
 import LoadingSpinner from 'components/LoadingSpinner'
 import Footer from 'components/Footer'
+import ErrorMessage from 'components/ErrorMessage'
 
 export const App = () => {
   const [movieTitle, setMovieTitle] = useState([])
@@ -19,7 +21,13 @@ export const App = () => {
   const fetchMovieTitle = () => {
     setLoading(true)
     fetch(LIST_URL)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('something went wrong!')
+        }
+      })
       .then((data) => setTimeout(() => setMovieTitle(data.results), 1000))
       .finally(() => setTimeout(() => setLoading(false), 1000))
   }
@@ -40,13 +48,17 @@ export const App = () => {
                 date={movie.release_date}
                 rating={movie.vote_average}
               />
-            )
-            )
-            }
+            ))}
             <Footer />
           </Route>
           <Route exact path='/movie/:id'>
             <MovieDetails />
+          </Route>
+          <Route>
+            <Link to='/' className="go-back-link">
+              <p> <span className="back-arrow">&#8666;</span> Go Back</p>
+            </Link>
+            <ErrorMessage error={'something went wrong!'} />
           </Route>
         </Switch>
       </BrowserRouter >
--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from "react"
 import './Header.css'
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 import facebookIcon from '../assets/facebook-icon.png'
 import twitterIcon from '../assets/twitter-icon.png'
 import youtubeIcon from '../assets/youtube-icon.png'
