e4f8dfebf3fe77ecbfa2d792a7709372dfeeac3a
Rephili <72921631+Rephili@users.noreply.github.com>
2021-11-04 12:40:19 +0100

    added a go back button and styled with flexbox

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { API_KEY, MOVIE_DETAILS_URL } from "../urls";
+import "../index.css";
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
 
   const [details, setDetails] = useState([]);
+
+  const history = useHistory();
 
   useEffect(() => {
     fetch(MOVIE_DETAILS_URL(API_KEY, movieId))
@@ -13,18 +16,39 @@ export const MovieDetails = () => {
       .then((data) => setDetails(data));
   }, [movieId]);
 
+  const goBackButton = () => {
+    history.goBack();
+  };
+
   return (
     <section>
       {details && (
-        <div>
-          <h1>{details.title}</h1>
-          <img
-            src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`}
-            alt={details.title}
-          />
-          <p>{details.overview}</p>
-          <p>{details.vote_average}/10</p>
-          <p>Release date: {details.release_date}</p>
+        <div
+          className="details-container"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${details.backdrop_path})`,
+          }}
+        >
+          <button className="go-back-button" onClick={goBackButton}>
+            <span role="img" aria-label="arrow">
+              &lArr;
+            </span>{" "}
+            Movies
+          </button>
+          <div className="movie-details-summary">
+            <img
+              className="movie-details-poster"
+              src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`}
+              alt={details.title}
+            />
+            <div className="movie-details-text">
+              <h1>
+                {details.title}
+                <span className="vote-average">{details.vote_average}/10</span>
+              </h1>
+              <p>{details.overview}</p>
+            </div>
+          </div>
         </div>
       )}
     </section>
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../index.css";
 
 export const MovieList = ({ movies }) => {
   return (
-    <section>
+    <section className="list-container">
       {movies.map((movie) => (
         <Link to={`/details/${movie.id}`} key={movie.id}>
-          <div key={movie.original_title}>
+          <div className="movie-card-container" key={movie.original_title}>
             <img
-              src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
               alt="movie poster"
             ></img>
-            <div>
-              <h1>{movie.original_title}</h1>
-              <p>Released {movie.release_date}</p>
+            <div className="movie-card-overlay">
+              <div className="movie-card-text">
+                <h1 className="movie-title">{movie.original_title}</h1>
+                <p className="movie-release-text">
+                  Released {movie.release_date}
+                </p>
+              </div>
             </div>
           </div>
         </Link>
