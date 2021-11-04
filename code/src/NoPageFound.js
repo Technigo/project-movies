cc78f4c0f7ea226d8d50b377692c0fea75e590b5
Jsfrulle <Jessicamhermansson@gmail.com>
2021-11-04 10:47:18 +0100

    fix and style page 404 and loading

--- code/src/NoPageFound.js
+++ code/src/NoPageFound.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import page404 from './images/page404.mp4';
 
 const NoPageFound = () => {
   return (
-    <div>
-      <h2> Sorry page not found</h2>
+    <div className="pageNotFoundContainer">
 
-      <button>
-        <Link to="/"> Go back </Link>
+
+<div className="imgPageNotFound">
+
+<video autoPlay loop muted src={page404} alt={"no page found"} />
+</div>
+
+      <div className="textPageNotFound"> 
+      <h2> Sorry page not found...</h2>
+      </div>
+<div className="buttonPageNotFound"> 
+      <button >
+        <Link  className="linkPageNotFound" to="/">
+         Go back 
+         </Link>
       </button>
+      </div>
     </div>
   );
 };
--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,11 +1,22 @@
 import React from "react";
+import loadingimg from "images/Loading.mp4"
 
 const Loading = () => {
 	return (
-		<div className="loadingContainer">
-			<p> Loading...</p>
-		</div>
-	);
+    <div className="pageNotFoundContainer">
+
+
+<div className="imgPageNotFound">
+
+<video autoPlay loop muted src={loadingimg} alt={"loading video"} />
+</div>
+
+      <div className="textPageNotFound"> 
+      <h2>Loading....</h2>
+      </div>
+
+    </div>
+  );
 };
 
 export default Loading;
