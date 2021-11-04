dc3ed717aaed8dc60443cdf6e1b2e39aff65706a
Lovisa Carling <lovisa.carling@gmail.com>
2021-11-04 19:08:15 +0100

    added pop up navbar with hamburger icon

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,23 +1,44 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import '../css/details.css'
 
 export const Header = () => {
-	return (
-		<header>
-			<nav>
-				<NavLink exact to="/" activeClassName="active">
-					Popular
-				</NavLink>
-				<NavLink to="/top-rated" activeClassName="active">
-					Top Rated
-				</NavLink>
-				<NavLink to="/upcoming" activeClassName="active">
-					Upcoming
-				</NavLink>
-				<NavLink to="/now-playing" activeClassName="active">
-					Now Playing
-				</NavLink>
-			</nav>
-		</header>
-	);
-};
+  const [isActive, setActive] = useState(false)
+
+  const toggleClass = () => {
+    setActive(!isActive)
+  }
+
+  return (
+    <>
+      <div className='header-wrapper'>
+        <h1>Movie releases</h1>
+
+        <div
+          className={isActive ? 'toggle-active' : 'toggle'}
+          onClick={toggleClass}
+        ></div>
+      </div>
+
+      <header>
+        <nav
+          className={isActive ? 'side-menu-active' : 'side-menu-hidden'}
+          onClick={toggleClass}
+        >
+          <NavLink exact to='/' activeClassName='active'>
+            Popular
+          </NavLink>
+          <NavLink to='/top-rated' activeClassName='active'>
+            Top Rated
+          </NavLink>
+          <NavLink to='/upcoming' activeClassName='active'>
+            Upcoming
+          </NavLink>
+          <NavLink to='/now-playing' activeClassName='active'>
+            Now Playing
+          </NavLink>
+        </nav>
+      </header>
+    </>
+  )
+}
--- code/src/components/NowPlayingOverview.js
+++ code/src/components/NowPlayingOverview.js
@@ -1,42 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { NOW_PLAYING_URL_LIST } from "utils/urls";
-import { LoadingSpinner } from "./LoadingSpinner";
-import { Header } from "./Header";
-import "../css/overview.css";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { NOW_PLAYING_URL_LIST } from 'utils/urls'
+import { LoadingSpinner } from './LoadingSpinner'
+import { Header } from './Header'
+
+import '../css/overview.css'
 
 export const NowPlayingOverview = () => {
-	const [nowPlayingOverview, setNowPlayingOverview] = useState([]);
-	const [loader, setLoader] = useState(false);
+  const [nowPlayingOverview, setNowPlayingOverview] = useState([])
+  const [loader, setLoader] = useState(false)
 
-	useEffect(() => {
-		fetchTopMovies();
-	}, []);
+  useEffect(() => {
+    fetchTopMovies()
+  }, [])
 
-	const fetchTopMovies = () => {
-		setLoader(true);
-		fetch(NOW_PLAYING_URL_LIST)
-			.then((response) => response.json())
-			.then((data) => setNowPlayingOverview(data.results))
-			.finally(() => setLoader(false));
-	};
+  const fetchTopMovies = () => {
+    setLoader(true)
+    fetch(NOW_PLAYING_URL_LIST)
+      .then((response) => response.json())
+      .then((data) => setNowPlayingOverview(data.results))
+      .finally(() => setLoader(false))
+  }
 
-	return (
-		<>
-			<Header />
-			<div className="movie-grid">
-				{loader && <LoadingSpinner />}
-				{nowPlayingOverview &&
-					nowPlayingOverview.map((movie) => (
-						<Link className="movie-container" key={movie.id} to={`/movies/${movie.id}`}>
-							<img className="movie-poster" src={`http://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title}></img>
-							<div className="overlay-container">
-								<h1 className="movie-title">{movie.title}</h1>
-								<p className="release-date">{movie.release_date}</p>
-							</div>
-						</Link>
-					))}
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      <Header />
+      <div className='movie-grid'>
+        {loader && <LoadingSpinner />}
+        {nowPlayingOverview &&
+          nowPlayingOverview.map((movie) => (
+            <Link
+              className='movie-container'
+              key={movie.id}
+              to={`/movies/${movie.id}`}
+            >
+              <img
+                className='movie-poster'
+                src={`http://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                alt={movie.title}
+              ></img>
+              <div className='overlay-container'>
+                <h1 className='movie-title'>{movie.title}</h1>
+                <p className='release-date'>{movie.release_date}</p>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </>
+  )
+}
