d35cba5cf78ba2bcca1ee337079898545e80ac38
Idautterstrom <ida.utterstrom@hotmail.com>
2021-11-04 16:24:30 +0100

    prettier code

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,8 +1,3 @@
 export const MOVIES_URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=a89f6720304b78135ad37cee571cef4e&language=en-US&page=1";
 
-//Is this the correct API key?
-
-/* export const IMAGES_URL =
-  "https://api.themoviedb.org/3/movie/${id}?api_key=a89f6720304b78135ad37cee571cef4e&language=en-US";
- */
