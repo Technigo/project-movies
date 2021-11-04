9645e7aca4778ae3ed192b14ce6a82297c93643f
pdetli <74938586+pdetli@users.noreply.github.com>
2021-11-05 00:43:24 +0100

    og tags and image

--- code/src/components/Loader.js
+++ code/src/components/Loader.js
@@ -1,15 +1,30 @@
 import React from "react"
 import styled from "styled-components"
 
-const LoaderStyled = styled.div``
+const LoaderStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  ${
+    "" /* top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 1;
+  width: 900px;
+  height: auto;
+  margin: auto 0; */
+  }
+`
 
 const Loader = () => {
   return (
-    <LoaderStyled>
-      <div>
-        <img src="/popcorn.gif" className="loader-image" alt="popcorn loader" />
-      </div>
-    </LoaderStyled>
+    <div className="loader-image">
+      <LoaderStyled>
+        <img src="/popcorn.gif" alt="popcorn loader" />
+      </LoaderStyled>
+    </div>
   )
 }
 
