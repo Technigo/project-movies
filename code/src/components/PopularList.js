682f376259b507ee0b08473394b9df150a25469a
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-11-05 08:48:56 +0100

    Updated in PopularList component

--- code/src/components/PopularList.js
+++ code/src/components/PopularList.js
@@ -10,7 +10,7 @@ const PopularList = ({ movies }) => {
 		//here we map our movies and list them and we display a poster for each with original title and release date
 		<section className="movies">
 			{movies.map((movie) => (
-				<Link key={movie.original_title} to={`/details/${movie.id}`}>
+				<Link key={movie.title} to={`/details/${movie.id}`}>
 				<div>
                 	<img src ={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt="posters of movies"></img>
 					<div className="details">
