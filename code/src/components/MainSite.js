27774ac025651df57201eb6118e40fc3b8a09742
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-11-05 09:23:45 +0100

    added a sort()

--- code/src/components/MainSite.js
+++ code/src/components/MainSite.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//((a, b) => Date.parse(b.release_date) - Date.parse(a.release_date))
+
 const MainSite = ({ movies }) => {
   return (
     <div className="movie-box">
-      {movies.map((movie) => (
-        <Link key={movie.id} to={`/details/${movie.id}`}>
-          <div className="movie-and-text">
-            <img
-              className="main-site-poster"
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <div className="movie-info">
-              <h1>{movie.title}</h1>
-              <p>Released {movie.release_date}</p>
+      {movies
+        .sort((a, b) => a.title.localeCompare(b.title))
+        .map((movie) => (
+          <Link key={movie.id} to={`/details/${movie.id}`}>
+            <div className="movie-and-text">
+              <img
+                className="main-site-poster"
+                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <div className="movie-info">
+                <h1>{movie.title}</h1>
+                <p>Released {movie.release_date}</p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
     </div>
   );
 };
