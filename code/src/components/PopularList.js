65c272611bfe8d4867d6ccc73b14e930ca476c84
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-11-05 08:42:07 +0100

    changed original title to title on popularlist component

--- code/src/components/PopularList.js
+++ code/src/components/PopularList.js
@@ -14,7 +14,7 @@ const PopularList = ({ movies }) => {
 				<div>
                 	<img src ={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt="posters of movies"></img>
 					<div className="details">
-            			<h1>{movie.original_title}</h1>
+            			<h1>{movie.title}</h1>
             			<p>Released {movie.release_date}</p>
           			</div>
 				</div>
