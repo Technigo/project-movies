c5dcd3b27be2e6a98b091e3ce207af490c82ce1f
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-11-05 08:52:33 +0100

    removed import deploy did not work

--- code/src/components/Navbar.js
+++ code/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import "../components/Navbar.css"
 
 //simple navbar with props
