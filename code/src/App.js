96bf79445f0a1d3214e510418f0803297a3139ba
Jessica Nygren <jessicanygren@C02D54BJMD6R.lan>
2021-11-07 20:06:11 +0100

    deleting none used componenents in app

--- code/src/App.js
+++ code/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Movies from './components/Movies'
 import MovieDetails from './components/MovieDetails'
-import LoadPage from 'components/LoadPage'
 
 export const App = () => {
   
