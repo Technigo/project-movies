4ba9eccb6848177e0f6420b9a4658a020979bf3c
vrill <52007698+vrill@users.noreply.github.com>
2021-11-07 20:23:54 +0100

    Added a Loading... component to App.js, but not working as expected in the browser for some reason. To be fixed.

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import { API_KEY, MOVIES_URL } from "./utils/urls";
 
+// import Loading from "./components/Loading";
 import { MovieList } from "./components/MovieList";
 import { MovieDetails } from "./components/MovieDetails";
 
 export const App = () => {
   const [movies, setMovies] = useState([]);
+  // const [loading, setLoading] = useState('false')
 
   useEffect(() => {
+    // setLoading(true)
     fetch(MOVIES_URL(API_KEY))
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-      });
-  }, []);
+    .then((res) => res.json())
+    .then((data) => {
+      setMovies(data.results);
+    })
+    // .finally(() => setLoading(false))
+  }, [])
 
   return (
     <BrowserRouter>
@@ -24,4 +29,4 @@ export const App = () => {
       </Switch>
     </BrowserRouter>
   );
-};
+}
