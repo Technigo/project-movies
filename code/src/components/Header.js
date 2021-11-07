e1aa8ddf181186ff1e929a766b984f5600024f00
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-11-07 16:28:11 +0100

    update styling of drop-down

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -8,13 +8,13 @@ const Header = ({ select, setSelect }) => {
 
   return (
     <header className="moviePicker">
-      <label htmlFor="movies">Choose a list:</label>
-        <select name="movies" id="movies" value={select} onChange={onSelectChange}>
-          <option value="popular">Popular</option>
-          <option value="top">Top rated</option>
-          <option value="upcoming">Upcoming</option>
-          <option value="now">Now playing</option>
-        </select>
+      <label htmlFor="movies">Choose a category: </label>
+      <select name="movies" id="movies" value={select} onChange={onSelectChange}>
+        <option value="popular">Popular</option>
+        <option value="top">Top rated</option>
+        <option value="upcoming">Upcoming</option>
+        <option value="now">Now playing</option>
+      </select>
     </header>
   )
 }
