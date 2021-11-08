65ae837658c4fbabaec1317fa5ff25f200299ef8
Charlotte Nyman <85078175+Svempolin@users.noreply.github.com>
2021-11-08 08:34:52 +0100

    error fix

--- code/src/components/NotFound.js
+++ code/src/components/NotFound.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
+// import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const onHomePageClick = () => {
-        history.push('/');
-    };
-    const onPreviousPageClick = () => {
-        history.goBack();
-    };
+  const onHomePageClick = () => {
+    history.push("/");
+  };
+  const onPreviousPageClick = () => {
+    history.goBack();
+  };
 
-    return (
-        <div>
-            <p>Sorry this page doesn't exist!</p>
-            <button onClick={onHomePageClick}>Back to the homepage</button>
-            <button onClick={onPreviousPageClick}>Back to Previouspage</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>Sorry this page doesn't exist!</p>
+      <button onClick={onHomePageClick}>Back to the homepage</button>
+      <button onClick={onPreviousPageClick}>Back to Previouspage</button>
+    </div>
+  );
+};
 export default NotFound;
