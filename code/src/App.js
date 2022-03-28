1e5c44608812b96209f26809a3fdac66212cc8e7
Arianna Allahyar <anallahyar@gmail.com>
2022-03-28 17:09:29 +0200

    Initial api call

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { TOKEN } from "token";
 
 export const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const moviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TOKEN}&language=en-US&page=1`;
+
+  useEffect(() => {
+    fetch(moviesUrl)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((error) => console.log(error));
+  }, [moviesUrl]);
+
   return (
     <div>
-      Find me in src/app.js!
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.release_date}</p>
+          <img src={`https://image.tmdb.org/t/p/w300/${movie.backdrop_path}`} alt={movie.title} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
