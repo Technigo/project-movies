4f4b33b96d0e8f604899c04de681fc3d0ce6232c
anki palviainen <anki.palviainen@gmail.com>
2022-03-28 14:00:44 +0300

    add links
    Co-authored-by: Goncalocv <Goncalocv@users.noreply.github.com>

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { LIST_LINK, MOVIE_DETAILS } from 'utilis/urls'
 
 export const App = () => {
   return (
