20b2c16bf41ce6862431511e754a0e252493acf7
Nhung Phan <nhungphan@Joel-Lov.local>
2022-03-28 12:32:01 +0300

    adding basic stuff

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { API_KEY } from 'ultils/API_KEY'
 
 export const App = () => {
+
+  const [list,setList] = useState([])
+
+  useEffect(() => {
+    fetch(API_KEY)
+    .then(res => res.json())
+    .then(data => setList(data.results))
+  },[])  
+  console.log(list)
+
   return (
     <div>
       Find me in src/app.js!
---
+++ code/src/ultils/API_KEY.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'https://api.themoviedb.org/3/movie/popular?api_key=eb85018c9b0845f43bd288b4feca8a24&language=en-US&page=1'
