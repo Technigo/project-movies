4db5358e95f17f149fbeab65e67b94f970b6fb90
Rawisara Saman <rawiisara@gmail.com>
2022-03-29 19:40:36 +0200

    added flex-wrap

--- code/src/components/PopularList.js
+++ code/src/components/PopularList.js
@@ -1,33 +1,38 @@
-import React, {useState, useEffect} from "react"
-import {ALL_MOVIES_URL} from "./urls"
+import React, { useState, useEffect } from "react"
+
+import { ALL_MOVIES_URL } from "../utils/urls"
+
+
+import './PopularList.css'
 
 
 const PopularList = () => {
-    const [movies, setMovies] = useState([])
-    useEffect(() => {
-        fetch(ALL_MOVIES_URL)
-          .then((res) => res.json())
-          .then((resJson) => {
-            setMovies(resJson.results);
-          })
-      }, [])
-
-
-      const movieElements = movies.map((movie) => {
-        return (
-          <div className="movies-wrapper">
-              <img src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`}/>
-              <h2>{movie.original_title}</h2>
-              <p>Release date:{movie.release_date}</p>
-              </div>
-              )
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    fetch(ALL_MOVIES_URL)
+      .then((res) => res.json())
+      .then((resJson) => {
+        setMovies(resJson.results);
       })
-      return (
-        <section className="popular-movies">
-          {movieElements}
-        </section>
-      )
-    }
+  }, [])
+
+
+  const movieElements = movies.map((movie) => {
+    return (
+      <div className="movies-wrapper">
+            <img className="movie-img" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} />
+          <h2>{movie.original_title}</h2>
+          <p>Release date:{movie.release_date}</p>
+
+      </div>
+    )
+  })
+  return (
+    <section className="popular-movies">
+      {movieElements}
+    </section>
+  )
+}
 
 
 export default PopularList
