d12a71c356e06c0ccfb481ba1e5c1194d4ce6173
Michael Chang <michaelchang@MacBook-Pro.local>
2022-03-29 14:39:26 +0200

    trying deploy refresh fix again

--- code/src/Pages/Home.jsx
+++ code/src/Pages/Home.jsx
@@ -62,20 +62,6 @@ const Home = () => {
                     {!isLoading && <Details films={popular} />}
                     </>
                 }/>
-                {/* Catch-All Route */}
-                <Route
-                    path="/*"
-                    element={
-                        <>
-                        {isLoading && <LoadingSpinner />}
-                        {!isLoading && 
-                            <main>
-                            <FilmList films={popular} />
-                            </main>
-                        }
-                        </>
-                        }
-                />
             </Routes>
         </BrowserRouter>
     )
