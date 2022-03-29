e4fa73ea90276b07f3f382f879e79f325186d50f
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-03-29 18:23:31 +0200

    displayed movie info in Movies page

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Details } from 'Pages/Details'
-import { Movies } from 'Pages/Movies'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Details } from "Pages/Details";
+import { Movies } from "Pages/Movies";
 
 export const App = () => {
   return (
@@ -10,6 +10,5 @@ export const App = () => {
         <Route path="/" element={<Movies />} />
       </Routes>
     </BrowserRouter>
-  )
-}
-
+  );
+};
--- code/src/Pages/Movies.js
+++ code/src/Pages/Movies.js
@@ -1,22 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import getTokenBeforeClosingBracket from "eslint-plugin-react/lib/util/getTokenBeforeClosingBracket";
+
+const MovieCard = styled.div`
+  background-color: black;
+`;
+
+const Info = styled.div`
+  display: block;
+`;
+
+const Title = styled.h1`
+  color: white;
+`;
+
+const Released = styled.h3`
+  color: white;
+`;
 
 export const Movies = () => {
-  const [movie, setMovie] = useState([])
+  const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=3d84d8a2e7d9f4fcf68bb05afb8dd7ef&language=en-US&page=1')
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=3d84d8a2e7d9f4fcf68bb05afb8dd7ef&language=en-US&page=1"
+    )
       .then((res) => res.json())
-      .then((json) => {
-        console.log(json)
-        setMovie(json.results)
-      })
-  }, [])
+      .then((data) => setMovie(data.results));
+  }, []);
 
   return (
-    <div>
-      Movies page
-    </div>
-  )
-}
+    <>
+      <main>
+        {movie.map((movie) => (
+          <MovieCard key={movie.id}>
+            <Link to={`/details/${movie.id}`}>
+              {" "}
+              <Info>
+                <Title>{movie.title}</Title>
+                <Released>Date released: {movie.release_date}</Released>
+              </Info>
+            </Link>
+          </MovieCard>
+        ))}
+      </main>
+      ; )
+    </>
+  );
+};
 
 // API key for The Movie Data Base: 3d84d8a2e7d9f4fcf68bb05afb8dd7ef //
