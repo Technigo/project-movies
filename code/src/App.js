a7ad333d9d4730229d303a3f7cd7c98abd4a7c66
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-03-29 14:31:30 +0200

    added first fetch (in Movies.js component) to get the list of movies

--- code/src/App.js
+++ code/src/App.js
@@ -7,10 +7,9 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Movies />} />
+        <Route path="/" element={<Movies />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
-// API key for The Movie Data Base: 3d84d8a2e7d9f4fcf68bb05afb8dd7ef //
--- code/src/Pages/Movies.js
+++ code/src/Pages/Movies.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Movies = () => {
+  const [movie, setMovie] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=3d84d8a2e7d9f4fcf68bb05afb8dd7ef&language=en-US&page=1')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        setMovie(json.results)
+      })
+  }, [])
+
   return (
     <div>
       Movies page
     </div>
   )
 }
+
+// API key for The Movie Data Base: 3d84d8a2e7d9f4fcf68bb05afb8dd7ef //
