b32f3b89cc609363c5b8f4769af5fdc4f840454f
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-03-30 16:20:55 +0200

    continued basic styling

--- code/src/Components/MovieList.js
+++ code/src/Components/MovieList.js
@@ -8,13 +8,13 @@ const MovieList = ({movies}) => {
         return <Link   
                   key={movie.id}
                   to={`/details/${movie.id}`}
-                  className="list-element"
+                  className="movie-box"
                   >
-                  <div className="info">
-                    <h1 className="title">{movie.original_title}</h1>
+                  <div className="info-box">
+                    <h1>{movie.original_title}</h1>
                     <p>Released {movie.release_date}</p>
                   </div>
-                  <img className="list-poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+                  <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
                   </Link>
         })}
     </section>
