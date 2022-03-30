9488b5cdaa948e84a4969cdca3bc1754445b2c88
Amanda Tange <amandatange@hotmail.com>
2022-03-30 18:13:58 +0200

    added layout and styling for details component

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -8,9 +8,22 @@ const imgSize = "original";
 const GoBackButton = styled.button`
     padding: 10px;
     color: white;
-    background-color: black;
+    background: rgba(0, 0, 0, 0.4);
     font-style: italic;
+    font-size: 1rem;
+    font-weight: 500;
+    max-width: 7rem;
+    max-height: 3rem;
+    border: 2px solid white;
+    border-radius: 2rem;
+    cursor: pointer;
 `;
+
+const BackDrop = styled.div`
+        background-image: linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%), url(${(props) => props.url});
+        background-size: cover;
+        min-height: 100vh;
+    `;
 
 
 export const Details = () => {
@@ -19,12 +32,10 @@ export const Details = () => {
     const navigate = useNavigate();
 
     const onBackButtonClick = ()=> {
-        // Use it to go back to previous page
         navigate(-1);
-
-        // navigate('/details/squirtle')
     }
 
+    
     const API_KEY = '3bf2860c1d44881bc87143482ddf0f94';
     const API = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
 
@@ -35,21 +46,30 @@ export const Details = () => {
     }, [API])
     console.log(movie)
     return (
-        <div>
-            <GoBackButton onClick={onBackButtonClick}>Go back</GoBackButton>
-            <h2>{movie.original_title}</h2>
-            <p className='rating'>{movie.vote_average}</p>
-            <p className='overview'>{movie.overview}</p>
-            <img className='backdrop' src={`${baseURL}${imgSize}${movie.backdrop_path}`} alt="backdrop movie" />
-            <img className='poster-img' src={`${baseURL}${imgSize}${movie.poster_path}`} alt={movie.original_title} />
-            {/* {movie && (
-                <div key={movie.idMovie}>
-                    <img src={`${movie.strMovieThumb}/preview`} alt={movie.strMovie} />
-                    <h1>{movie.strMovie}</h1>
-                    <h3>{movie.intYearReleased}</h3>
-                    <h3>Genre: {movie.strGenre}</h3>
+        <BackDrop
+            url={baseURL + imgSize + movie.backdrop_path}>
+            
+            <div className='movie-info-container'>
+                <GoBackButton onClick={onBackButtonClick}>Go back</GoBackButton>
+                <div className='movie-info'>
+                    <img className='detail-poster' src={`${baseURL}${imgSize}${movie.poster_path}`} alt={movie.original_title} />
+                    <div>
+                        <h2>{movie.original_title}</h2>
+                        <p className='rating'>{movie.vote_average}</p>
+                        <p className='overview'>{movie.overview}</p>
+                    </div>
+                    {/* <img className='backdrop' src={`${baseURL}${imgSize}${movie.backdrop_path}`} alt="backdrop movie" /> */}
+                    
+                    {/* {movie && (
+                        <div key={movie.idMovie}>
+                            <img src={`${movie.strMovieThumb}/preview`} alt={movie.strMovie} />
+                            <h1>{movie.strMovie}</h1>
+                            <h3>{movie.intYearReleased}</h3>
+                            <h3>Genre: {movie.strGenre}</h3>
+                        </div>
+                    )} */}
                 </div>
-            )} */}
-        </div>
+            </div>
+        </BackDrop>
     )
 }
