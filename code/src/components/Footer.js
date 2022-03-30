da804e24761bc62f5f24d54d8dbc77475498c42b
Emma Lindell <emma.lindell4@gmail.com>
2022-03-30 21:33:26 +0200

    Added styling for footer aswell

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Footer = () => {
 
     return (
 
-        <div className="footer-section">
+        <div>
+        <StyledFooter>
             <p>Made by Emma and Fay <span role="img" aria-label="popcorn emoji">🍿</span></p>
             <p>Technigo Bootcamp 2022</p>
             <p>Project 12 – Movies</p>
+            </StyledFooter>
         </div>
 
     )
@@ -15,3 +18,11 @@ const Footer = () => {
 }
 
 export default Footer
+
+
+//styled component for footer
+
+const StyledFooter = styled.div`
+text-align: center;
+color: gray;
+`
