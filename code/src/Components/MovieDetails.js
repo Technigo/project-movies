761bce0923fbea8ef7fbb4b2e0d8972fc7ae2a16
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-03-30 17:28:32 +0200

    styling of more-page

--- code/src/Components/MovieDetails.js
+++ code/src/Components/MovieDetails.js
@@ -21,13 +21,15 @@ const MovieDetails = () => {
   }, [movieTitle]);
 
   return (
-    <div>
-      <div className="background" style={{ backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }}>
-        <button onClick={onBackButtonClick}>Movies</button>
-        <img className="poster" src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt={movieDetails.original_title} />
+    <div className='more-page'>
+      <button onClick={onBackButtonClick}>Movies</button>
+      <div className="background" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0) 40%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }}>
         <div className="more-info">
-          <h3>{movieDetails.original_title}</h3>
-          <p>{movieDetails.overview}</p>
+        <img className="poster" src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt={movieDetails.original_title} />
+          <div className="more-info-text">
+            <h3>{movieDetails.original_title}<span className="highlight">{movieDetails.vote_average}/10</span></h3>
+            <p>{movieDetails.overview}</p>
+          </div>
         </div>
       </div> 
     </div>
