0d74ad062138243e2685e0df446b0402bbe0fb3a
TereseClaesson <terese.claesson@gmail.com>
2022-03-30 09:00:17 +0200

    fixed with maping title and images on detail.js

--- code/src/Components/Detail.js
+++ code/src/Components/Detail.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 
-import { DETAIL_URL } from "utils/urls";
+import { LIST_URL } from "utils/urls";
 
 const Detail = () => {
     const [movieDetail,setMovieDetail] = useState([])
     const { movieId } = useParams()
 
     useEffect(() => {
-        fetch(DETAIL_URL, (movieId))
+        fetch(LIST_URL)
         .then(res=> res.json())
         .then(data=> { 
-            setMovieDetail(data)
+            setMovieDetail(data.results)
         })
-    }, [])
+    }, [movieId])
 
   return (
     <div>
-      {movieDetail.title}
-         Detail
+      {movieDetail.map((popular) => ( 
+      <div key={popular.id}>
+         <img className="card-images" src={`https://image.tmdb.org/t/p/w300${popular.poster_path}`} alt={popular.original_title}/>
+         <img className="card-images" src={`https://image.tmdb.org/t/p/w1280${popular.backdrop_path}`} alt={popular.original_title}/>
+         <p>{popular.original_title}</p>
+         <p>{popular.vote_average}</p>
+         <p>{popular.overview}</p>
          </div>
-
+      ))}
+    </div>     
   ) 
 };
 export default Detail;
--- code/src/Components/PopularList.js
+++ code/src/Components/PopularList.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const PopularList = ({movielist}) => {
     
   return ( 
 
     <main> 
-        {movielist.map((popular) => { 
-            return (<div key={popular.id}>
+        {movielist.map((popular) => (  
+             <div key={popular.id}>
                 <img className="card-images" src={`https://image.tmdb.org/t/p/w300${popular.backdrop_path}`} alt={popular.original_title}/>
-                <h3>{popular.original_title}</h3>
+                <h3>
+                <Link to={`/movie/${popular.id}`}>
+                {popular.original_title}
+                </Link>
+                </h3>
                 <p>{popular.release_date}</p>
-            </div>)
-        })}
-        
-        </main>
+            </div>
+        ))}
+
+        </main>   
   )
   
 };
