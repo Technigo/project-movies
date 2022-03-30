121eeb36be41eb64efa3daeeda56633e13a857b0
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-03-30 14:39:54 +0200

    work on details component, added movie details fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Details } from "Pages/Details";
 import { Movies } from "Pages/Movies";
+import { Details } from "Pages/Details";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Movies />} />
+        <Route path="/details/:detailsId" element={<Details />} />
       </Routes>
     </BrowserRouter>
   );
--- code/src/Pages/Details.js
+++ code/src/Pages/Details.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-const Details = () => {
+export const Details = () => {
+  const { detailsId } = useParams()
 
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${detailsId}?api_key=3d84d8a2e7d9f4fcf68bb05afb8dd7ef&language=en-US`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+      })
+  }, [detailsId])
+
+  return (
+    <div>Movie details in progress... <span role="img" aria-label="movie emoji">&#127909;</span></div>
+  )
 }
 
-export default Details
+// the movie emoji in span is just me being tired and silly :)
