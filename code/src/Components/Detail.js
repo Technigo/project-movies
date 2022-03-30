e3847d94898b58d4a8568388b1b4d9e45195ca23
TereseClaesson <terese.claesson@gmail.com>
2022-03-30 17:59:09 +0200

    Styled components

--- code/src/Components/Detail.js
+++ code/src/Components/Detail.js
@@ -2,21 +2,44 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components'
 
-import { DETAIL_URL, LIST_URL } from "utils/urls";
+import { DETAIL_URL } from "utils/urls";
+
+const Section = styled.section `
+min-height: 100vh;
+background-repeat: no-repeat;
+background-size: cover;
+display: grid;
+padding: 5vw;
+background-image: url(${(props) => props.backgroundimage});
+`;
+
+const DetailContainer = styled.div `
+display:flex;
+flex-direction: flex-end;
+position:realtive;
+`;
 
 const GoBackButton = styled.button `
     background-color:green;
+    border-radius: 50%;
 `;
 
-const BackgroundImage = styled.img `
-width: 150px;
-
+const Poster = styled.img `
+border: 3px solid #fff; 
 `;
+
+const DetailsText = styled.div `
+justify-content:flex-start;
+align-self:flex-end;
+color:#fff;
+`;
+
 
 const Detail = () => {
     const [movieDetail,setMovieDetail] = useState(null)
     const { movieId } = useParams()
     const navigate = useNavigate();
+    const backgroundImageUrl = `https://image.tmdb.org/t/p/w1280${movieDetail?.backdrop_path}`;
 
     const onBackButtonClick = () => {
       navigate(-1);
@@ -27,27 +50,26 @@ const Detail = () => {
         .then(res=> res.json())
         .then(data=> { 
             setMovieDetail(data)
-        
         })
     }, [movieId])
 
 console.log(movieDetail)
+
 if (movieDetail === null) {
   return <p></p>
 }
   return (
-    <div>
-      <GoBackButton onClick={onBackButtonClick}>Go Back</GoBackButton>
-      
- 
-         <img className="card-images" src={`https://image.tmdb.org/t/p/w300${movieDetail.poster_path}`} alt={movieDetail.original_title}/>
-         <BackgroundImage  src={`https://image.tmdb.org/t/p/w1280${movieDetail.backdrop_path}`} alt={movieDetail.original_title}/>
-         <p>{movieDetail.original_title}</p>
-         <p>{movieDetail.vote_average}</p>
+    <Section backgroundimage={backgroundImageUrl}>
+    <DetailContainer>
+      <GoBackButton onClick={onBackButtonClick}>Back</GoBackButton>
+         <Poster src={`https://image.tmdb.org/t/p/w300${movieDetail.poster_path}`} alt="poster-image"/>
+         <DetailsText>
+         <p>{movieDetail.original_title} {movieDetail.vote_average}</p>
          <p>{movieDetail.overview}</p>
-        
-      
-    </div>     
+         </DetailsText>
+    </DetailContainer>  
+    </Section>   
   ) 
 };
 export default Detail;
+
--- code/src/Components/PopularList.js
+++ code/src/Components/PopularList.js
@@ -1,24 +1,93 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const PopularContainer = styled.div `
+display:flex;
+flex-wrap:wrap;
+position:relative;
+background-color:#000;
+`;
+
+const CardContainer = styled.div `
+position:relative;
+
+
+img{
+  width:100%;
+  height:100%
+}
+
+a{
+  display:flex;
+}
+
+
+@media(min-width: 767px) {
+  width: 50%;
+  height: 100%;
+}
+@media(min-width: 1024px) {
+  width: 25%;
+  height: 100%;
+}
+`;
+
+const MovieCardOverlay = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  opacity: 0;
+  transition: 0.5s ease;
+  background: rgba(0, 0, 0, 0.75);
+  display: flex;
+  padding: 1.5rem;
+  box-sizing: border-box;
+  &:hover{
+    opacity: 1;
+  }
+  `;
+
+const MovieText = styled.div `
+justify-content:flex-start;
+align-self:flex-end;
+color:#fff;
+`;
+
+const MovieTitle = styled.div `
+font-size: 30px;
+`;
+
+const ReleaseText = styled.div ` 
+font-size: 16px;
+`;
+
+
 
 const PopularList = ({movielist}) => {
     
   return ( 
 
-    <main> 
+    <PopularContainer> 
         {movielist.map((popular) => (  
-             <div key={popular.id}>
-                <img className="card-images" src={`https://image.tmdb.org/t/p/w300${popular.backdrop_path}`} alt={popular.original_title}/>
-                <h3>
-                <Link to={`/movie/${popular.id}`}>
-                {popular.original_title}
+             <CardContainer key={popular.id}>
+                 <Link to={`/movie/${popular.id}`} key={popular.id}>
+                <img src={`https://image.tmdb.org/t/p/w342${popular.poster_path}`} alt="Movies"></img>
+                <MovieCardOverlay>
+                <MovieText>
+                <MovieTitle>{popular.original_title}</MovieTitle>
+                <ReleaseText>{popular.release_date}</ReleaseText>
+                </MovieText>
+                </MovieCardOverlay>
                 </Link>
-                </h3>
-                <p>{popular.release_date}</p>
-            </div>
+            </CardContainer>
         ))}
 
-        </main>   
+        </PopularContainer>   
   )
   
 };
