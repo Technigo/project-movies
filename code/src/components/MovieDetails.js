612ed48dafb3deb823888c9651f8ae1822afdd46
Jessica Sandler <jessica.sandler@hotmail.com>
2022-03-31 20:22:27 +0200

    Added containers for styling

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -14,8 +14,8 @@ const MovieDetails = () => {
 
     const Background = styled.div`
     background-size: cover; 
-    width: 100vw;
-    border: 1px solid green;
+    min-height: 100vh;
+    position:relative;
     `
 
     const GoBackButton = styled.button`
@@ -30,9 +30,28 @@ const MovieDetails = () => {
             padding: 5px;
         }
 `
-    const onBackButtonClick = ()=> {
-    navigate(-1);
-}
+    const onBackButtonClick = () => {
+        navigate(-1);
+    }
+
+    const DetailContainer = styled.div`
+    display:flex;
+    align-items: flex-end;
+    justify-content: flex-start;
+    flex-direction:row; 
+    padding: 10px;    
+    margin-bottom: 2rem;
+    `
+
+    const TitleRatingContainer = styled.div`
+    display:flex;
+    justify-content:flex-start;
+    flex-direction:column; 
+    padding: 10px; 
+    max-width: 30vw;     
+    background-color: rgba(220,220,220, 0.3)
+    `
+
 
     useEffect(() => {
         fetch(MOVIE_DETAIL_URL(id))
@@ -42,26 +61,22 @@ const MovieDetails = () => {
 
     console.log(movie)
 
-    // const backdrop = BACKDROP_URL + movie.backdrop_path
-    // const poster = POSTER_URL + movie.poster_path
+    const backdrop = `${BACKDROP_URL}${movie.backdrop_path}`
+    const poster = POSTER_URL + movie.poster_path
 
 
     return (
-        <article>  
-            
-            
-        
-            {/* <img src={BACKDROP_URL + movie.backdrop_path} alt={movie.title} /> */}
-            {/* <Background style={{backgroundImage: `(url(${movie.backdrop_path ? BACKDROP_URL + movie.backdrop_path : ''}))`}}> */}
-            <Background style={{backgroundImage: `(url(${BACKDROP_URL + movie.backdrop_path}))`}}>
-            <h2>{movie.title} {movie.vote_average}/10</h2>
-           
-            <img src={movie.backdrop_path ? BACKDROP_URL + movie.backdrop_path : ''} alt={movie.title} />
-            <img src={movie.poster_path ? POSTER_URL + movie.poster_path : ''} alt={movie.title} />     
-
-            <h3>{movie.tagline}</h3>
-            <p>{movie.overview}</p>
-            <GoBackButton onClick={onBackButtonClick}><FaArrowLeft />Back to movielist</GoBackButton>
+        <article>
+            <Background style={{ backgroundImage: `url(${backdrop})` }}>
+                <GoBackButton onClick={onBackButtonClick}><FaArrowLeft />Back to movielist</GoBackButton>
+                <DetailContainer>
+                    <img src={movie.poster_path ? poster : ''} alt={movie.title} />
+                    <TitleRatingContainer>
+                    <h2>{movie.title} {movie.vote_average}/10</h2>
+                    <h3>{movie.tagline}</h3>
+                    <p>{movie.overview}</p>
+                    </TitleRatingContainer>
+                </DetailContainer>
             </Background>
         </article>
     )
@@ -69,7 +84,3 @@ const MovieDetails = () => {
 
 export default MovieDetails
 
-/* <img
-  src={movie.poster_path ? `${apiImageAddress}w300${movie.poster_path}` : ''}
-  alt="movie_picture"
-/> */
