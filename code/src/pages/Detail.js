7b7d263417a381ba083380e36c914148a6f940f3
motherofbeasts <91845855+motherofbeasts@users.noreply.github.com>
2022-03-31 14:58:28 +0200

    added css to detail page

--- code/src/pages/Detail.js
+++ code/src/pages/Detail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { API_DETAILS } from '../utils/urls'
+import '../css/detail.css'
 
 
 const Detail = () => {
@@ -18,14 +19,18 @@ useEffect (() => {
     }, [movieId]) 
 
     return (
-        <section>
+        <section className="detailGrid">
             {details && (
              <div style={{ backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${details.backdrop_path})` }} >
                 <div>
-                    <button onClick={onBackButtonClick}>Back to list of movies</button>
-                    <h1>{details.title}{details.vote_average}/10</h1>
-                    <p>{details.overview}</p>
+                    <button onClick={onBackButtonClick}>back to movies</button>
+                    <div className="detailContent">
                     <img src={`https://image.tmdb.org/t/p/w342${details.poster_path}`} alt={details.title} />
+                    <div className="detailText">
+                    <h1 className="detailTitle">{details.title}{details.vote_average}/10</h1>
+                    <p className="detailBread">{details.overview}</p>
+                    </div>
+                   </div>
                 </div>
              </div>
             )} 
