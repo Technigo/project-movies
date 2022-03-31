446f77163e3668412b4fb8dacd57e5b104c52313
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-03-31 10:54:52 +0200

    started with styling homepage

--- code/src/components/MovieCards.js
+++ code/src/components/MovieCards.js
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom";
 
 const MovieCards = ({movies}) => {
     return (
-        <section className="move-cards">
+        <section className="movie-cards">
             {movies.map((movie) => (
-                <Link key={movie.id} to={`/moviedetails/${movie.id}`} >
-                    <div className="image-overlay">
-                        <h3
-                            className="movie-title">
-                            {movie.title}
-                        </h3>
-                        <p>Release date {movie.release_date}</p>
+                <Link className="link-container" key={movie.id} to={`/moviedetails/${movie.id}`} >
+                    <div className="img-container">
                         <img 
                             className="movie-poster"
                             src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
                             alt={movie.title}
                         />
+                    </div>
+                    <div className="info-container">
+                        <h3
+                            className="movie-title">
+                            {movie.title}
+                        </h3>
+                        <p>Release date {movie.release_date}</p>
                     </div>
                 </Link>
             ))}
