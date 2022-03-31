7b06a56ae49fbb6db42778100b2040f9a3152da4
Joanna Lodell <joannalodell@c80-216-145-228.bredband.tele2.se>
2022-03-31 16:50:41 +0200

    [JL] styling and added more details

--- code/src/pages/MovieDetails.js
+++ code/src/pages/MovieDetails.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import styled from "styled-components"
 
-export const StyledMovieDetails = styled.div `
-    background-color: red;
-`
 
 export const MovieDetails = () => {
     const { id } = useParams()
@@ -18,14 +15,82 @@ export const MovieDetails = () => {
             })
     }, [id])
 
+    const StyledMovieDetails = styled.article `
+    background-image: url("https://image.tmdb.org/t/p/w1280${movie.backdrop_path}");
+    min-height: 100vh;
+    display: flex;
+    background-size: cover;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    .backLink {
+        color: white; 
+        padding: 10px;
+        top: 5%;
+        left: 5%;
+        position: absolute;
+        font-weight: 700;
+        text-decoration: none;
+    }
+
+    .backArrow {
+        background: white;
+        color: black; 
+        border-radius: 50%;
+    }
+
+    .summary {
+        display: flex;
+        align-items: end;
+        padding: 30px; 
+    }
+
+    .poster {
+        border: solid white 5px;
+    }
+
+    h1 {
+        color: white;
+        font-size: 20px;
+    }
+
+    .rating {
+        color: red;
+        font-size: 17px;
+        font-weight: 400;
+        margin-left: 10px;
+    }
+
+    .details {
+        padding: 20px;
+    }
+
+    .details p {
+        max-width: 400px;
+        color: white;
+        font-size: 16px;
+    }
+`
+
     return (
         <StyledMovieDetails>
-            <div key={movie.id} >
-                <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
-                <h1>{movie.title}</h1>
-                <p>{movie.vote_average}/10</p>
-                <p>{movie.overview}</p>
-            </div>
+            <article className="movieDetailsPage">
+                {/* <img className="backdrop" src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={movie.title}/> */}
+                <Link to="/" className="backLink">
+                    <span className="backArrow">&#60;</span>Movies
+                </Link>
+                <div key={movie.id} className="summary">
+                    <img className="poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title}/>
+                    <div className="details">
+                        <h1>
+                            {movie.title}
+                            <span className="rating">{movie.vote_average}/10</span>
+                        </h1>
+                        <p>{movie.overview}</p>
+                    </div>
+                </div>
+            </article>
         </StyledMovieDetails>
     )
+    
 }
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,6 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { StyledMovieDetails } from "./MovieDetails"
+import styled from 'styled-components'
+
+const StyledMovieList = styled.div`
+    background: #000; 
+
+    .movieListPage {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .movieListPage a {
+        width: 25%;
+        position: relative;
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .movieListPage a img {
+        width: 100%;
+    }
+
+    .listDetails {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background: rgba(0,0,0,.75);
+        display: none;
+        padding: 20px;
+    }
+
+    & a:hover .listDetails {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+    }
+    
+`
 
 export const MovieList = () => {
     const [movies, setMovies] = useState ([])
@@ -16,15 +54,20 @@ export const MovieList = () => {
 
 
     return (
-        <div>
-            {movies.map(movie => (
-                <Link key={movie.id} to={`/movie/${movie.id}`} >
-                <div >
-                    <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
-                    <h2>{movie.title}</h2>
-                </div>
-                </Link>
-            ))}
-        </div>
+        <StyledMovieList>
+            <div className="movieListPage">
+                {movies.map(movie => (
+                    <Link key={movie.id} to={`/movie/${movie.id}`} >
+                    <div>
+                        <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+                        <div className="listDetails">
+                            <h1>{movie.title}</h1>
+                            <p>Released {movie.release_date}</p>
+                        </div>
+                    </div>
+                    </Link>
+                ))}
+            </div>
+        </StyledMovieList>
     )
 }
