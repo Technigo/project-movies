8947a128f024170ab6b4f8d8ce830f6b5b6559bb
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-03-31 11:56:12 +0300

    added sorting funtionality

--- code/src/Components/MovieDetails.js
+++ code/src/Components/MovieDetails.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, Link, useNavigate} from 'react-router-dom'
+import { useParams, useNavigate} from 'react-router-dom'
 import { NotFound } from './NotFound'
 
 
@@ -25,14 +25,12 @@ export const MovieDetails = ( { movieList } ) => {
     }
   return (
     <main className="moviedetail-wrapper" style={backgroundStyle}>
-      {/* <Link to={`/`}  className="back-link"> */}
       <div className="back-link">
-      <svg width="25" height="25" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <button onClick={onBackButtonClick} className="back-btn">
+                <svg width="25" height="25" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fillRule="evenodd" clipRule="evenodd" d="M80 40C80 62.0914 62.0914 80 40 80C17.9086 80 0 62.0914 0 40C0 17.9086 17.9086 0 40 0C62.0914 0 80 17.9086 80 40ZM24.8932 46.3017L24.8674 46.3256L19 40.8874L19.0258 40.8635L19 40.8395L24.8674 35.4013L24.8932 35.4253L49.0882 13L54.9556 18.4382L30.7606 40.8635L54.0378 62.438L48.1704 67.8763L24.8932 46.3017Z" fill="white"/>
-      </svg>
-        <button onClick={onBackButtonClick} className="back-btn">Movies</button>
+      </svg><span className="back-btn--text"> Movies</span></button>
         </div>
-      {/* </Link> */}
       <div className="detail-wrapper">
         <img className="detail-info-img"src={`https://image.tmdb.org/t/p/w342${movieMatch.poster_path}`} alt="poster"/>
         <div className="detail-info">
--- code/src/Components/MovieList.js
+++ code/src/Components/MovieList.js
@@ -1,23 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from 'react-router-dom'
 
 export const MovieList = ({movieList}) => {
-
     console.log('movielist', movieList)
 
+    const [sorting, setSorting] = useState('')
+    const [newMovieList, setNewMovieList] = useState(movieList)
+
+    useEffect(() => {
+        if (sorting === 'popularity'){
+            const sortedMovieList = [...newMovieList]
+            setNewMovieList(sortedMovieList.sort((a,b) => a.popularity < b.popularity ? 1 : -1))
+           
+        } else if (sorting === 'released') {
+            const sortedMovieList = [...newMovieList]
+            setNewMovieList(sortedMovieList.sort((a,b) => a.release_date < b.release_date ? 1 : -1))
+           
+        }
+    }, [sorting])
+
     return(
-        <section className="main">
-            {movieList.map((movie) => (
-                <Link to={`/details/${movie.id}`} className="movie-wrapper" key={movie.id}>
-                <div className="movie">
-                    <img className="poster-img"src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie poster"/>
-                </div>
-                    <div className="overlay">
-                      <h1>{movie.title}</h1>
-                      <p>Released {movie.release_date}</p>
+        <main>
+            <label htmlFor="sorting">
+                 <select id="sorting" value={sorting} onChange={(e) => setSorting(e.target.value)}>
+                    <option value="popularity">most popular</option>
+                    <option value="released">recently released</option>
+                </select>
+            </label>
+            <section className="main">
+                {newMovieList.map((movie) => (
+                    <Link to={`/details/${movie.id}`} className="movie-wrapper" key={movie.id}>
+                    <div className="movie">
+                        <img className="poster-img"src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie poster"/>
                     </div>
-                </Link>   
-            ))}
-        </section>
+                        <div className="overlay">
+                        <h1>{movie.title}</h1>
+                        <p>Released {movie.release_date}</p>
+                        </div>
+                    </Link>   
+                ))}
+            </section>
+        </main>
     )
 }
