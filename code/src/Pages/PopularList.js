daff3203681503a52a8634003ade87c5bdd34747
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-03-31 13:09:25 +0200

    fixed bugs for netlify

--- code/src/Pages/PopularList.js
+++ code/src/Pages/PopularList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const PopularList = () => {
   const [movies, setMovies] = useState([])
