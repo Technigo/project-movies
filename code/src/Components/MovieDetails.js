fc22a2f15d64475b30e0beebd1a06f3c0c931616
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-04-01 16:18:48 +0200

    added ternary operator to image fetch

--- code/src/Components/MovieDetails.js
+++ code/src/Components/MovieDetails.js
@@ -27,7 +27,7 @@ const MovieDetails = () => {
       <button onClick={onBackButtonClick}>⇦ Movies</button>
       <div className="background" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0) 40%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${movieDetails.backdrop_path})` }}>
         <div className="more-info">
-        <img className="poster" src={`https://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt={movieDetails.original_title} />
+        <img className="poster" src={movieDetails.poster_path ? `https://image.tmdb.org/t/p/w342${movieDetails.poster_path}` : ''} alt={movieDetails.original_title} />
           <div className="more-info-text">
             <h3>{movieDetails.original_title}<span className="highlight">{movieDetails.vote_average}/10</span></h3>
             <p>{movieDetails.overview}</p>
