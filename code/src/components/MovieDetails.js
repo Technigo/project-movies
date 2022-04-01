9054afaed9d30915bce220658c18479dcddc114b
Louisa <lundislon@gmail.com>
2022-04-01 15:56:25 +0200

    added styled component button

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from "react-router-dom";
-// import {SINGLE_MOVIES_URL} from "utils/urls" //../utils/urls?
-import "../css/moviedetails.css";
+// import {SINGLE_MOVIES_URL} from "utils/urls"
 import styled from "styled-components";
 
-// const GoBackButton = styled.button`
-//     padding: 10px;
-//     color: green;
-//     background-color: red;
-//     font-style: italic;
-// `;
 
 const BackDrop = styled.div`
 background-image: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%), url(${props => props.url});
 min-height: 100vh;
 background-size: cover;
-
 `;
 
+const GoBackButton = styled.button`
+    padding: 10px;
+    margin-top: 35px;
+    width: 75px;
+    color: white;
+    background-color: grey;
+`;
 
    // const backgroundImageUrl = `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
     // const background = {
@@ -45,7 +44,7 @@ const MovieDetails = () => {
     return (
         <BackDrop url={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
                 <div className="details-container">
-                    <button onClick={onBackButtonClick}>Go back</button>
+                    <GoBackButton onClick={onBackButtonClick}>Go back</GoBackButton>
                     <div className="container">
                         <div className="info-child">
                             <h1 className="movie-title">{movie.title}</h1>
@@ -66,4 +65,3 @@ const MovieDetails = () => {
 }
 
 export default MovieDetails;
-
--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -4,24 +4,3 @@ export const URL_MOVIES = 'https://api.themoviedb.org/3/movie/popular?api_key=e9
 // export const URL_DETAILS ='https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US';
 
 // export const SINGLE_MOVIES_URL = (movieId) => `https://api.themoviedb.org/3/movie/${movieId}?api_key=e99c6c8e9d657c50fc40ffe9db9d16f5&language=en-US`;
-
-
-
-
-
-
-// import React from 'react';
-
-
-// export const MovieCards = ({leias}) => {
-//     return (
-//         <section>
-//             {leias.map((leia) => (
-//                 <div key={leia.title}>
-//                     <h3>{leia.title}</h3>
-//                     <img className="movie-poster" src={`https://image.tmdb.org/t/p/w300${leia.poster_path}`} alt={leia.title}/>
-//                 </div>
-//             ))}
-//         </section>      
-//     )
-// }
