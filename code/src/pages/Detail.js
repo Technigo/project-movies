e2721cb953add148d57e45b7d4738b5294f474c1
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-04-01 09:21:59 +0200

    just trying that commit works after merge

--- code/src/pages/Detail.js
+++ code/src/pages/Detail.js
@@ -27,7 +27,8 @@ useEffect (() => {
                     <div className="detailContent">
                     <img src={`https://image.tmdb.org/t/p/w342${details.poster_path}`} alt={details.title} />
                     <div className="detailText">
-                    <h1>{details.title}{details.vote_average}/10</h1>
+                    <h1>{details.title}</h1>
+                    <p>{details.vote_average}/10</p>
                     <p>{details.overview}</p>
                     </div>
                    </div>
