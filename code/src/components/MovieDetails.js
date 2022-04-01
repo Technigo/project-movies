0cb4a756d5e489b28fd27381518c215139b3a059
Arianna Allahyar <anallahyar@gmail.com>
2022-04-01 17:32:44 +0200

    Desktop styling added

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -12,14 +12,17 @@ const MovieDetails = ({ movies }) => {
     <section className="movie-container">
       <Link className="back-link" to={"/"}>
         <img className="back-icon" src="../../backarrow.png" alt="back arrow" />
-				<span className="back-label">Films</span>
+        <span className="back-label">Films</span>
       </Link>
-      {/* <img className="movie-poster" src={movie.coverImgUrl} alt={movie.coverImgUrl} /> */}
       <img className="movie-poster" src={movie.backdropImgUrl} alt={movie.backdropImgUrl} />
-      <h2 className="movie-title">{movie.title}</h2>
-      <p className="movie-rating">{movie.rating}/10</p>
-      <p>{movie.description}</p>
-      <img className="movie-background" src={movie.backdropImgUrl} alt={movie.backdropImgUrl} />
+      <div className="movie-desktop-details">
+        <img className="movie-cover" src={movie.coverImgUrl} alt={movie.coverImgUrl} />
+        <div className="movie-details-group">
+          <h2 className="movie-title">{movie.title}</h2>
+          <p className="movie-rating">{movie.rating}/10</p>
+          <p>{movie.description}</p>
+        </div>
+      </div>
     </section>
   );
 };
