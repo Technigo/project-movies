5f4a7070c80fdb3fe4054dc35296745902cdfd63
Jacob <jacob@engd.se>
2022-04-02 15:44:21 +0200

    added previous-button

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import "./Details.css"
 
@@ -8,6 +8,7 @@ import { DETAILS_URL } from "../utils/urls"
 const Details = () => {
   const [details, setDetails] = useState([])
   const { movieId } = useParams() //access parameters from the URL
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(DETAILS_URL(movieId))
@@ -19,6 +20,7 @@ const Details = () => {
     <div className='details'>
       <img className='backdrop' src={`https://image.tmdb.org/t/p/original${details.backdrop_path}`} alt="backdrop" />
       <div className='details-wrapper'>
+      <div className="backbutton" onClick={() => navigate(-1)}> <i className="arrow-left"></i></div>
         <img className="movie-img" src={`https://image.tmdb.org/t/p/w342${details.poster_path}`} alt={details.title} />
         <div className='details-info'>
           <div className='details-title-votes white'>
