9f985fc1db0ef34518889e3403531c4661346fd9
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-04-02 17:50:06 +0200

    merged more styling for detail text

--- code/src/pages/PopularList.js
+++ code/src/pages/PopularList.js
@@ -6,7 +6,8 @@ const PopularList = ({movielist}) => {
     return (
         <div className="movie-container">
             {movielist.map((movie) => (
-                <Link className="link-container"
+                <Link
+                   className="link-container"
                    key={movie.title}
                    to={`/details/${movie.id}`}
                 >
@@ -14,12 +15,10 @@ const PopularList = ({movielist}) => {
                         src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
                         alt={movie.title}
                     />
-                    {/* <div className="overlay"> */}
                       <div className="title-container">
                         <h1>{movie.title}</h1>
                         <p>Released {movie.release_date}</p>
                       </div> 
-                    {/* </div>  */}
                 </Link>
             ))}
         </div>
