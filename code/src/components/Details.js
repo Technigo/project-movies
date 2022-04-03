d8ad6dd083d577e5a2ce85b6e20824e92b3ab96a
Gonçalo Vilela <goncalocvv@gmail.com>
2022-04-03 23:42:04 +0200

    Styled detail and error page

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { MOVIE_DETAILS } from "utils/Urls";
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { MOVIE_DETAILS } from 'utils/Urls';
 
+import { AiFillCaretLeft } from 'react-icons/ai';
 
 const Details = () => {
+	const { movieId } = useParams();
 
-	const {movieId} = useParams()
-
-	const [details, setDetails] = useState([])
+	const [ details, setDetails ] = useState([]);
 
 	const navigate = useNavigate();
 
@@ -15,44 +15,46 @@ const Details = () => {
 		//Use this if go to previous page
 		//navigate(-1)
 
-		navigate('/')
-	}
-
-	useEffect(() => {
-		fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=25d003084b426a61d39038ca690f9a7c&language=en-US`)
-		  .then((res) => res.json())
-		  .then((data) => setDetails(data));
-	  }, [movieId]);
-
-	  console.log(details)
-
-	  return (
-		<section> 
-			{details.backdrop_path && 
-			<div
-			className="details-container"
-			style={{
-			  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/original${details.backdrop_path})`,
-			}}
-			>
-			<button onClick={onBackButtonClick}>Movies</button>
-			  <div>
-				<img
-				  className="movie-details-cover-image"
-				  src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`}
-				  alt={details.title}
-				/>
-				<div className="movie-introduction">
-				  <h1>
-					{details.title}
-					<span className="votes">{details.vote_average}/10</span>
-				  </h1>
-				  <p>{details.overview}</p>
-				</div>
-			  </div>
-			</div>}
-		</section>
-	  );
+		navigate('/');
 	};
 
-export default Details
+	useEffect(
+		() => {
+			fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=25d003084b426a61d39038ca690f9a7c&language=en-US`).then((res) => res.json()).then((data) => setDetails(data));
+		},
+		[ movieId ]
+	);
+
+	console.log(details);
+
+	return (
+		<section>
+			{details.backdrop_path && (
+				<div
+					className="details-container"
+					style={{
+						backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/original${details.backdrop_path})`
+					}}
+				>
+					<button onClick={onBackButtonClick}>
+						<span>
+							<AiFillCaretLeft />
+						</span>Movies
+					</button>
+					<div className="details">
+						<img className="movie-details-cover-image" src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`} alt={details.title} />
+						<div className="movie-introduction">
+							<h1>
+								{details.title}
+								<span className="votes">{details.vote_average}/10</span>
+							</h1>
+							<p>{details.overview}</p>
+						</div>
+					</div>
+				</div>
+			)}
+		</section>
+	);
+};
+
+export default Details;
--- code/src/components/NotFound.js
+++ code/src/components/NotFound.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 const NotFound = () => {
-    const navigate = useNavigate()
-    const onHomeButtonClick = () => {
-        navigate('/')
-    }
+	const navigate = useNavigate();
+	const onHomeButtonClick = () => {
+		navigate('/');
+	};
 
-    return(
-        <div>
-            <p>Sorry this page not found</p>
-            <button onClick={onHomeButtonClick}>Go homepage</button>
-        </div>
-    )
-}
+	return (
+		<div className="not-found">
+			<p className="error">Sorry this page was not found</p>
+			<button className="home-page" onClick={onHomeButtonClick}>
+				Go to homepage
+			</button>
+		</div>
+	);
+};
 
-export default NotFound
+export default NotFound;
