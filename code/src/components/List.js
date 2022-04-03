235239ab7a0e54837ae374232e3351566176ac61
Gonçalo Vilela <goncalocvv@gmail.com>
2022-04-03 21:32:17 +0200

    Styled homepage

--- code/src/components/List.js
+++ code/src/components/List.js
@@ -1,25 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const List = ({list}) => {
+const List = ({ list }) => {
+	console.log(list);
 
-	console.log(list)
-
-    return(
+	return (
 		<section className="wrapper">
-		{list.map((movie) => (
-			<Link to={`${movie.id}`} key={movie.id}>
-			<div>
-			 <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="cover of movie"/>
-			</div>
-			 <div>
-			   <h1>{movie.title}</h1>
-			   <p>Released {movie.release_date}</p>
-			 </div>
-			</Link>
-		))}
-	</section>
-    )
-}
+			{list.map((movie) => (
+				<Link to={`${movie.id}`} key={movie.id}>
+					<div className="container">
+						<img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="cover of movie" />
+						<div className="movie-details">
+							<h1>{movie.title}</h1>
+							<p>Released {movie.release_date}</p>
+						</div>
+					</div>
+				</Link>
+			))}
+		</section>
+	);
+};
 
-export default List
+export default List;
