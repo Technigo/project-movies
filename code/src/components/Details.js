9e4337bbc865eba115bac4b2f8e644a98bbfd060
anki palviainen <anki.palviainen@gmail.com>
2022-04-04 08:37:30 +0300

    delete url

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { MOVIE_DETAILS } from 'utils/Urls';
 
 import { AiFillCaretLeft } from 'react-icons/ai';
 
