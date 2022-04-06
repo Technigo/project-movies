bbc2a10e3662fed63ab00cfd9e76cbb3e0e5dad0
Arianna Allahyar <anallahyar@gmail.com>
2022-04-06 13:39:12 +0200

    Link removed from select to avoid instant rerouting

--- code/src/components/smart/Header.js
+++ code/src/components/smart/Header.js
@@ -1,29 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./Header.css";
 
 const Header = ({ value, setSelection }) => {
+  let navigate = useNavigate();
+
   return (
     <header>
       <h1>Film Finder</h1>
       <nav>
-        <Link className="home" to="/">
-          <select
-            value={value}
-            onChange={(event) => {
-              setSelection(event.target.value);
-            }}
-          >
-            <option value="" disabled>
-              Choose film collection
-            </option>
-            <option value="now_playing">Now Playing</option>
-            <option value="upcoming">Upcoming</option>
-            <option value="popular">Popular</option>
-            <option value="top_rated">Top Rated</option>
-          </select>
-        </Link>
+        <select
+          value={value}
+          onChange={(event) => {
+            setSelection(event.target.value);
+            navigate("/");
+          }}
+        >
+          <option value="" disabled>
+            Choose film collection
+          </option>
+          <option value="now_playing">Now Playing</option>
+          <option value="upcoming">Upcoming</option>
+          <option value="popular">Popular</option>
+          <option value="top_rated">Top Rated</option>
+        </select>
       </nav>
     </header>
   );
