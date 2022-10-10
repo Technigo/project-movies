2a1fe86dc7e5f804f3db67a18d646ea7b9040c2a
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-10-10 11:27:26 +0200

    connection to API working

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export const App = () => {
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=f8eb3d0941c8cec7118b188a305910c5&language=en-US&page=1')
+.then((data) => data.json())
+.then((configuredData) => console.log(configuredData))
   return (
     <div>
       Find me in src/app.js!
