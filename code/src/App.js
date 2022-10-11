65ee13eeda6e77e5277ab6c95f0b912a28945515
Sailornina <sylcardozo.sc@gmail.com>
2022-10-11 11:08:43 +0200

    Home.js was created

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
 import List from './components/List';
@@ -8,10 +9,10 @@ import API_URL from './urls/urls'
 export const App = () => {
   return (
     <BrowserRouter>
+     <Home />
       <Routes>
-        <Route path="/" />
-        <List />
-        <Route path="/Movie" element={<Movie />} />
+        <Route path="/" element={<List/>} />
+        <Route path="/movie" element={<Movie />} />
         <Route path="/404" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
