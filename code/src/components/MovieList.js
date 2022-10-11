6bea766471a64d95694edfaaf760a5293e0a2f32
uohman <ulrikalovisa@gmail.com>
2022-10-11 20:17:20 +0100

    Fetched poster images

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -5,7 +5,16 @@ const MovieList = ({ movies }) => {
   return (
     <div>
       {movies.map((singleMovie) => {
-        return <Link key={singleMovie.id} to={`/details/${singleMovie.title}`}>{singleMovie.title}</Link>
+        return (
+          <Link
+            key={singleMovie.id}
+            to={`/details/${singleMovie.title}`}>
+            {singleMovie.title}
+            <img
+              src={`https://image.tmdb.org/t/p/w342${singleMovie.poster_path}`}
+              alt="movie_poster" />
+          </Link>
+        )
       })}
     </div>
   );
