7f2d54d4c23c9ec09de76ff87230278d8588431e
Petra Eriksson <eriksson.petra@gmail.com>
2022-10-12 12:08:09 +0200

    hover effect on MovieList

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -4,11 +4,12 @@ import { NavLink, Link } from 'react-router-dom'
 export const Header = () => {
   return (
     <header>
-      <NavLink to="/">  Home </NavLink>
-      <NavLink to="/details"> Details  </NavLink>
-      <Link to="/404"> Not found </Link>
+      <nav>
+        <NavLink to="/">  Home </NavLink>
+        <NavLink to="/details"> Details  </NavLink>
+        <Link to="/404"> Not found </Link>
+      </nav>
     </header>
-
   );
 }
 export default Header;
--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -13,6 +13,9 @@ const MovieList = ({ movies }) => {
             className="list-element"
             src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
             alt={movie.title} />
+          <div className="movie-info">
+            <h1>{movie.title}</h1>
+          </div>
         </Link>
       ))}
     </section>
