fa10c28546afccf988279b4be1ea14cb227fb6af
a-well <amandaemkwellington@gmail.com>
2022-10-12 11:37:34 +0200

    Add styling

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const MovieList = ({ movieList }) => {
   console.log(movieList)
   return (
-    <article>
+    <article className="poster-container">
       {movieList.map((movie) => {
         return (
           <Link className="movies" key={movie.id} to={`/moviedetails/${movie.title}`}>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} poster`} />
-            <div>
+            <img className="poster-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} poster`} />
+            <div className="poster-text-container">
               <h1>{movie.title}</h1>
               <p>Released: {movie.release_date}</p>
             </div>
