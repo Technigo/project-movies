7be30872c9a57cdfa2ce53a807bc56f7be72e4d7
katirum <kartikaprw@gmail.com>
2022-10-12 22:03:46 +0200

    styling, edit link & surprise animation

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import '../css/moviedetails.css'
+import RotatingPoo from './RotatingPoo';
 
 const MovieDetails = () => {
   const [movieDetail, setMovieDetail] = useState({})
   const [loading, setLoading] = useState(true)
   const { movieId } = useParams()
-  const navigate = useNavigate()
 
   console.log(movieId)
-  const onBackButtonClick = () => {
-    navigate(-1)
-  }
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=02c269c6419024906430a5cca2edf53f&language=en-US`)
@@ -21,16 +18,16 @@ const MovieDetails = () => {
       .finally(() => setLoading(false))
   }, [movieId])
   if (loading) {
-    return <h1>hold your poo</h1>
+    return <RotatingPoo />
   }
   console.log(movieDetail)
   return (
     <article className="main-container">
 
-      <button className="back-button" type="button" onClick={onBackButtonClick}>
+      <Link className="back-button" to="/">
         <img src="../cheveron-left.svg" alt="left arrow" className="left-arrow" />
       Movies
-      </button>
+      </Link>
       <div className="background" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/w1280/${movieDetail.backdrop_path})` }}>
         <div className="summary">
           <img src={`https://image.tmdb.org/t/p/w342${movieDetail.poster_path}`} alt="movie poster" />
---
+++ code/src/components/RotatingPoo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RotatingPoo = () => {
+  return (
+    <div className="relative">
+      <div className="loading">Hold your poo!</div>
+      <div className="the-poo">💩</div>
+    </div>
+  )
+}
+
+export default RotatingPoo
