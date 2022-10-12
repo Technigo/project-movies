232fcb529e7dba5601b0800d90361a7a220e0301
joelohman <joel.ohman@entryevent.se>
2022-10-12 22:10:18 +0200

    Added back file with correct name

---
+++ code/src/components/TheMovies.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-use-before-define */
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/themovies.css'
+import RotatingPoo from './RotatingPoo';
+
+const TheMovies = () => {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = () => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=02c269c6419024906430a5cca2edf53f&language=en-US&page=1')
+      .then((res) => res.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+  }
+
+  if (loading) {
+    return <RotatingPoo />
+  }
+  console.log(movies)
+
+  return (
+    <main className="main-page">
+      {movies.results.map((movie) => {
+        return (
+          <Link className="image-container" key={movie.id} to={`/movies/${movie.id}`}>
+            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="movie poster" />
+
+            <div className="overlay-detail">
+              <h1>{movie.title}</h1>
+              <p>Released {movie.release_date}</p>
+            </div>
+          </Link>
+        )
+      })}
+    </main>
+  )
+}
+
+export default TheMovies;
