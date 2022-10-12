f569b9a02a3d8b1db19819cf8a63b3f7604c7500
a-well <amandaemkwellington@gmail.com>
2022-10-12 10:27:37 +0200

    Add release date

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom'
 const MovieList = ({ movieList }) => {
   console.log(movieList)
   return (
-    <div>
+    <article>
       {movieList.map((movie) => {
         return (
           <Link className="movies" key={movie.id} to={`/moviedetails/${movie.title}`}>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Movie poster" />{movie.title}
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} poster`} />
+            <div>
+              <h1>{movie.title}</h1>
+              <p>Released: {movie.release_date}</p>
+            </div>
           </Link>
         )
       })}
-    </div>
+    </article>
   )
 }
 
