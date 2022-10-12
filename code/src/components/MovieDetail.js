2da5d5d2d4a5c2700955364399ccf7e0931dfa60
NaghmehOkhovat <99764109+NaghmehOkhovat@users.noreply.github.com>
2022-10-12 23:10:22 +0200

    Added loading component and style to rating box

--- code/src/components/MovieDetail.js
+++ code/src/components/MovieDetail.js
@@ -1,12 +1,15 @@
+/* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable no-tabs */
 /* eslint-disable no-lone-blocks */
 /* eslint-disable no-unreachable */
 import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom';
 import backIcon from './left-arrow.svg'
+import Loading from './Loading';
 
 const MovieDetail = () => {
   const [movieDetailList, setMovieDetailList] = useState([])
+  const [loading, setLoading] = useState(true)
   const { id } = useParams()
   // eslint-disable-next-line no-unused-vars
   useEffect(() => {
@@ -16,23 +19,30 @@ const MovieDetail = () => {
       .then((json) => {
         setMovieDetailList(json)
       })
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
   }, [id])
 
   return (
     <article
       className="movie-page"
       style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(https://image.tmdb.org/t/p/w1280${movieDetailList.backdrop_path})` }}>
-      <Link className="go-back" to="/"> <img className="back-icon" src={backIcon} alt="Go back" />Movies</Link>
-      <div className="movie-detail-info">
-        <img className="movie-poster" src={`http://image.tmdb.org/t/p/w342${movieDetailList.poster_path}`} alt={movieDetailList.original_title} />
-        <div className="movie-info">
-          <div className="rated-title">
-            <h2>{movieDetailList.title} </h2>
-            <p className="rating">⭐️ {Number(movieDetailList.vote_average).toFixed(1)}</p>
+      {loading === true
+        ? <Loading />
+        : <>
+          <Link className="go-back" to="/"> <img className="back-icon" src={backIcon} alt="Go back" />Movies</Link>
+          <div className="movie-detail-info">
+            <img className="movie-poster" src={`http://image.tmdb.org/t/p/w342${movieDetailList.poster_path}`} alt={movieDetailList.original_title} />
+            <div className="movie-info">
+              <div className="rated-title">
+                <h2>{movieDetailList.title} </h2>
+                <p className="rating">⭐️ {Number(movieDetailList.vote_average).toFixed(1)}</p>
+              </div>
+              <p>{movieDetailList.overview}</p>
+            </div>
           </div>
-          <p>{movieDetailList.overview}</p>
-        </div>
-      </div>
+        </>}
     </article>
 
   )
