988fe1278400b8dc44cdc16aa95310243a14b179
jnbjensen <mail@jnbjensen.com>
2022-10-13 14:57:33 +0200

    Submission-ready state

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -5,14 +5,17 @@ export const MovieList = ({ list }) => {
   return (
     <section className="list">
       {list.map((movie) => (
-        <div className="list-item">
-          <Link key={movie.list} to={`/moviedetails/${movie.id}`}>
-            {/* <h2>{movie.title}</h2> */}
-            <img
-              src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`}
-              alt={movie.title} />
-          </Link>
-        </div>
+        <Link key={movie.list} to={`/moviedetails/${movie.id}`}>
+          <div
+            className="list-item"
+            style={{ backgroundImage: `url(http://image.tmdb.org/t/p/w342/${movie.poster_path})` }}
+            alt={movie.title}>
+            <div className="overlay">
+              <h1>{movie.title}</h1>
+              <p>{movie.release_date}</p>
+            </div>
+          </div>
+        </Link>
       ))}
     </section>
   );
