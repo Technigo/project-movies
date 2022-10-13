c6bead212a976e02c6e3cdfc4e17ff89dc687860
Elin <elin.segelov@gmail.com>
2022-10-13 11:25:23 +0200

    Fix ratingbox issue

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import loadingIcon from './loading-icon.svg'
 
 const Loading = () => {
   return (
     <div className="loading">
       <p>Loading Loading Loading ...</p>
+      <img src={loadingIcon} alt="Loading" />
     </div>
   )
 }
