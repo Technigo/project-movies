43db3e8e80e4e00e007dc7c6847c26c5a9dbd21f
Scarylie <saralie.bognandi@gmail.com>
2022-10-13 15:09:41 +0200

    Removed setReady/Loading

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -7,26 +7,19 @@ const apiKey = 'd4669261ce30d2ac76f238d73f4bd890';
 
 const Details = () => {
   const [movie, setMovie] = useState({});
-  const [ready, setReady] = useState(false);
   const params = useParams();
   const navigate = useNavigate();
   const API_URL = `https://api.themoviedb.org/3/movie/${params.movieId}?api_key=${apiKey}&language=en-US&page=1`;
 
   useEffect(() => {
-    setReady(true);
     fetch(API_URL)
       .then((response) => response.json())
-      .then((json) => setMovie(json))
-      .catch((error) => console.error(error))
-      .finally(() => setReady(false));
-  }, []);
+      .then((json) => setMovie(json));
+  });
 
   const goBack = () => {
     navigate(-1);
   };
-  if (ready) {
-    return <p>Something is loading. Import Loader here</p>;
-  }
 
   return (
     <div
