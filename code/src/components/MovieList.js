97a7fd3efbacb007c441042361f3f7d2d7dcf530
carimore <110664183+carimore@users.noreply.github.com>
2022-10-13 09:21:32 +0200

    Update MovieList.js

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -22,21 +22,3 @@ const MovieList = ({ list }) => {
 
 export default MovieList;
 
-/* import React from 'react';
-import { Link } from 'react-router-dom';
-
-const MovieList = ({ list }) => {
-  return (
-    <div className="popular-list">
-      {list.map((movie) => {
-        return (
-          <Link key={movie.id} to={`/details/${movie.id}`}>
-            <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="movie_poster" />
-          </Link>
-        )
-      })}
-    </div>
-  );
-}
-
-export default MovieList; */
