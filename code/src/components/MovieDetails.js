3bd564aa8bb95c218f317c69b3c2126e1f46fc42
carimore <110664183+carimore@users.noreply.github.com>
2022-10-13 10:04:05 +0200

    Update MovieDetails.js

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -18,12 +18,16 @@ const MovieDetails = () => {
 
   return (
     <section className="details-page">
-      <button type="button" onClick={goBack}> Go Back </button>
-      <div className="movie-container">
-        <img className="detail-image" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
-        <div className="movie-description">
-          <h2>{movie.title}</h2>
-          <p>{movie.overview}</p>
+      <div className="background-image">
+        <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={movie.tittle} />
+        <button type="button" onClick={goBack}> Movies </button>
+        <div className="movie-container">
+          <img className="movie-poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+          <div className="movie-description">
+            <h2>{movie.title}</h2>
+            <h3>{Math.round(movie.vote_average * 10) / 10}</h3>
+            <p>{movie.overview}</p>
+          </div>
         </div>
       </div>
     </section>
@@ -31,40 +35,3 @@ const MovieDetails = () => {
 }
 
 export default MovieDetails;
-
-/* import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-
-const MovieDetails = () => {
-//   const params = useParams();
-  const { id } = useParams()
-  const [movie, setMovie] = useState({})
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=2248800ec8114dc953ca7e0362a1332b&language=en-US`)
-      .then((response) => response.json())
-      .then((json) => {
-        setMovie(json)
-        console.log('fetch ID', movie)
-      })
-  }, [id]);
-
-  const navigate = useNavigate();
-  useEffect(() => {
-    // console.log(id);
-  });
-  const goBack = () => {
-    navigate(-1);
-  }
-  return (
-    <div>
-      <button type="button" onClick={goBack}> Go back </button>
-      <h2>{movie.original_title}</h2>
-      <p>khsjashajsh{movie.overview}</p>
-      <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="POSTER" />
-    </div>
-  );
-}
-
-export default MovieDetails */
-
