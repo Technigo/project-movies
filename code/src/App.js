49862e33565c76ce1f675158a26ea05f42b72c93
jessikalind <jessika.lind@me.com>
2022-10-14 09:50:01 +0200

    start page styling almost done

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Movies from 'Components/Movies';
 import Details from 'Components/Details';
 
--- code/src/Components/Movies.js
+++ code/src/Components/Movies.js
@@ -6,18 +6,19 @@ const Movies = ({ movies }) => {
     <section className="movies">
       {movies.map((movie) => {
         return (
-          <Link
-            key={movie.title}
-            to={`/details/${movie.id}`}
-            className="movie">
-            <div className="image-container">
-              <img className="details-img" src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="poster" />
-            </div>
-            <div className="movie-details">
-              <h1> {movie.title} </h1>
-              <p> {movie.release_date} </p>
-            </div>
-          </Link>
+          <div className="movie-container">
+            <Link
+              key={movie.title}
+              to={`/details/${movie.id}`}
+              className="movie">
+              <div className="image-overlay" />
+                <img className="details-img" src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="poster" />
+              <div className="movie-details">
+                <h1> {movie.title} </h1>
+                <p> {movie.release_date} </p>
+              </div>
+            </Link>
+          </div>
         )
       })}
     </section>
