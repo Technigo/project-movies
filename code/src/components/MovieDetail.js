0be2c793536a6e7e39eb8e71f8becaf59a23a64c
NaghmehOkhovat <99764109+NaghmehOkhovat@users.noreply.github.com>
2022-10-14 09:04:30 +0200

    Added setError to MovieDetails for NotFound page

--- code/src/components/MovieDetail.js
+++ code/src/components/MovieDetail.js
@@ -1,21 +1,37 @@
 /* eslint-disable react/jsx-closing-tag-location */
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
 import GoBack from './GoBack';
 import Loading from './Loading';
+import NotFound from './NotFound';
 
 const MovieDetail = () => {
   const [movieDetailList, setMovieDetailList] = useState([])
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false);
   const { id } = useParams()
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=9316509df2a80192a5e837648d693f17&language=en-US`)
       .then((res) => res.json())
-      .then((json) => setMovieDetailList(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false))
+      .then((json) => {
+        if (json.id) {
+          setMovieDetailList(json);
+        } else {
+          setError(true);
+        }
+      })
+      .catch(() => setError(true))
+      // .catch((error) => console.error(error))
+      // .finally(() => setLoading(false))
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500)
   }, [id])
+
+  if (error) {
+    return <NotFound />;
+  }
 
   return (
     <article
