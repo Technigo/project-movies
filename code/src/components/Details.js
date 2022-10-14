d25f319e721c12fd5a9966c7070c52f85b309f50
YoTesfam <102465267+YoTesfam@users.noreply.github.com>
2022-10-14 14:01:04 +0200

    Media changes

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -52,6 +52,7 @@ const Details = () => {
             {' '}
             Back{' '}
           </button> */}
+          <div></div>
           <img
             src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
             alt={movie.title}
