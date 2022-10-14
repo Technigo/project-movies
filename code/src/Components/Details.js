0cd0c0a65d82943b4365f1cf0538f67e78da8b11
jessikalind <jessika.lind@me.com>
2022-10-14 11:48:07 +0200

    more media q

--- code/src/Components/Details.js
+++ code/src/Components/Details.js
@@ -21,13 +21,13 @@ const Details = () => {
       className="details-container"
       style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${details.backdrop_path})` }}>
       <button className="go-back-btn" type="button" onClick={onBackButtonClick}>Go back</button>
-      <img className="poster-img" src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`} alt="poster" />
+      <img className="poster-img" src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`} alt="poster" />
       <div className="movie-info">
         <div className="title-and-rating">
           <h1>{details.title}</h1>
-          <p>{details.vote_average}</p>
+          <p className="rating">⭐️{details.vote_average}</p>
         </div>
-        <p>{details.overview}</p>
+        <p className="overview">{details.overview}</p>
       </div>
     </section>
   )
