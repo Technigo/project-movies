df43f4f1790d4ff7f6e12718a59ce683369a71ed
a-well <amandaemkwellington@gmail.com>
2022-10-16 21:56:29 +0200

    Format date

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'components/Loader'
+import moment from 'moment'
 
 const MovieList = ({ movieList, loading }) => {
   console.log(movieList)
@@ -15,7 +16,7 @@ const MovieList = ({ movieList, loading }) => {
             <img className="poster-image" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={`${movie.title} poster`} />
             <div className="poster-text-container">
               <h1 className="poster-text-container-title">{movie.title}</h1>
-              <p>Released on {movie.release_date}</p>
+              <p>Release date: {moment(movie.release_date).format('D MMMM YYYY')}</p>
             </div>
           </Link>
         )
