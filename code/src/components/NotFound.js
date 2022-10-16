db46f04ba624a15cd45925cbd6df51ca34ff879c
KajaWilbik <wilbik.kaja@gmail.com>
2022-10-16 18:14:18 +0200

    button link

--- code/src/components/NotFound.js
+++ code/src/components/NotFound.js
@@ -2,26 +2,28 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate, Link } from 'react-router-dom';
 import workingInProgress from '../workingInProgress.png';
 import { MovieList } from './MovieList';
 
 export const NotFound = () => {
-  // tried to make back button from 404 error
-  // const navigate2 = useNavigate();
-  // useEffect(() => {
-  //   console.log('button2');
-  // });
-  // const goBack2 = () => {
-  //   <MovieList />
-  //   console.log('navigate', goBack2)
+  // ANOTHER WAY TO MAKE BUTTON RETURN TO MAKE PAGE USING onCLick=(redirect)
+  // const navigate2 = useNavigate()
+  // const redirect = () => {
+  //   console.log('click', redirect);
+  //   navigate2('/')
   // }
   return (
     <div className="notFound">
       <h2> OBS! It look like you got lost...... </h2>
       <h3>404 PAGE NOT FOUND</h3>
       <img src={workingInProgress} alt="working" />
-      <button type="button" onClick> 👈🏼 Movies </button>
+      <Link to="/">
+        <button
+          type="button"> 👈🏼 Go back to movies
+        </button>
+      </Link>
+
     </div>
   )
 }
