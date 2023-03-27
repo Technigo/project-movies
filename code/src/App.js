d3491fd1d8d6b042cd81564a2f61e9880cd2b793
Ed Code <edvard.ekstrom@gmail.com>
2023-03-27 21:57:15 +0200

    function init

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Details } from './components/Details'
+
 export const App = () => {
+  const movieId = '603692';
+  const apiKey = 'c78445f7216324dcfc408149681f2fcd'
+
   return (
     <div>
-      Find me in src/app.js!
+      <Details movieId={movieId} apiKey={apiKey} />
     </div>
   );
 }
--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -1,1 +1,34 @@
-https://api.themoviedb.org/3/movie/{movie_id}?api_key={c78445f7216324dcfc408149681f2fcd}&language=en-US
+import React, { useEffect, useState } from 'react';
+import './Details.css';
+
+export const Details = ({ movieId, apiKey }) => {
+  const [loading, setLoading] = useState(false);
+  const [movieData, setMovieData] = useState({});
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`, { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => setMovieData(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <h1 className="loading">Loading data...</h1>
+    )
+  }
+  return (
+    <div className="detail-container">
+      <div className="detail-background-picture" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movieData.backdrop_path}` }} />
+      <section className="movie-data-section">
+        <h1>
+          {movieData.title}
+          <img className="detail-cover-picture" src={`https://image.tmdb.org/t/p/w1280${movieData.backdrop_path}`} alt="Yo" />
+        </h1>
+      </section>
+    </div>
+
+  )
+}
