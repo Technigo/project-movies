7a033fea6e3b28450a35fb5c6ab0e2437e6aec66
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-27 16:29:46 +0200

    added css and cleaned up app.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header } from 'components/Header'
-import { StartPage } from 'components/StartPage'
-import { TVseries } from 'components/TVseries'
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <main>
-        <Header />
-        <Routes />
-        <Route path="/" element={<StartPage />} />
-        <Route path="/TV-series" element={<TVseries />} />
-        <Routes />
-      </main>
-    </BrowserRouter>
+
+    <main>
+      <Header />
+    </main>
+
   );
 }
