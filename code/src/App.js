91db2f8f1987d7b0b3c39179a207dfc6d6e498c2
khleoth <leothunell@gmail.com>
2023-03-27 19:23:37 +0200

    added apikeys

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/*  Fetching popular movies for the list page
+https://api.themoviedb.org/3/movie/popular?api_key={apikey}&language=en-US&page=1
+ Fetching a movie's details
+https://api.themoviedb.org/3/movie/{movie_id}?api_key={apikey}&language=en-US
+ Our API ID
+c78dd9bbcb31d6943ebef79a85954869  */
+
 export const App = () => {
   return (
     <div>
