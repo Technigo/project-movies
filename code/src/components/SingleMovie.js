c99725e368ef4bf2c6a9c49261d7e4b78f65dc48
Nora Terbocs <nora.terbocs@gmail.com>
2023-03-27 22:45:26 +0200

    Added hover

--- code/src/components/SingleMovie.js
+++ code/src/components/SingleMovie.js
@@ -1,14 +1,22 @@
-import React from 'react';
+/* eslint-disable operator-linebreak */
+import React, { useState } from 'react';
 
 const SingleMovie = ({ movie }) => {
+  const [hovered, setHovered] = useState(false)
   return (
-    <div className="movie-list-section" key={movie.id}>
-      <img className="movie-list-img" src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={`${movie.title}_image`} />
-      {/* <div className="movie-list-short-info">
-        <h1>{movie.title}</h1>
-        <p>{movie.release_date}</p>
-      </div> */}
-    </div>
+    <>
+      <img
+        className="movie-list-img"
+        src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+        alt={`${movie.title}_image`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)} />
+      {hovered &&
+      <div className="movie-list-short-info">
+        <h1 className="movie-title">{movie.title}</h1>
+        <p className="movie-release-date">{movie.release_date}</p>
+      </div>}
+    </>
   )
 }
 
