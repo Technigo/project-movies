9413efc1814ce6430d44bccbe0c7ecf6b7d57c33
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-03-28 14:10:43 +0200

    first in

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import PopularMovies from 'pages/PopularMovies';
+import NotFound from 'pages/NotFound';
+import MovieDetails from 'pages/MovieDetails';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PopularMovies />} />
+        <Route path="/details" element={<MovieDetails />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
+
+// https://api.themoviedb.org/3/movie/popular?api_key=52f9f6dc03440f8be0c16de930243dfe&language=en-US&page=1
---
+++ code/src/pages/PopularMovies.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const PopularMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=52f9f6dc03440f8be0c16de930243dfe&language=en-US&page=1')
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+  }, [])
+  if (loading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+  return (
+    <section className="popular-movies-section">
+      <h1>Popular Movies</h1>
+      <div>
+        {movies.map((movie) => {
+          return (
+            <article className="movie-article">
+              <Link
+                key={movie.id}
+                to={`/movies/${movie.id}`}>
+                <img src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title} />
+                <div className="movie-details">
+                  <h1>{movie.title}</h1>
+                  <p>{movie.release_date}</p>
+                </div>
+              </Link>
+            </article>
+          )
+        })}
+
+      </div>
+
+    </section>
+
+  )
+}
+
+export default PopularMovies;
