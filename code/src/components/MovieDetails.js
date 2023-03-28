c06cf2edc724ca27b580790c1692425d9535dad0
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-28 15:39:12 +0200

    It is still working - wohoo

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -7,7 +7,7 @@ const MovieDetails = () => {
   const navigate = useNavigate();
   //   const params = useParams();
   const { movieId } = useParams();
-  const [movie, setMovie] = useState({});
+  const [details, setDetails] = useState({});
   // this { pokemonName } above can be switched to the movie details
   const onBackButtonClick = () => {
     navigate(-1)
@@ -18,22 +18,15 @@ const MovieDetails = () => {
 
   useEffect(() => {
     fetch(MOVIE_DETAILS)
-      .then((response) => response.json())
-      .then((json) => {
-        setMovie(json);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    // console.log('params', params);
-    console.log('movieDetails', MovieDetails);
+      .then((data) => data.json())
+      .then((configuredData) => setDetails(configuredData))
   }, [movieId]);
   
   return (
     <section
       className="movie-details-containter"
       style={{ backgroundImage:
-      `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
+      `url(https://image.tmdb.org/t/p/w1280${details.backdrop_path})` }}>
       <button
         className="go-back-btn"
         type="button"
@@ -41,15 +34,17 @@ const MovieDetails = () => {
       </button>
       <img
         className="poster-img"
-        src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`}
         alt="poster" />
       <div
         className="movie-info">
         <div className="title-and-rating">
-          <h1 className="title">{movie.title}</h1>
-          <p className="rating">⭐️{movie.vote_average}</p>
+          <h1 className="title">{details.title}</h1>
+          <p className="rating">⭐️{details.vote_average}</p>
+          {/* Since toFixed() didn't work, you could use the following:
+average={String(Math.round(listItem.vote_average * 10) / 10)} */}
         </div>
-        <p className="overview">{movie.overview}</p>
+        <p className="overview">{details.overview}</p>
         <button type="button" onClick={onGoToNotFoundButtonClick}>
           Go to NotFound
         </button>
