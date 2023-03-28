7572657243172cd764b1ce78806c4b34578073d8
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-03-28 14:55:17 +0200

    created folder with components and urls

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
---
+++ code/src/utils/urls.js
@@ -0,0 +1,2 @@
+// export const BASE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=a2d2aecb5322eff67704d6c8635d60c1'
+// export const SINGLE_MOVIE_URL = (movieName) => `${BASE_URL}movie_id}`;
