6384d295a0bd1cbdcb252526eecacb7e4c182eb7
Vio Szabo <vio@vioszabo.se>
2023-03-28 20:07:53 +0200

    title of site and movies changed

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -19,8 +19,9 @@ export const Details = () => {
 
   return (
     <div className="details-container">
-      <h2>I am the details for {movie.title}</h2>
+      <h2>{movie.title}</h2>
       <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+      <p>Release date: {movie.release_date}</p>
       <p>Rating: {Math.round(movie.vote_average * 10) / 10}</p>
       <p>{movie.overview}</p>
       <p>Budget: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(movie.budget)}</p>
