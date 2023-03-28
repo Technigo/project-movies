c5409ffc9b1c6f3618a576ceec8d8d203ea07fb3
EmiliaGA <emilia-lundmark@hotmail.com>
2023-03-28 20:18:12 +0200

    fixed react-install

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MovieList } from 'components/MovieList';
 
 export const App = () => {
@@ -16,8 +16,8 @@ export const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MovieList movieList={movieList} />} />
-        <Route path="*" element={<Navigate to="/404" />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
