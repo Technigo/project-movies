e6d1779427719dd1a0ce77c165230d7ad07cc8c8
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-28 19:16:59 +0200

    tvshow componenet error fix

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
-  return (<header>
-    <h1>EVA FLEX</h1>
-    <nav>
-      <NavLink to="/">Home</NavLink>
-    </nav></header>)
+  return (
+    <header>
+      <ul className="navling-container">
+        <h1>EVA FLEX</h1>
+        <nav>
+          <li>
+            <NavLink to="/TV-series">Tv-series</NavLink>
+          </li>
+        </nav>
+      </ul>
+    </header>)
 }
