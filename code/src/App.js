2bd126971c164b173a957670b477976ccb8201aa
EmiliaGA <emilia-lundmark@hotmail.com>
2023-03-28 19:43:55 +0200

    trying to fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,41 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-import { MovieList } from 'components/Movies';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { MovieList } from 'components/MovieList';
 
 export const App = () => {
   const [movieList, setMovieList] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=c78445f7216324dcfc408149681f2fcd&language=en-US&page=1')
-      .then((res) => res.json())
-      .then((data) => {
-        setMovieList(data.results);
-      })
-      .finally(() => {
-        setTimeout(() => {
-          setLoading(false);
-        }, 2600);
+      .then((data) => data.json())
+      .then((configuredData) => {
+        setMovieList(configuredData.results);
       })
   }, []);
-
-  // only shows if spammed  or with slow internet
-  if (loading) {
-    return (
-      <div className="loader">
-        <p>Loading...</p>
-      </div>
-    );
-  }
   return (
     <BrowserRouter>
-      <main>
-        <Routes>
-          <Route path="/" element={<MovieList movies={movieList} />} />
-          <Route path="*" element={<Navigate to="/404" />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<MovieList movieList={movieList} />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
     </BrowserRouter>
   );
 }
