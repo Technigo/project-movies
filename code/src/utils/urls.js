e7b0f575ce0477989caf759157600b2c3eb76ad3
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-28 12:52:02 +0200

    Added components

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,3 @@
 // these URLS can be switched to the movie details
-export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=%7Bapi_key%7D&language=en-US&page=1';
 export const SINGLE_POKEMON_URL = (pokemonName) => `${BASE_URL}/${pokemonName}`;
