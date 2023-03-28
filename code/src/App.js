8552cbd7e1ccbcd4a4d643a1fdb666b6e62bb51d
Sammy <sammyolsson@gmail.com>
2023-03-28 21:47:19 +0200

    Testing branch

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//API KEY: bdc909b4c34b17568b8111077d9d4d62
+//Exempel på API-begäran: https://api.themoviedb.org/3/movie/550?api_key=bdc909b4c34b17568b8111077d9d4d62
+
 export const App = () => {
   return (
     <div>
