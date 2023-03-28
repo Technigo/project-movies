5ae5de7d441f675c248744de540bce9747879cca
khleoth <leothunell@gmail.com>
2023-03-28 19:16:54 +0200

    added minor details to component-details

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { /* useNavigate, */ useParams } from 'react-router-dom';
 import { SINGLE_MOVIE_URL } from 'utils/urls';
 
 export const Details = () => {
-  const navigate = useNavigate();
+  /* const navigate = useNavigate(); */
   const [movie, setMovie] = useState();
   const { id } = useParams();
 
@@ -13,17 +13,18 @@ export const Details = () => {
       .then((data) => setMovie(data))
   }, [id])
 
-  const onGoToNotFoundButtonClick = () => {
+  /*   const onGoToNotFoundButtonClick = () => {
     navigate('/404');
-  }
+  } */
   return (
     <div>
       {movie && (
-        <>
-          <h2> This is the Details component for the movie {movie.title} </h2>
-          <button type="button" onClick={onGoToNotFoundButtonClick}>Go to NotFound</button>
-          <img className="movie-backdrop" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} />
-        </>
+        <div className="movie-backdrop" style={{ backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})` }}>
+          <h2> <span>{movie.title}</span><span className="rating">{Math.round(movie.vote_average * 10) / 10}</span></h2>
+          {/* <button type="button" onClick={onGoToNotFoundButtonClick}>Go to NotFound</button> */}
+          <img className="movie-poster" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
+          <p className="synopsis">{movie.overview}</p>
+        </div>
       )}
     </div>)
 }
