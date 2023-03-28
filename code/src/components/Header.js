2731952848c706d2d3dfafd34ab9eb4ec226ddd9
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-28 16:43:19 +0200

    fix

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -5,7 +5,6 @@ export const Header = () => {
   return (<header>
     <h1>This is the header</h1>
     <nav>
-      <NavLink to="/details">Go to Details</NavLink>
       <NavLink to="/">Home</NavLink>
     </nav></header>)
 }
