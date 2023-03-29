5fcc214c076e25f0c75ab229854c58d9a7e2fcb9
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-03-29 23:01:49 +0200

    styling to the movie list with hover and movie info

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
   return (
-    <section>
+    <section className="movies-wrapper">
       {movies.map((movie) => (
         <Link
-          className="movie-list"
+          className="movies-container"
           key={movie.id}
           to={`/MovieDetails/${movie.id}`}>
 
           <img
-            // className=""
+            className="single-movie"
             src={`https://image.tmdb.org/t/p/w342${movie.poster_path} `}
             alt={movie.title} />
+          <div className="movie-hoverinfo">
+            <h1>{movie.title}</h1>
+            <h4>Released {movie.release_date}</h4>
+          </div>
         </Link>
       ))}
     </section>
--- code/src/components/urls.js
+++ code/src/components/urls.js
@@ -1,3 +1,5 @@
 /* eslint-disable */
 export const POPULARMOVIE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=2eb2f32ea6c8d9e53656503050d0b6fa&language=en-US&page=1';
 export const MOVIEDETAILS_URL = (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}?api_key=2eb2f32ea6c8d9e53656503050d0b6fa&language=en-US`;
+export const MOVIE_TRAILER_URL = (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=2eb2f32ea6c8d9e53656503050d0b6fa&language=en-US`;
+
