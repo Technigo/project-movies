3dbf479134f1cbbb0d7811b4b9ed6a9fa91684b4
camcron <camillacronqvist@hotmail.com>
2023-03-29 21:56:35 +0200

    Fetched API to show all the movies from popular

--- code/src/App.js
+++ code/src/App.js
@@ -1,24 +1,22 @@
-import React from "react";
-import "./styles.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { PopularList } from "./components/PopularList/PopularList";
-import { NotFound } from "./components/NotFound/NotFound";
-import { SingleMovie } from "./components/SingleMovie/SingleMovie";
+import React from 'react';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PopularList } from './components/PopularList/PopularList';
+// import { NotFound } from './components/NotFound/NotFound';
+import { SingleMovie } from './components/SingleMovie/SingleMovie';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/popularlist" element={<PopularList />} />
+        <Route path="/" element={<PopularList />} />
         <Route path="/movie/:id" element={<SingleMovie />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404/" replace />} />
+        {/* <Route path="/404" element={<NotFound />} /> */}
+        {/* <Route path="*" element={<Navigate to="/404/" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
 };
-
-
 
 // API KEY: bdc909b4c34b17568b8111077d9d4d62
 // Exempel på API-begäran: https://api.themoviedb.org/3/movie/550?api_key=bdc909b4c34b17568b8111077d9d4d62
