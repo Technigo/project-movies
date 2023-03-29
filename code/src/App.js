876bc49e52da9183e96351d1ebf53607825d64ba
Sammy <sammyolsson@gmail.com>
2023-03-29 20:04:17 +0200

    Added components, with js and css files

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-//API KEY: bdc909b4c34b17568b8111077d9d4d62
-//Exempel på API-begäran: https://api.themoviedb.org/3/movie/550?api_key=bdc909b4c34b17568b8111077d9d4d62
+// API KEY: bdc909b4c34b17568b8111077d9d4d62
+// Exempel på API-begäran: https://api.themoviedb.org/3/movie/550?api_key=bdc909b4c34b17568b8111077d9d4d62
 
 export const App = () => {
   return (
