0cc268ccbef3bd6fce780df2d38a9ffd862abbfa
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-29 10:01:59 +0200

    Added a basic header with navlinks

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import 'header.css';
 
 const Header = () => {
   return (
     <header>
+      <Link to="/"><img className="logo-image" src="https://images.unsplash.com/photo-1599305445671-ac291c95aaa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Movie Page Logo" width="80" height="50" /></Link>
+
       <nav>
-        <Link to="/pages/movieDetails">Details about movies</Link>
-        <Link to="/">List</Link>
+        <ul className="navlink-container">
+          <li className="navlink">
+            <NavLink to="/onCinema">Playiing Now</NavLink>
+          </li>
+          <li className="navlink">
+            <NavLink to="/TV-series">TV Series</NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   )
