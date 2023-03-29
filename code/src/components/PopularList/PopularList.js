85b317dc57aa5f6c9848814c16fdfca4fed46344
camcron <camillacronqvist@hotmail.com>
2023-03-29 22:02:09 +0200

    Removed extra curly

--- code/src/components/PopularList/PopularList.js
+++ code/src/components/PopularList/PopularList.js
@@ -22,17 +22,15 @@ export const PopularList = () => {
   }, []);
 
   return (
-    <div className="movie-wrapper">
+    <div className="popularPage">
       {list.map((movie) => (
-        <div key={movie.id} className="movieItem">
-          <Link to={`/movies/${movie.id}`}>
-            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
-            <div className="details">
-              <h1>{movie.title}</h1>
-              <p>Released {movie.release_date}</p>
-            </div>
-          </Link>
-        </div>
+        <Link key={movie.id} to={`/movies/${movie.id}`}>
+          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+          <div className="details">
+            <h1>{movie.title}</h1>
+            <p>Released {movie.release_date}</p>
+          </div>
+        </Link>
       ))}
     </div>
   )
