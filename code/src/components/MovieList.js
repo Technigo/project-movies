7f27490e7f0258256cd2fa746c2b31bb89f987df
fannystenberg <ffannystenberg@gmail.com>
2023-03-29 13:01:59 +0200

    added loading animation

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -8,20 +8,25 @@ const MovieList = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=c77f0f257b1db8c5cdfe593182e32f79&language=en-US&page=1')
-      .then((res) => res.json())
-      .then((data) => setList(data.results))
-      .catch((e) => {
-        console.error(e)
-      })
-      .finally(() => {
-        setLoading(false);
-      })
+    setTimeout(() => {
+      fetch('https://api.themoviedb.org/3/movie/popular?api_key=c77f0f257b1db8c5cdfe593182e32f79&language=en-US&page=1')
+        .then((res) => res.json())
+        .then((data) => setList(data.results))
+        .catch((e) => {
+          console.error(e)
+        })
+        .finally(() => {
+          setLoading(false);
+        })
+    }, 1700)
   }, [])
 
   if (loading) {
     return (
-      <p>Loading...</p>
+      <div className="loadingWrapper">
+        <img alt="loading" className="loadingImg" src="https://media.tenor.com/CLVR-rgpQL8AAAAi/popcorn-joypixels.gif" />
+        <p className="loadingText">Grab your popcorn, movies coming up!</p>
+      </div>
     )
   }
   return (
