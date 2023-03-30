ca79092aaf00c737146d21fa98c1629fc2284afe
Emilia Saberski <emilia@saberski.com>
2023-03-30 15:53:09 +0200

    Did some styling for the MovieList

--- code/src/components/MoviesList.js
+++ code/src/components/MoviesList.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import { LIST_URL } from '../data/Url';
 import '../css/MoviesList.css'
 
-
-
 export const MoviesList = () => {
   const [movies, setMovies] = useState([])
 
@@ -22,12 +20,11 @@ export const MoviesList = () => {
       {movies.map((movie) => (
         <Link key={movie.id} to={`/movies/${movie.id}`}>
           <div className="movie-overlay">
-          <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} className="movie-poster"/>
+            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} className="movie-poster" />
             <h1 className="movie-text">{movie.title}</h1>
           </div>
         </Link>
-        )
-      )}
+      ))}
     </section>
   )
 }
