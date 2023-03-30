0a218f6938ce8afadc1b4dd965bd9befd5210295
Sofia Gerdmar <sofia.gerdmar@gmail.com>
2023-03-30 21:47:24 +0200

    styling movielist

--- code/src/components/MovieList.js
+++ code/src/components/MovieList.js
@@ -6,12 +6,17 @@ export const MovieList = ({ movies }) => {
     <section className="list">
       {movies.map((movie) => {
         return (
-          <Link
-            key={movie.original_title}
-            to={`/details/${movie.id}`}
-            className="list-element">{movie.original_title}
-            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w185${movie.poster_path}` : ''} alt="movie poster" />
-          </Link>
+          <section>
+            <div className="list-container">
+              <Link
+                key={movie.original_title}
+                to={`/details/${movie.id}`}
+                className="list-title">{movie.original_title}
+              </Link>
+              <img className="list-img" src={movie.poster_path ? `https://image.tmdb.org/t/p/w1280${movie.poster_path}` : ''} alt="movie poster" />
+              <p className="list-date">Release date: {movie.release_date}</p>
+            </div>
+          </section>
         )
       })}
     </section>
