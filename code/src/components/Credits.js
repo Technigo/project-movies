a38c5184bff2a0323621c31ea1620a29b33b0ebc
Emilia Saberski <emilia@saberski.com>
2023-03-30 22:10:18 +0200

    tried fix the credits again (but failed). Have asked on stack

--- code/src/components/Credits.js
+++ code/src/components/Credits.js
@@ -2,26 +2,25 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { CREDITS_URL } from '../data/Url';
+import { API_KEY } from '../data/Url';
 
-export const Credits = () => {
-    const [ credits, setCredits ] = useState([])
+ const Credits = () => {
+    const [ credits, setCredits ] = useState({})
     const { movieId } = useParams()
   
     useEffect(() => {
-        fetch(CREDITS_URL(movieId))
-          .then((res) => res.json())
-          .then((json) => {
-            console.log(json)
-            setCredits(json.credits)
-          })
-      }, [movieId])
+        fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`)
+        .then((res) => res.json())
+        .then((data) => setCredits(data))
+    }, [movieId])
 
     return (
         <div className="cast">
-              {credits[0].map((credit) => (
+              {credits.map((credit) => (
             <p key={credit.name}>{credit.name}</p>
               ))}
             </div>
     )
 }
+
+export default Credits;
--- code/src/components/MoviesDetails.js
+++ code/src/components/MoviesDetails.js
@@ -1,12 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { DETAILS_URL } from '../data/Url';
 import '../css/MoviesDetails.css';
 
 export const MoviesDetails = () => {
-  const [ movie, setMovie ] = useState()
+  const [movie, setMovie] = useState()
   const navigate = useNavigate();
   const { movieId } = useParams()
 
@@ -16,35 +15,35 @@ export const MoviesDetails = () => {
 
   useEffect(() => {
     fetch(DETAILS_URL(movieId))
-        .then((res) => res.json())
-        .then((json) => {
+      .then((res) => res.json())
+      .then((json) => {
         setMovie(json)
-          })
-      }, [movieId])
-      console.log(movie)
+      })
+  }, [movieId])
+  console.log(movie)
 
   return (
-      <section className="movie-container">
+    <section className="movie-container">
       {movie && (
         <>
-        <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={movie.title} className="background"/>
-        <div className="summary">
-        <button className="back-button" type='button∑' onClick={(onBackButtonClick)}>Back</button>
-        <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} className="poster"/>
-        <div className="details">
-        <h1>{movie.title}</h1>
-        <span className="tagline">"{movie.tagline}"
-        </span>
-        <span className="overview">{movie.overview}
-        </span>
-        <span className="rating">⭐️ {Math.round(movie.vote_average * 10) / 10}
-        </span>
-        <span className="length">{movie.runtime} min
-        </span>
-        </div>
-        </div>
+          <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={movie.title} className="background" />
+          <div className="summary">
+            <button className="back-button" type="button" onClick={(onBackButtonClick)}> Back </button>
+            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} className="poster" />
+            <div className="details">
+              <h1>{movie.title}</h1>
+              <span className="tagline">{movie.tagline}
+              </span>
+              <span className="overview">{movie.overview}
+              </span>
+              <span className="rating">⭐️ {Math.round(movie.vote_average * 10) / 10}
+              </span>
+              <span className="length">{movie.runtime} min
+              </span>
+            </div>
+          </div>
         </>
-        )}
-        </section>
-    )
+      )}
+    </section>
+  )
 }
