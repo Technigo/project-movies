867aa024b4a92d925220b82a71fea102fc0b1c2f
theresBL <tbrannberglendt@gmail.com>
2023-03-30 16:09:46 +0200

    additional styling

--- code/src/components/Trailer.js
+++ code/src/components/Trailer.js
@@ -12,6 +12,7 @@ export const Trailer = () => {
       .then((res) => res.json())
       .then((data) => {
         setTrailer(data)
+        console.log(data)
       })
       .catch((e) => {
         console.error(e);
@@ -27,7 +28,7 @@ export const Trailer = () => {
         width="500"
         height="300"
         src={`https://www.youtube.com/embed/${trailer.key}`}
-        frameBorder="0"
+        // frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen />
     </div>
