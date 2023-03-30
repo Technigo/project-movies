35a97d9dba8ad90ef6f0d5621d9bb4cd5096eb44
Vera Sjunnesson <Vera@admins-mbp.home>
2023-03-30 15:08:56 +0200

    added a lot of movie details

--- code/src/components/MovieDetails.js
+++ code/src/components/MovieDetails.js
@@ -20,8 +20,34 @@ export const MovieDetails = () => {
   return (
     <div>
       <button type="button" onClick={onBackButtonClick}>Go back!</button>
-      <h2>{details.original_title}</h2>
-      <img src={details.backdrop_path ? `https://image.tmdb.org/t/p/w1280${details.backdrop_path}` : ''} alt="poster" />
+      <div className="movie-details">
+        <a href={`https://www.imdb.com/title/${details.imdb_id}/?ref_=nv_sr_srsg_0`} target="_blank" rel="noreferrer">
+          <img className="small-poster" src={`https://image.tmdb.org/t/p/w342/${details.poster_path}`} alt={details.original_title} />
+          <h2>{details.original_title}</h2>
+        </a>
+        <div className="genres">
+          {details.genres
+            ? (details.genres.map((genre) => {
+              return (
+                <p key={genre.id} className="genre">{genre.name}</p>
+              )
+            }))
+            : ''}
+        </div>
+        <div className="production-countries">
+          {details.production_countries
+            ? (details.production_countries.map((country) => {
+              return (
+                <p key={country.iso_3166_1} className="country">{country.name}</p>
+              )
+            }))
+            : ''}
+          <p key={details.original_title}>{new Date().getFullYear(details.release_date)}</p>
+        </div>
+        <p>{Math.floor(details.runtime / 60)}h {(details.runtime) % 60}min</p>
+        <p>Rating: {Math.round(details.vote_average * 10) / 10}/10</p>
+        <p>{details.overview}</p>
+      </div>
     </div>
   )
 }
