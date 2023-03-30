80245142f4df646ff12092cca8c6dfcf90a4bc02
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-03-30 17:42:25 +0200

    Add most styling for details

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -1,3 +1,7 @@
+/* eslint-disable react/jsx-max-props-per-line */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-first-prop-new-line */
+/* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -32,7 +36,12 @@ const Details = () => {
       ) : (
         <img className="backdrop" src="https://via.placeholder.com/1280x720" alt="Backdrop placeholder" />
       )}
-      <a className="goBackBtn" href="/"><svg className="goback-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z" fill="#fff" fillRule="evenodd" /></svg><p>Movies</p></a>
+      <a className="goBackBtn" href="/"><svg className="goback-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+        <path d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27
+        14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844
+        0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-
+        .7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z"
+        fill="#fff" fillRule="evenodd" /></svg><span className="backBtnName">Movies</span></a>
       <div className="movie-details-container">
         <div className="poster-rating-container">
           {movie.poster_path ? (
@@ -43,11 +52,10 @@ const Details = () => {
           <div className="title-rating-text">
             <h2 className="title-details title-details-margin">{movie.title}Movie title</h2>
             <h2 className="rating">{Math.round(movie.vote_average * 10) / 10} Rating goes here</h2>
-            <p className="movie-description">{movie.overview}Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, assumenda soluta nisi accusantium a quo eligendi ducimus optio! Odio velit nulla officiis molestiae ducimus ratione libero tempora cumque illum ullam?</p>
+            <p className="movie-description">{movie.overview}Description Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illum, assumenda soluta nisi accusantium a quo eligendi ducimus optio!
+            Odio velit nulla officiis molestiae ducimus ratione libero tempora cumque illum ullam?</p>
           </div>
-        </div>
-        <div className="details-container">
-          <div className="overview-text">{movie.overview}</div>
         </div>
       </div>
     </div>
