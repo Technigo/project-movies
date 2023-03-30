63933185a0c88f2ce4a999fd894cc2f18d067de9
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-30 10:03:32 +0200

    fixing component tvseries

--- code/src/components/TVseriesDetail.js
+++ code/src/components/TVseriesDetail.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { TVSHOW_URL } from 'utils/Urls'
 import { Link, useParams } from 'react-router-dom'
 
 export const TVseriesDetails = () => {
-  const [tvShow, setTVshow] = useState([])
+  const [tvDetails, setTvDetails] = useState([])
   const { id } = useParams()
 
   useEffect(() => {
-    fetch(TVSHOW_URL(id))
+    fetch(`https://api.themoviedb.org/3/tv/popular/${id}api_key=7f5b30559b3d85aec06d3d1a010f4a39&language=en-US`)
       .then((response) => response.json())
-      .then((data) => { setTVshow(data) })
+      .then((data) => { setTvDetails(data) })
       .catch((error) => alert(error, 'error'))
   }, [id])
 
@@ -20,8 +19,8 @@ export const TVseriesDetails = () => {
       </Link>
       <div
         className="background"
-        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${tvShow.backdrop_path})` }}>
-        <h1>{tvShow.name}</h1>
+        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${tvDetails.backdrop_path})` }}>
+        <h1>{tvDetails.name}</h1>
       </div>
     </div>
   )
