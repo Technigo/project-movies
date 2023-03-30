caefb5a25ef3228e97b6e3b10c2310e8c373464e
khleoth <leothunell@gmail.com>
2023-03-30 09:53:42 +0200

    Co-authored-by: jenster5 <jenster5@users.noreply.github.com>

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -27,23 +27,22 @@ export const Details = () => {
             backgroundSize: 'cover',
             backgroundPosition: 'center' }}>
           <div className="details-wrapper">
-            <img className="movie-poster" style={{ border: '5px solid white', marginTop: '80px', width: '70%' }} src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
+            <img className="movie-poster-details" style={{ border: '5px solid white', marginTop: '80px', width: '70%' }} src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
             <div
               className="poster-info"
               style={{ width: '90vw',
                 display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center' }}>
-              <h2>
+                flexDirection: 'column' }}>
+              <h2 style={{ margin: '0' }}>
                 <span style={{ color: 'white' }}>{movie.title} <br /></span>
                 <span style={{ color: 'white', marginLeft: '10px' }}>
-                  {Math.round(movie.vote_average * 10) / 10} {' '}
-                  <img src={star} className="star-icon" alt="a star" style={{ scale: '1.1' }} />
+                  {Math.round(movie.vote_average * 10) / 10}/10
+                  <img src={star} className="star-icon" alt="a star" style={{ scale: '1.1', marginLeft: '5px' }} />
 
                 </span>
               </h2>
 
-              <p className="synopsis" style={{ color: 'white', fontWeight: 'bold' }}>{movie.overview}</p>
+              <p className="synopsis" style={{ color: 'white', fontWeight: 'bold', marginBottom: 0 }}>{movie.overview}</p>
             </div>
           </div>
         </div>
