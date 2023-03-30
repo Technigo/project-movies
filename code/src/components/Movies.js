d7d8508cf39162fbbf9b9084e4cf8bb38fbacd2c
Hannah Ek <ekhannahek@gmail.com>
2023-03-30 21:01:16 +0200

    added loading function

--- code/src/components/Movies.js
+++ code/src/components/Movies.js
@@ -1,30 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Spinning from './Spinner';
 
 export const Movies = () => {
   const [movies, setMovies] = useState({ results: [] });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=4d07e418e3a6ee346618c7a898de9210&language=en-US&page=1')
       .then((response) => response.json())
       .then((fetchData) => {
         setMovies(fetchData);
-      });
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
-    <div className="movie-container">
-      {movies.results.map((singleMovie) => {
-        return (
-          <Link key={singleMovie.id} to={`/details/${singleMovie.id}`} className="movie-list">
-            <img className="image" src={`https://image.tmdb.org/t/p/w342/${singleMovie.poster_path}`} alt={singleMovie.original_title} />
-            <div className="text-container">
-              <h2 className="name">{singleMovie.original_title}</h2>
-              <p className="date">Release date {singleMovie.release_date}</p>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
+    <>
+      <div className="movie-container">
+        {movies.results.map((singleMovie) => {
+          return (
+            <Link key={singleMovie.id} to={`/details/${singleMovie.id}`} className="movie-list">
+              <img className="image" src={`https://image.tmdb.org/t/p/w342/${singleMovie.poster_path}`} alt={singleMovie.original_title} />
+              <div className="text-container">
+                <h2 className="name">{singleMovie.original_title}</h2>
+                <p className="date">Release date {singleMovie.release_date}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      {loading && (<Spinning />)}
+    </>
   );
 };
---
+++ code/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinning = () => (
+  <div className="loading-thoughts">
+    <div className="loading-spinner">
+    🍿
+    </div>
+  </div>
+)
+
+export default Spinning;
