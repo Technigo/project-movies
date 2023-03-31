068641936a36f29e55f781a75168496fd795dc96
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-31 13:32:03 +0200

    Made navbar responsive

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,29 +1,44 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import 'header.css';
 
 const Header = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
   return (
-    <header className="header-top">
-      <div className="logo-container">
-        <Link to="/"><img className="logo-image" src="../images/popcorn-logo.jpg" alt="Movie Page Logo" /></Link>
-        <Link to="/" style={{ textDecoration: 'none' }}><h3 className="logo-title">MOVIE NIGHT</h3></Link>
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo-container" />
+        <Link to="/">
+          <img className="logo-image" src="../images/popcorn-logo.jpg" alt="Movie Page Logo" />
+        </Link>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <h3 className="logo-title">MOVIE NIGHT</h3>
+        </Link>
+        <div />
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <img src="../images/burger-bar.png" alt="menu" style={{ width: 40, height: 40, backgroundColor: 'whitesmoke', borderRadius: 5 }} />
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Popular</NavLink>
+            </li>
+            <li>
+              <NavLink to="/upcoming">Upcoming</NavLink>
+            </li>
+            <li>
+              <NavLink to="/tv">TV Series</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-      <nav>
-        <ul className="navlink-container">
-          <li className="navlink">
-            <NavLink to="/">Popular</NavLink>
-          </li>
-          <li className="navlink">
-            <NavLink to="/upcoming">Upcoming</NavLink>
-          </li>
-          <li className="navlink">
-            <NavLink to="/tv">TV Series</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    </nav>
   )
 };
-
 export default Header;
