f41edba2ed5db6446000cb5fb5e1d2b31796eeb5
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-31 16:41:48 +0200

    Fixed footer and the links

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -3,15 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-/* Ask Daniel at todays 1:1 as VS code asks if we mean "Footer" below - but the header works fine */
-/* so we don't really understand why it's complaining here by the footer but not with the header */
 const Footer = () => {
   return (
     <footer>
-      <p className="footer-paragraph">A Technigo movie release project by Ylva Karlsson & Sandra Olsson </p>
+      <p className="footer-paragraph">A Technigo movie release project made by:</p>
       <div className="footer-container">
         <div className="footer-section">
-          <h3>Ylva Karlsson</h3>
+          <h4>Ylva Karlsson</h4>
           <a href="https://github.com/YlvaKarlsson">
             <FontAwesomeIcon icon={faGithub} size="3x" />
           </a>
@@ -23,25 +21,17 @@ const Footer = () => {
           </a>
         </div>
         <div className="footer-section">
-          <h3>Sandra Olsson</h3>
+          <h4>Sandra Olsson</h4>
           <a href="https://github.com/SandraMadeleine">
             <FontAwesomeIcon icon={faGithub} size="3x" />
           </a>
-          <a href="https://www.linkedin.com/in/person2">
+          <a href="https://www.linkedin.com/">
             <FontAwesomeIcon icon={faLinkedin} size="3x" />
           </a>
-          <a href="mailto:person2@example.com">
-            <FontAwesomeIcon icon={faEnvelope} size="3x" />ADD SANDRAS MAIL
+          <a href="mailto:sandramohlsson@yahoo.com">
+            <FontAwesomeIcon icon={faEnvelope} size="3x" />
           </a>
         </div>
-      </div>
-      <div className="footer-favicon-text">
-        <a
-          href="https://icons8.com"
-          rel="noopener noreferrer"
-          target="_blank">
-              Favicon from icons8.com
-        </a>
       </div>
     </footer>
   )
