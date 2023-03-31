d1d2841de842c62f73fa1c6a04f83202827b4ca1
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-31 14:58:16 +0200

    Ylvas branch with fixed footer-links

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
+/* Ask Daniel at todays 1:1 as VS code asks if we mean "Footer" below - but the header works fine */
+/* so we don't really understand why it's complaining here by the footer but not with the header */
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-text">
-        <p>A Technigo movie release project by Ylva Karlsson & Sandra Olsson</p>
+      <p>A Technigo movie release project by Ylva Karlsson & Sandra Olsson </p>
+      <div className="footer">
+        {/* <div className="footer-text">
+          {/* <p>A Technigo movie release project by Ylva Karlsson & Sandra Olsson </p> */}
+        {/* </div> */}
+        <div className="footer-section">
+          <h3>Ylva Karlsson</h3>
+          <a href="https://github.com/YlvaKarlsson">
+            <FontAwesomeIcon icon={faGithub} size="3x" />
+          </a>
+          <a href="https://www.linkedin.com/in/ylvakarlsson87/">
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
+          </a>
+          <a href="mailto:ylva_87@hotmail.com">
+            <FontAwesomeIcon icon={faEnvelope} size="3x" />
+          </a>
+        </div>
+      </div>
+      {/* footer-text below was called footer-section from ChatGPT */}
+      <div className="footer-section">
+        <h3>Sandra Olsson</h3>
+        <a href="https://github.com/SandraMadeleine">
+          <FontAwesomeIcon icon={faGithub} size="3x" />
+        </a>
+        <a href="https://www.linkedin.com/in/person2">
+          <FontAwesomeIcon icon={faLinkedin} size="3x" />
+        </a>
+        <a href="mailto:person2@example.com">
+          <FontAwesomeIcon icon={faEnvelope} size="3x" />ADD SANDRAS MAIL
+        </a>
+      </div>
+      <div className="footer-favicon-text">
         <a
           href="https://icons8.com"
           rel="noopener noreferrer"
