6dd0ea8910f553d05b342458218d243404cc4462
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-03-31 15:47:22 +0200

    Change h2 to h1, update alt text for images

--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -24,16 +24,16 @@ const Details = () => {
     <div className="backdrop-overlay">
       {/* The ternary operator checks if the image paths property of the movieDetail object is truthy (i.e., not undefined, null, 0, false, etc.). If it is truthy, it
       sets the src attribute of the imgtag to the URL of the image file. If it is falsy, it sets the src` attribute to an empty string. */}
-      <img className="backdrop" src={movieDetail.backdrop_path ? `https://image.tmdb.org/t/p/w1280${movieDetail.backdrop_path}` : ''} alt={movieDetail.title} />
+      <img className="backdrop" src={movieDetail.backdrop_path ? `https://image.tmdb.org/t/p/w1280${movieDetail.backdrop_path}` : ''} alt={`Movie backdrop for"${movieDetail.title}`} />
       <a className="goBackBtn" href="/"><svg className="goback-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z" fill="#fff" fillRule="evenodd" /></svg><span className="backBtnName">Movies</span></a>
       <div className="movie-details-container">
         <div className="poster-rating-container">
-          <img className="poster" src={movieDetail.poster_path ? `https://image.tmdb.org/t/p/w342/${movieDetail.poster_path}` : ''} alt={movieDetail.title} />
+          <img className="poster" src={movieDetail.poster_path ? `https://image.tmdb.org/t/p/w342/${movieDetail.poster_path}` : ''} alt={`Movie poster for" ${movieDetail.title}`} />
           <div className="title-rating-text">
-            <h2 className="title-rating-container">
+            <h1 className="title-rating-container">
               <span className="title-details title-details-margin">{movieDetail.title}</span>
               <span className="rating">⭐{Math.round(movieDetail.vote_average * 10) / 10}</span>
-            </h2>
+            </h1>
             <p className="movie-description">{movieDetail.overview}</p>
           </div>
         </div>
