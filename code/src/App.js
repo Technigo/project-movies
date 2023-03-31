8671f4a0341d7ae729649612d04c0f0fe4aaa3e5
EmiliaGA <emilia-lundmark@hotmail.com>
2023-03-31 09:19:00 +0200

    added loader and header

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,32 @@
+/* eslint-disable react/self-closing-comp */
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MovieList } from 'components/MovieList';
+import { Header } from 'components/Header'
 
 export const App = () => {
   const [movieList, setMovieList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=c78445f7216324dcfc408149681f2fcd&language=en-US&page=1')
       .then((data) => data.json())
       .then((configuredData) => {
         console.log(configuredData.results)
         setMovieList(configuredData.results);
       })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
   }, []);
+  if (loading) {
+    return (
+      <div className="loader">Loading...</div>
+    )
+  }
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<MovieList movieList={movieList} />} />
 
---
+++ code/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Movie releases</h1>
+    </header>
+  )
+}
+
