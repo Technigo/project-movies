b60f68d82e8a324e8e4a694189a8a1a3b7eddb0a
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-04-01 08:31:50 +0200

    loading intro

--- code/src/Components/Intro.js
+++ code/src/Components/Intro.js
@@ -4,8 +4,8 @@ const Popcorn = () => (
   <div className="loading-movies">
     <span className="loading-intro" role="img" aria-label="loading">
 
-      <img src="https://payload.cargocollective.com/1/1/44701/13320134/New-Yorker-Movies-GIFWEB.gif" alt="Smiley face" width="500" height="410" />
-      <h5>bajs</h5>
+      <img src="https://payload.cargocollective.com/1/1/44701/13320134/New-Yorker-Movies-GIFWEB.gif" alt="popcorngif" className="center" />
+      <h5>Get your snacks ready</h5>
     </span>
   </div>
 )
