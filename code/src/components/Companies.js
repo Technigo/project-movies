82d3ef2bf110f06c04d1415e365c641703d2dcc8
Nora Terbocs <nora.terbocs@gmail.com>
2023-04-01 11:29:49 +0200

    Added text if production company img is not available

--- code/src/components/Companies.js
+++ code/src/components/Companies.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
-import { CgWebsite } from 'react-icons/cg'
+import { CgWebsite, CgSmileSad } from 'react-icons/cg'
 import { FaCity } from 'react-icons/fa'
 import { API_KEY } from '../utils/urls';
 import BackButton from './BackButton'
@@ -21,7 +21,8 @@ const Companies = () => {
       <BackButton />
       <div className="single-company-page-img-details-container">
         <h1 className="single-company-page-name">{company.name}</h1>
-        {company.logo_path !== undefined && <img className="single-company-page-img" src={`https://image.tmdb.org/t/p/w780${company.logo_path}`} alt={`${company.name}_image`} />}
+        {company.logo_path !== undefined && company.logo_path !== null
+          ? <img className="single-company-page-img" src={`https://image.tmdb.org/t/p/w780${company.logo_path}`} alt={`${company.name}_image`} /> : <p style={{ color: 'white' }}>The image is not available <CgSmileSad /></p>}
         <div className="single-company-page-details">
           <p className="single-company-page-headquarters"><FaCity /> : {company.headquarters}</p>
           <p className="single-company-page-homepage"><CgWebsite /> : {company.homepage}</p>
