f1eedf0822a0242ee9893edea85a5661650fe746
Alexander-Gabor <gaboralexanderalexgmail.com>
2023-04-01 16:47:04 +0200

    Responsiveness fixes and making logo clickable

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,12 +1,18 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Logoimage from '../images/moviesheader.PNG'
 
 const Header = () => {
+  const navigate = useNavigate();
+  function handleLogoClick() {
+    navigate(-1)
+  }
   return (
     <header>
-      <img className="logo-image" src={Logoimage} alt="site logo" />
+      <img className="logo-image" src={Logoimage} alt="site logo" onClick={() => { handleLogoClick() }} />
       <nav>
         <NavLink to="/"> Upcoming movies </NavLink>
         <NavLink to="/popularmovies"> Popular movies </NavLink>
