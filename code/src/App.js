c9c48139f38ff180af0ad79498898f0654b19124
Emma Engvall <emma.engvall@ica.se>
2023-04-02 12:01:04 +0200

    removed header and added styling for detail-page

--- code/src/App.js
+++ code/src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { List } from 'components/List'
 import NotFound from 'components/NotFound';
 import { Details } from 'components/Details';
-import Header from 'components/Header';
+
 import Footer from 'components/Footer';
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<List />} />
         <Route path="/details/:movieId" element={<Details />} />
--- code/src/components/Details.js
+++ code/src/components/Details.js
@@ -18,20 +18,21 @@ export const Details = () => {
   }
 
   return (
-    <article className="detailPage">
-      <div className="background">
-        <img className="background-img" src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`} alt={movie.original_title} />
-        <div className="details-container">
+    <div className="background">
+      <img className="background-img" src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`} alt={movie.original_title} />
+      <button className="backButton" type="button" onClick={onGoBackButtonClick}>
+        Back to movies
+      </button>
+      <div className="details-container">
+        <img className="poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
+        <div className="movie-text">
           <h2>{movie.title}</h2>
-          <img className="poster" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title} />
           <p><strong>Release date:</strong> {movie.release_date}</p>
           <p><strong>Rating:</strong> ⭐ {Math.round(movie.vote_average * 10) / 10}</p>
           <p><strong>About:</strong> {movie.overview}</p>
           <p><strong>Budget:</strong> {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(movie.budget)}</p>
-          <button type="button" onClick={onGoBackButtonClick}>Go back
-          </button>
         </div>
       </div>
-    </article>
+    </div>
   )
 }
